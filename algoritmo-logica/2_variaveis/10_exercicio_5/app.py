3d8eca42b7a12ccec82e05c7e9ecba6e6cd01281
George Salu <george.salu10@gmail.com>
2025-12-24 14:29:28 -0300

    algoritmo-logica - secao 2 - variaveis - exercicio 5

---
+++ algoritmo-logica/2_variaveis/10_exercicio_5/app.py
@@ -0,0 +1,19 @@
+"""
+Crie as variaveis: a = 5, b = 3, c = 10, d = 2 e realize as seguintes comparacoes
+a > b; c == a; d != a; a <= c;b > c; a != c; d >= a; a != b; d < c; b >= a;
+"""
+a = 5
+b = 3
+c = 10
+d = 2
+
+print(a > b)
+print(c == a)
+print(d != a)
+print(a <= c)
+print(b > c)
+print(a != c)
+print(d >= a)
+print(d != b)
+print(d < c)
+print(b >= a)
