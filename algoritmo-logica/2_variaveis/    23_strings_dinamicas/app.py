17984fb2455812050d2d68a82a790c3044e80e42
George Salu <george.salu10@gmail.com>
2025-12-25 15:32:33 -0300

    algoritmo-logica - secao 2 - variaveis - strings dinamicas

---
+++ algoritmo-logica/2_variaveis/    23_strings_dinamicas/app.py
@@ -0,0 +1,9 @@
+name = "Matheus"
+print("Ola, meu nome é %s"%name)
+
+idade = 29
+print("eu tenho %d anos"%idade)
+
+carro = "Ferrari"
+ano = 2000
+print("O meu carro é uma %s e foi frabricado no ano %d"%(carro,ano))
