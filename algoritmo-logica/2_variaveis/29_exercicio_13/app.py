4072b8aa6f9cf43cf5169ee9350572da48fbc064
George Salu <george.salu10@gmail.com>
2025-12-25 16:55:56 -0300

    algoritmo-logica - secao 2 - variaveis - exercicio 13

---
+++ algoritmo-logica/2_variaveis/29_exercicio_13/app.py
@@ -0,0 +1,9 @@
+"""
+Crie uma variavel com a frase: nos estamos procurando o rubi na floresta
+e imprima apenas a palavra rubi da frase 
+"""
+frase = "Nós estamos procurando o rubi na floresta"
+
+rubi = frase[25:29]
+
+print(rubi)
