e9dbc651c5e7ca0b966826e99402f791e999a713
George Salu <george.salu10@gmail.com>
2025-12-25 15:39:50 -0300

    algoritmo-logica - secao 2 - variaveis - exercicio 11

--- algoritmo-logica/2_variaveis/25_exercicio_11/app.py
+++ algoritmo-logica/2_variaveis/25_exercicio_11/app.py
@@ -1,3 +1,8 @@
+"""
+Crie uma string dinamica exibindo o seu nome e sua idade
+crie uma outra string dinamica exibindo uma quantidade de dinheiro que voce possui
+"""
+
 nome = "Matheus"
 idade = 29
 print("Eu sou o %s e tenho %d anos" % (nome, idade))
