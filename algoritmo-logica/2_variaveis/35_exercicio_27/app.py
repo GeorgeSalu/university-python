f2bdc40c97f2d0046f67ceed07fc1f8ef472c9dd
George Salu <george.salu10@gmail.com>
2025-12-26 10:56:58 -0300

    algoritmo-logica - secao 2 - variaveis - exercicio 17

---
+++ algoritmo-logica/2_variaveis/35_exercicio_27/app.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que recebe uma base e uma potencia
+e exiba o resultado na tela
+"""
+base = int(input("Digite o valor base: "))
+potencia = int(input("Digite o valor potencia: "))
+
+calculo = base**potencia
+print(calculo)
