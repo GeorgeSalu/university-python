68205c18a661f0cd4122a944ed9f7a5f15f8b0a4
George Salu <george.salu10@gmail.com>
2025-12-25 21:47:33 -0300

    algoritmo-logica - secao 2 - variaveis - exercicio 14

---
+++ algoritmo-logica/2_variaveis/31_exercicio14/app.py
@@ -0,0 +1,7 @@
+"""
+Crie um programa que receba o seu nome como dado
+imprima o seu nome em uma frase de saudação utilizando a forma de strings dinamicas
+"""
+nome = input("Digite seu nome: ")
+print(nome)
+print("Ola %s, tudo bem?"%nome)
