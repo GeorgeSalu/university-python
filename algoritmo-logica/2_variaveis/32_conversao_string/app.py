9cb14d6794a6db4280ec7fe74e8088d9490ddec4
George Salu <george.salu10@gmail.com>
2025-12-26 10:12:00 -0300

    algoritmo-logica - secao 2 - variaveis - conversao de dados

--- algoritmo-logica/2_variaveis/32_conversao_string/app.py
+++ algoritmo-logica/2_variaveis/32_conversao_string/app.py
@@ -6,3 +6,8 @@ numero = int(numero_text)
 numero_novo = 5 + numero
 
 print(numero_novo)
+
+novo_float = float(input("digite o seu salario : "))
+print(novo_float)
+soma_float = 100.59+novo_float
+print(soma_float)
