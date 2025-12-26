cbd99728d30e619edd925beae7c398530192897f
George Salu <george.salu10@gmail.com>
2025-12-26 10:31:49 -0300

    algoritmo-logica - secao 2 - variaveis - exercicio 15

--- algoritmo-logica/2_variaveis/33_exercicio_15/app.py
+++ algoritmo-logica/2_variaveis/33_exercicio_15/app.py
@@ -1,5 +1,9 @@
+"""
+Crie duas variaveis que vao receber numeros
+faca a conversao deles e insira em uma variavel
+"""
 num_a = int(input("digite o primeiro numero: "))
 num_b = int(input("digite o segundo numero: "))
 
 soma = num_a + num_b
-print(soma)
+print("a soma dos dois numeros é %d"%soma)
