3a7e8f139ed21c42c53fef42f1316c1259d2350d
George Salu <george.salu10@gmail.com>
2025-12-26 12:00:05 -0300

    algoritmo-logica - secao 3 - estruturas condicionais - exercicio 20

--- algoritmo-logica/3_estrutura_de_condicao/3_exercicio_20/app.py
+++ algoritmo-logica/3_estrutura_de_condicao/3_exercicio_20/app.py
@@ -1,3 +1,8 @@
+"""
+escreva um programa que le uma idade e insira ela em uma variavel
+verifique por if se a idade é maior ou igual a 18
+se sim, imprima uma mensagem "pode entrar na balada""
+"""
 idade = int(input("digite a idade: "))
 
 print(idade)
