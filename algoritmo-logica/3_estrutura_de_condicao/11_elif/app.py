624cead3234a7af281c71ae9cc81b9e3ff668b11
George Salu <george.salu10@gmail.com>
2025-12-27 10:58:12 -0300

    algoritmo-logica - secao 3 - estruturas condicionais - condiciona elif

---
+++ algoritmo-logica/3_estrutura_de_condicao/11_elif/app.py
@@ -0,0 +1,8 @@
+nome = input("digite o seu nome: ")
+
+if nome == "george":
+    print("ola, voce é um admin")
+elif nome == "pedro":
+    print("ola, voce é um produtor de conteudo")
+else:
+    print("vc é um usuario comum")
