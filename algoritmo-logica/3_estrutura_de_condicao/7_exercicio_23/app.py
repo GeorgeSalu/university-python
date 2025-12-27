82a5e5750eae8c9c20651e89aa9b4b6bf5a5dcc2
George Salu <george.salu10@gmail.com>
2025-12-27 10:35:32 -0300

    algoritmo-logica - secao 3 - estruturas condicionais - exercicio 23

---
+++ algoritmo-logica/3_estrutura_de_condicao/7_exercicio_23/app.py
@@ -0,0 +1,11 @@
+"""
+crie um programa com a variavel salario
+se for maior que 1800 imprima uma mensagem de que é necessario pagar imposto de renda
+se nao imprima uma mensagem que nao precisa parr IR
+"""
+salario = float(input("Qual o seu salario? "))
+
+if salario > 1800:
+    print("voce precisa pagar imposto de renda")
+else:
+    print("voce nao precisa pagar imposto de salario")
