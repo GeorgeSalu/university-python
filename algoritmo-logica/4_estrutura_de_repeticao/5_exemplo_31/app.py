6ad76b73e50c78f9f248e9eac665543e61d78b2e
George Salu <george.salu10@gmail.com>
2025-12-27 12:43:17 -0300

    algoritmo-logica - secao 4 - estruturas de repeticao - exercicio 31

--- algoritmo-logica/4_estrutura_de_repeticao/5_exemplo_31/app.py
+++ algoritmo-logica/4_estrutura_de_repeticao/5_exemplo_31/app.py
@@ -1,5 +1,6 @@
 """
 crie um programa que imprime todos os numeros pares de 1 a 50
+imprima todos os numeros na tela
 """
 numero = 1
 
@@ -7,4 +8,3 @@ while numero <= 50:
     if numero % 2 == 0:
         print(numero)
     numero = numero + 1
-    
