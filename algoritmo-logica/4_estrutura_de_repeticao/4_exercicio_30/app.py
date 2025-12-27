cf305a701b7b684c20ac874130b263c81e400a0f
George Salu <george.salu10@gmail.com>
2025-12-27 12:40:01 -0300

    algoritmo-logica - secao 4 - estruturas de repeticao - exercicio 20

---
+++ algoritmo-logica/4_estrutura_de_repeticao/4_exercicio_30/app.py
@@ -0,0 +1,10 @@
+"""
+crie um loop while em que o fim é determinado pelu numero que o usuario insere no sistema
+imprima os numeros com print
+"""
+fim = int(input("quando o loop vai terminar : "))
+
+inicio = 0
+while inicio < fim:
+    print(inicio)
+    inicio = inicio + 1
