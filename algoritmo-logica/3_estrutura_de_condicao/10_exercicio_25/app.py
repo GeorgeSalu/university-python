13b187f64255dd8ecbe4d6e39e15f65f44fd5711
George Salu <george.salu10@gmail.com>
2025-12-27 10:53:15 -0300

    algoritmo-logica - secao 3 - estruturas condicionais - exercicio 25 solucao

--- algoritmo-logica/3_estrutura_de_condicao/10_exercicio_25/app.py
+++ algoritmo-logica/3_estrutura_de_condicao/10_exercicio_25/app.py
@@ -4,3 +4,18 @@ verifique se o numero é maior que 10,se nao for imprim uma mensagem avisando qu
 no primeiro if verifique se o numero é menor que 20, e imprima a multiplicacao dele por 2
 se nao imprima o numero multiplicador por 5
 """
+numero = int(input("insira um numero: "))
+
+print(numero)
+
+if numero > 10:
+
+    if numero > 20:
+        print("numero maior que 20")
+        print(numero * 2)
+    else:
+        print("numero menor que 20")
+        print(numero * 5)
+
+else:
+    print("o numero precisa ser maior que 10")
