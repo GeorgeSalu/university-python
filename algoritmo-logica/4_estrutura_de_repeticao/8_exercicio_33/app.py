4aff00a17a6f7fa02dc83e1dd22a392429a0315d
George Salu <george.salu10@gmail.com>
2025-12-27 14:38:35 -0300

    algoritmo-logica - secao 4 - estruturas de repeticao - exercicio 33 solucao

--- algoritmo-logica/4_estrutura_de_repeticao/8_exercicio_33/app.py
+++ algoritmo-logica/4_estrutura_de_repeticao/8_exercicio_33/app.py
@@ -2,3 +2,13 @@
 crie um loop que exibe os numeros digitados do usuario na tela
 o loop deve ser canceldo quando o usuario digitar 0
 """
+x = 0
+
+while(x < 1):
+  numero = int(input("Digite um número: "))
+
+  print(numero)
+
+  if numero == 0:
+    print("Saindo do loop!")
+    break
