98310187ca6783ef86f8e8a2c9faec8d8d105bff
George Salu <george.salu10@gmail.com>
2025-12-27 10:50:26 -0300

    algoritmo-logica - secao 3 - estruturas condicionais - exercicio 25 enunciado

---
+++ algoritmo-logica/3_estrutura_de_condicao/10_exercicio_25/app.py
@@ -0,0 +1,6 @@
+"""
+escreva um programa que recebe um numero
+verifique se o numero é maior que 10,se nao for imprim uma mensagem avisando que para prosseguir precisa se maior que 10
+no primeiro if verifique se o numero é menor que 20, e imprima a multiplicacao dele por 2
+se nao imprima o numero multiplicador por 5
+"""
