edd70b87bcebf3331606a62b69dc9934a0a03d02
George Salu <george.salu10@gmail.com>
2025-12-27 23:10:45 -0300

    algoritmo-logica - secao 5 - percorrendo listas

--- algoritmo-logica/5_listas/3_percorrendo_lista/app.py
+++ algoritmo-logica/5_listas/3_percorrendo_lista/app.py
@@ -2,3 +2,10 @@ lista = ["maça","uva","banana","morango","goiaba","figo","ameixa"]
 
 for nome in lista:
     print(nome)
+
+print("-----------------------------")
+
+i = 0
+while i < len(lista):
+    print(lista[i])
+    i = i + 1
