6dfd8e63e76e8c64535e93992c90fddd0ed09a15
George Salu <george.salu10@gmail.com>
2025-12-30 09:37:04 -0300

    algoritmo-logica - secao 5 - exercicio 37 - exercicio 39

---
+++ algoritmo-logica/5_listas/12_exercicio_39/app.py
@@ -0,0 +1,13 @@
+"""
+crie uma lista com 5 numeros
+faça um loop baseado na funcao len
+"""
+
+lista = [5,10,15,20,25]
+
+print(lista)
+
+i = 0
+while i < len(lista):
+    print(lista[i] + 10)
+    i += 1
