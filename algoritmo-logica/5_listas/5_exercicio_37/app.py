fc17b237765eaaae59a2333aa7a706e5cfef1567
George Salu <george.salu10@gmail.com>
2025-12-29 22:32:17 -0300

    algoritmo-logica - secao 5 - exercicio 37

---
+++ algoritmo-logica/5_listas/5_exercicio_37/app.py
@@ -0,0 +1,17 @@
+"""
+crie uma lista com 5 valores zerados
+faça um loop para percorrer a lista e preencher os valores zerados
+os valores devem ser inseridos pelo usuario
+imprima o resultado final com print
+"""
+lista = [0,0,0,0,0]
+
+print(lista)
+
+i = 0
+while i < 5:
+    numero = int(input("Digite um valor: "))
+    lista[i] = numero
+    i += 1
+
+print(lista)
