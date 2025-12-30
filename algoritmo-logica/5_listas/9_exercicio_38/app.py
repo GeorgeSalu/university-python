3bcbd8475dbfbafe98d4a0e7b7ad2ab2cd2a347e
George Salu <george.salu10@gmail.com>
2025-12-30 09:17:16 -0300

    algoritmo-logica - secao 5 - exercicio 37 - exercicio 38

---
+++ algoritmo-logica/5_listas/9_exercicio_38/app.py
@@ -0,0 +1,9 @@
+"""
+crie uma lista com 10 elementos
+por meio de fatiamento extraia do terceiro ao setimo
+atribua eta nova lista a uma variavel e a exiba no terminal
+"""
+lista = [0,1,2,3,4,5,6,7,8,9]
+
+nova_lista = lista[2:7]
+print(nova_lista)
