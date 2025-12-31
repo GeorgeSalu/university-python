4a8806d8314c18bc9210780cad5787752c29f7e1
George Salu <george.salu10@gmail.com>
2025-12-31 14:58:10 -0300

    algoritmo-logica - secao 5 - lista dentro de listas

---
+++ algoritmo-logica/5_listas/27_lista_com_listas/app.py
@@ -0,0 +1,9 @@
+carro_a = ["bmw",50000]
+carro_b = ["ferrari", 1000000]
+carro_c = ["vw", 300000]
+
+carros = [carro_a,carro_b,carro_c]
+print(carros)
+print(carros[0][0])
+print(carros[0][1])
+print(carros[2][0])
