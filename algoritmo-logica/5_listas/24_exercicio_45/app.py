b8ea4a62e7a285437dc364cb6e83259311dd1922
George Salu <george.salu10@gmail.com>
2025-12-31 10:32:07 -0300

    algoritmo-logica - secao 5 - exercicio 45

--- algoritmo-logica/5_listas/24_exercicio_45/app.py
+++ algoritmo-logica/5_listas/24_exercicio_45/app.py
@@ -1,2 +1,9 @@
+"""
+crie um loop com range
+que parte do numero 3
+vi ate o numero 99
+e com o incremento de 3
+imprima todos os numeros do loop na tela
+"""
 for numero in range(3, 99, 3):
     print(numero)
