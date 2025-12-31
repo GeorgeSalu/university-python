c39085d197befda2a9358b755c6f3e3d1cbbbb0c
George Salu <george.salu10@gmail.com>
2025-12-31 14:55:24 -0300

    algoritmo-logica - secao 5 - exercicio 46

---
+++ algoritmo-logica/5_listas/26_exercicio/app.py
@@ -0,0 +1,11 @@
+"""
+crie um programa que verifique o menor valore de uma lista
+"""
+lista = [10,20,50,35,-14]
+
+menor_valor = lista[0]
+for i in lista:
+    if i < menor_valor:
+        menor_valor = i
+
+print("o menor vlor é o %d" % menor_valor)
