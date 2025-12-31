f979749f61979bced885063efd189385a66d95b3
George Salu <george.salu10@gmail.com>
2025-12-31 10:20:13 -0300

    algoritmo-logica - secao 5 - exercicio 44 - solucao

--- algoritmo-logica/5_listas/21_exercicio_44/app.py
+++ algoritmo-logica/5_listas/21_exercicio_44/app.py
@@ -3,3 +3,15 @@ crie um programa que busca por um elemento
 o metodo de loop precisa ser for
 imprima a mensagem com o elemento encontrado
 """
+numeros = [100,200,300,400,500,600]
+
+qual_numero_quer_encontrar = int(input("qual numero quer encontrar? "))
+
+encontrado = False
+for n in numeros:
+    if n == qual_numero_quer_encontrar:
+        print("O numero %d foi encontrado" % n)
+        encontrado = True
+
+if encontrado == False:
+    print("nao contramos os numero %d"%qual_numero_quer_encontrar)
