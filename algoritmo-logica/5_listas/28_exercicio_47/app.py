fc52a08348896ac48d00f94e5dcbd77ca0e65f54
George Salu <george.salu10@gmail.com>
2026-01-01 11:25:28 -0300

    algoritmo-logica - secao 5 - corrige enunciado exericico 47

--- algoritmo-logica/5_listas/28_exercicio_47/app.py
+++ algoritmo-logica/5_listas/28_exercicio_47/app.py
@@ -1,5 +1,5 @@
 """
 crie uma lista com outras listas dentro dela
-uma ideia seria: produtos com nome, cor e preco
+uma ideia seria: produtos com nome, cor e preço
 imprima cada um dos itens da listas que estão inseridas na lista pai
 """
