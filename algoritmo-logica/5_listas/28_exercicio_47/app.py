7406c0d9710f110581adcd780b765c3293416616
George Salu <george.salu10@gmail.com>
2026-01-01 11:22:34 -0300

    algoritmo-logica - secao 5 - exericico 47

--- algoritmo-logica/5_listas/28_exercicio_47/app.py
+++ algoritmo-logica/5_listas/28_exercicio_47/app.py
@@ -1,5 +1,5 @@
 """
-crie uma lista com listas dentro dela
+crie uma lista com outras listas dentro dela
 uma ideia seria: produtos com nome, cor e preco
 imprima cada um dos itens da listas que estão inseridas na lista pai
 """
