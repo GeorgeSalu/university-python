948714a32ec102a21301de06011a127ae8224e55
George Salu <george.salu10@gmail.com>
2026-01-02 12:20:36 -0300

    algoritmo-logica - secao 2 - adicionando enunciado no exercicio 8

--- algoritmo-logica/2_variaveis/17_exercicio_8/app.py
+++ algoritmo-logica/2_variaveis/17_exercicio_8/app.py
@@ -1,3 +1,9 @@
+"""
+crie uma varivel salario com um valor de sua escolha
+crie uma variavel aumento com um valor tambem de sua escolha
+atribua a variavel novo_salario o valor do salario com o aumento
+exiba o resultado na tela
+"""
 salario = 1800
 aumento = 400
 
