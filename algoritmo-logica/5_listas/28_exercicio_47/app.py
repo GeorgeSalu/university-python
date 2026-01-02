22c686e490d4e3077e04f0274dc8e05b3966de5a
George Salu <george.salu10@gmail.com>
2026-01-02 09:58:45 -0300

    algoritmo-logica - secao 5 - exercicio 47 solucao

--- algoritmo-logica/5_listas/28_exercicio_47/app.py
+++ algoritmo-logica/5_listas/28_exercicio_47/app.py
@@ -3,3 +3,13 @@ crie uma lista com outras listas dentro dela
 uma ideia seria: produtos com nome, cor e preço
 imprima cada um dos itens da listas que estão inseridas na lista pai
 """
+produtos_a = ["camisa","azul",25.44]
+produtos_b = ["bermuda","marrom",125.44]
+produtos_c = ["casaco", "bege", 11]
+
+lista_produtos = [produtos_a,produtos_b,produtos_c]
+
+print(lista_produtos)
+
+for produto in lista_produtos:
+    print("O produto é: %s e tem a cor %s e seu preco é : %.2f"%(produto[0],produto[1],produto[2]))
