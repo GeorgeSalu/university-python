0ae6b8c8952d40e7c3b0e61a34ca6e2b2c581956
George Salu <george.salu10@gmail.com>
2026-01-02 12:04:55 -0300

    algoritmo-logica - secao 5 - iterando em tuplas

---
+++ algoritmo-logica/5_listas/35_iterando_em_tuplas/app.py
@@ -0,0 +1,11 @@
+tuplas = ("matheus","pedro","joao","jose")
+
+for nome in tuplas:
+    print(nome)
+
+print("-------------------------------")
+
+i = 0
+while i < len(tuplas):
+    print(tuplas[i])
+    i += 1
