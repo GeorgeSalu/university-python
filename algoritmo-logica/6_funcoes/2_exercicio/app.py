1d6cf9b17b7008c9da981ba69f48850e314b4524
George Salu <george.salu10@gmail.com>
2026-01-02 15:06:39 -0300

    algoritmo-logica - secao 6 - exercicio 50

---
+++ algoritmo-logica/6_funcoes/2_exercicio/app.py
@@ -0,0 +1,9 @@
+"""
+crie uma funcao que exibe a multiplicacao de dois numeros
+os numeros devem ser passaos por parametro
+"""
+def multiplicacao(a, b):
+    print(a * b)
+
+multiplicacao(2,4)
+multiplicacao(3,4)
