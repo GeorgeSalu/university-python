8304d957daa946dcd684f07384904be6657b544c
George Salu <george.salu10@gmail.com>
2026-01-02 10:57:01 -0300

    algoritmo-logica - secao 5 - exercicio 49

---
+++ algoritmo-logica/5_listas/33_exercicio_49/app.py
@@ -0,0 +1,26 @@
+"""
+crie um dicionario chamado carro com os seguintes valores
+pneus: 4,
+portas: 2,
+motor: 1,
+janelas: 4,
+adicione a chave teto sonar com valor 1
+delete motor e jenala, imprima o dicionario novamente
+"""
+carro = {
+    "pneus": 4,
+    "portas": 2,
+    "motor": 1,
+    "janelas": 4,
+}
+
+print(carro)
+
+carro["teto solar"] = 1
+print(carro)
+print(carro["teto solar"])
+
+del carro["motor"]
+del carro["janelas"]
+
+print(carro)
