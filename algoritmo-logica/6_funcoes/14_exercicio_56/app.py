56f7263882f87089d9faac5e2fd23a9108c4d949
George Salu <george.salu10@gmail.com>
2026-01-04 13:31:05 -0300

    algoritmo-logica - secao 6 - exercicio 56 - enunciado

---
+++ algoritmo-logica/6_funcoes/14_exercicio_56/app.py
@@ -0,0 +1,5 @@
+"""
+crie uma funcao que recebe outra como parametro
+a que vai receber parametros deve receber um nome,uma idade e uma profisao
+a funcao de argumentos deve apresentar estes dados em uam string dinamica
+"""
