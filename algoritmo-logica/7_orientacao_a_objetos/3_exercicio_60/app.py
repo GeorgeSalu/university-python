65a7c0aa5b0c9eb8c9f242bc1d2260043273b997
George Salu <george.salu10@gmail.com>
2026-01-05 15:13:33 -0300

    algoritmo-logica - secao 7 - exercicio 60 solucao

--- algoritmo-logica/7_orientacao_a_objetos/3_exercicio_60/app.py
+++ algoritmo-logica/7_orientacao_a_objetos/3_exercicio_60/app.py
@@ -2,3 +2,20 @@
 crie uma classe Carro com as propriedades de portas, motor se tem teto solar, marca, preço
 crie objetos preenchendo os valores das propriedades
 """
+class Carro:
+    def __init__(self, portas,motor,teto_solar,marca,preco):
+        self.portas = portas
+        self.motor = motor
+        self.teto_solar = teto_solar
+        self.marca = marca
+        self.preco = preco
+
+polo = Carro(4,1.0,True,"VW",70000)
+
+print(polo.preco)
+print(polo.marca)
+
+ferrari = Carro(2,3.0,True,"Ferrari",250000)
+
+print(ferrari.preco)
+print(ferrari.marca)
