29a8dbef68f444fb5fd5a38a4eeb577b1886520f
George Salu <george.salu10@gmail.com>
2026-01-05 17:36:05 -0300

    algoritmo-logica - secao 7 - exercicio 62 solucao

--- algoritmo-logica/7_orientacao_a_objetos/6_exercicio_62/app.py
+++ algoritmo-logica/7_orientacao_a_objetos/6_exercicio_62/app.py
@@ -3,3 +3,25 @@ crie um sistema que simule um banco, a classe deve ter um metodo de saque, depos
 propriedades nome do proprietario da conta, saldo
 realizar saques,deposito e transferencia entre contas
 """
+class Banco():
+    def __init__(self, nome,saldo):
+        self.nome = nome
+        self.saldo = saldo
+
+    def deposito(self,valor):
+        self.saldo += valor
+
+    def saque(self,valor):
+        self.saldo -= valor
+
+    def transferencia(self,outra_conta,valor):
+        self.saldo += valor
+        outra_conta.saldo += valor
+
+
+
+conta_matheus = Banco('Matheus',1000)
+print(conta_matheus.nome)
+
+conta_matheus.deposito(1000)
+print(conta_matheus.saldo)
