657a74389272c1da5fc92c38f3c6a0bff6907e3c
George Salu <george.salu10@gmail.com>
2026-01-05 16:21:45 -0300

    algoritmo-logica - secao 7 - exercicio 62 enunciado

---
+++ algoritmo-logica/7_orientacao_a_objetos/6_exercicio_62/app.py
@@ -0,0 +1,5 @@
+"""
+crie uma sistema que simule um banco, a classe deve ter um metodo de saque, deposito e transaferencia
+propriedades nome do proprietarioda conta, saldo
+realizar saques,deposito e transferencia entre contas
+"""
