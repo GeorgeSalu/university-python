2f0190a9f0013fe9a5416f1afa5bc8372cdd6440
George Salu <george.salu10@gmail.com>
2026-01-05 09:29:21 -0300

    algoritmo-logica - secao 6 - exercicio 58

---
+++ algoritmo-logica/6_funcoes/19_exercicio_58/app.py
@@ -0,0 +1,11 @@
+"""
+crie um modulo que faz operacoes matematics como:soma,subtracao,divisao e multiplicacao
+importe o modulo em outro arquivo e use as funcoes
+"""
+import matematica
+
+soma = matematica.soma(2,3)
+multiplicacao = matematica.multiplicacao(2,3)
+
+print(soma)
+print(multiplicacao)
