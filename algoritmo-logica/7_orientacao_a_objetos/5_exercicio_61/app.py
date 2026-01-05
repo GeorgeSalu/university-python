d1942209da6e02c991108cc1327a4c3de4a39a2a
George Salu <george.salu10@gmail.com>
2026-01-05 15:19:14 -0300

    algoritmo-logica - secao 7 - exercicio 61 enunciado

---
+++ algoritmo-logica/7_orientacao_a_objetos/5_exercicio_61/app.py
@@ -0,0 +1,5 @@
+"""
+crie uma classe Carro com as propriedades marca, valor, numero de portas e tanque de gasolina
+crie um metodo que abastece o tanque de gasolina
+crie um metodo dirigir que remove gasolina baseado em uma km rodada
+"""
