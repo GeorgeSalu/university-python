87982f5bbdebf09e8aae48b5568bf97dba194581
George Salu <george.salu10@gmail.com>
2026-01-06 10:07:04 -0300

    algoritmo-logica - secao 7 - exercicio 63 solucao

--- algoritmo-logica/7_orientacao_a_objetos/9_exercicio_63/app.py
+++ algoritmo-logica/7_orientacao_a_objetos/9_exercicio_63/app.py
@@ -4,3 +4,26 @@ crie uma classe de Super heroi que herda as caracteristicas de pessoa
 crie poderes especiais para o super heroi
 test as duas classes criando novos objetos
 """
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def falar(self):
+        print("ola galera")
+
+
+class Superheroi(Pessoa):
+    def __init__(self,nome,idade,super_podere):
+        super().__init__(nome,idade)
+        self.super_podere = super_podere
+
+    def utilizar_super_pode(self):
+        print("o heroi utilizou o %s"%self.super_podere)
+
+joao = Pessoa("joao", 21)
+joao.falar()
+
+matheus = Superheroi("matheusa", 21, "raio laser")
+matheus.falar()
+matheus.utilizar_super_pode()
