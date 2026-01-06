c9b000ae9fcc622bec13a3b320faafe0354ee387
George Salu <george.salu10@gmail.com>
2026-01-06 11:50:28 -0300

    algoritmo-logica - secao 7 - encapsulamento

--- algoritmo-logica/7_orientacao_a_objetos/12_encapsulamento/app.py
+++ algoritmo-logica/7_orientacao_a_objetos/12_encapsulamento/app.py
@@ -1,6 +1,6 @@
 class Aviao:
     __asas = 2
-    __qtd_passageiro = 100
+    __qtd_passageiros = 100
 
     def __init__(self, marca):
         self.marca = marca
@@ -8,10 +8,10 @@ class Aviao:
     def mostrar_asas(self):
         print("o aviao tem %d asas "%self.__asas)
 
-    def quantidade_passageiro(self):
-        print("a quantidade de passageiros é %d "%self.__qtd_passageiro)
+    def quantidade_passageiros(self):
+        print("a quantidade de passageiros é %d "%self.__qtd_passageiros)
 
 aviao = Aviao("Aviao hora de codar")
 print(aviao.marca)
 aviao.mostrar_asas()
-aviao.quantidade_passageiro()
+aviao.quantidade_passageiros()
