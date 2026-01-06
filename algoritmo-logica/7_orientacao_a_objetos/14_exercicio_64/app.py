34cfd3f1e6a775b74532c34b6986e7ee4223a291
George Salu <george.salu10@gmail.com>
2026-01-06 15:08:17 -0300

    algoritmo-logica - secao 7 - exercicio 64 enunciado

---
+++ algoritmo-logica/7_orientacao_a_objetos/14_exercicio_64/app.py
@@ -0,0 +1,6 @@
+"""
+crie uma classe Mamifero com propriedades de mamifero
+Herde os detalhes desta classe e crie novas para Cachorro, Gato e outros que quiser
+crie objetos destas classes que herdam de mamifero
+exiba as pripriedades e metodos ne tela
+"""
