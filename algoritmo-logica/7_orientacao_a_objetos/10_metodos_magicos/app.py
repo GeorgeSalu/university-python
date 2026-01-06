38c7a5954a0353b49a4dd2f15e227c71d7a9b433
George Salu <george.salu10@gmail.com>
2026-01-06 10:14:52 -0300

    algoritmo-logica - secao 7 - metodos magicos

--- algoritmo-logica/7_orientacao_a_objetos/10_metodos_magicos/app.py
+++ algoritmo-logica/7_orientacao_a_objetos/10_metodos_magicos/app.py
@@ -5,7 +5,8 @@ class Pessoa:
         self.profissao = profissao
 
     def __str__(self):
-        return f"Nome: {self.nome}, Idade: {self.idade}"
+        # return f"Nome: {self.nome}, Idade: {self.idade}"
+        return "O nome do usuario %s e tem %d anor e é um %s"%(self.nome, self.idade, self.profissao)
 
 matheus = Pessoa("Matheus", 20, "programador")
 print(matheus.__str__())
