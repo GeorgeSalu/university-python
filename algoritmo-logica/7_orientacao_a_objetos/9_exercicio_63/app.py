109400a0533a951e9110c46192bfcaa900a21ea8
George Salu <george.salu10@gmail.com>
2026-01-06 09:12:30 -0300

    algoritmo-logica - secao 7 - exercicio 63 enunciado

---
+++ algoritmo-logica/7_orientacao_a_objetos/9_exercicio_63/app.py
@@ -0,0 +1,6 @@
+"""
+crie uma classe Pessoa com nome e idade
+crie uma classe de Super heroi que herda as caracteristicas de pessoa
+crie poderes especiais para o super heroi
+test as duas classes criando novos objetos
+"""
