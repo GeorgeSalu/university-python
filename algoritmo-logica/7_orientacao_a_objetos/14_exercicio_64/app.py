8c3e36278e54ca94333beaa8f0fe0609318b7287
George Salu <george.salu10@gmail.com>
2026-01-06 15:11:53 -0300

    algoritmo-logica - secao 7 - exercicio 64 solucao

--- algoritmo-logica/7_orientacao_a_objetos/14_exercicio_64/app.py
+++ algoritmo-logica/7_orientacao_a_objetos/14_exercicio_64/app.py
@@ -4,3 +4,35 @@ Herde os detalhes desta classe e crie novas para Cachorro, Gato e outros que qui
 crie objetos destas classes que herdam de mamifero
 exiba as pripriedades e metodos ne tela
 """
+class Mamifero:
+    def __init__(self, patas, orelhas):
+        self.patas = patas
+        self.orelhas = orelhas
+
+    def andar(self):
+        print("o mamifero andou")
+
+class Cachorro(Mamifero):
+    def __init__(self, patas, orelhas,cor_do_pelo):
+        super().__init__(patas, orelhas)
+        self.cor_do_pelo = cor_do_pelo
+
+    def latir(self):
+        print("au au")
+
+
+class Gato(Mamifero):
+    def __init__(self, patas, orelhas,listras):
+        super().__init__(patas, orelhas)
+        self.listras = listras
+
+    def miar(self):
+        print("miau")
+
+turca = Cachorro(4,2,"preto")
+turca.andar()
+turca.latir()
+
+bartolomeu = Gato(4,2,False)
+bartolomeu.andar()
+bartolomeu.miar()
