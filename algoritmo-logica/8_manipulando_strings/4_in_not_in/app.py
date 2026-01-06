79490a4cb2435f0ef933e9eb357ac3dd479429d9
George Salu <george.salu10@gmail.com>
2026-01-06 18:16:12 -0300

    algoritmo-logica - secao 8 - checando se existe uma palavra na string

---
+++ algoritmo-logica/8_manipulando_strings/4_in_not_in/app.py
@@ -0,0 +1,9 @@
+frase = "esta é a frase que queremos achar uma palavra"
+
+print(frase)
+
+if "frase" in frase:
+    print("encontramos a palavra frase")
+
+if "segunda-feira" not in frase:
+    print("não contramos o segunda-feira")
