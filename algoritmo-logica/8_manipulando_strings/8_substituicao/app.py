254bd25d545acac2e5cef5a9a92d2ab63e59b37d
George Salu <george.salu10@gmail.com>
2026-01-08 12:24:51 -0300

    algoritmo-logica - secao 8 - adiciona comentario sobre os paramentros da funcao replace

--- algoritmo-logica/8_manipulando_strings/8_substituicao/app.py
+++ algoritmo-logica/8_manipulando_strings/8_substituicao/app.py
@@ -3,4 +3,5 @@ frase = "o rato roeu a roupa do rei de roma"
 print(frase.replace("rato","leao"))
 
 frase2 = "vou testar o replace em duas palavras testar"
+# o terceiro paramentro no replace se refe a a um parametro que limita a quantidade de subtiuicoes
 print(frase2.replace("testar","trocou", 2))
