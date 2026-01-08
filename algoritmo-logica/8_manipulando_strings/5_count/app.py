53eb025912bd23e12ca544dc0e79825c29be47c5
George Salu <george.salu10@gmail.com>
2026-01-08 10:15:10 -0300

    algoritmo-logica - secao 8 - contagem numero de ocorrencias

---
+++ algoritmo-logica/8_manipulando_strings/5_count/app.py
@@ -0,0 +1,8 @@
+frase = "esta é a frase que vamos checar as ocorrrencias da palavra frase"
+
+print(frase.count("frase"))
+
+if frase.count("frase") == 2:
+    print("a contagem esta correta")
+
+print(frase.count("ola"))
