99091ed5eb29961a9937e30a820d664d454911a5
George Salu <george.salu10@gmail.com>
2026-01-08 12:03:10 -0300

    algoritmo-logica - secao 8 - trocando palavras em strings

---
+++ algoritmo-logica/8_manipulando_strings/8_substituicao/app.py
@@ -0,0 +1,6 @@
+frase = "o rato roeu a roupa do rei de roma"
+
+print(frase.replace("rato","leao"))
+
+frase2 = "vou testar o replace em duas palavras testar"
+print(frase2.replace("testar","trocou", 2))
