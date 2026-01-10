4ac8b5aad70bedc131b62e52bed2dcbc915e144a
George Salu <george.salu10@gmail.com>
2026-01-10 15:29:31 -0300

    geek-universisty - pep8 #1

---
+++ geek-university/1_introducao/pep8.py
@@ -0,0 +1,14 @@
+"""
+PEP8 - Python Enhancement Proposals
+
+São Propostas de melhorias para a linguagem Python
+The Zen of Python
+import this
+A ideia da PEP8 é que possamos escrever codigos Python de forma Pythonica
+[1] - Utilize Camel Case para nomes de classes
+[2] - utlizar nomes em minusculos, separados por underline para funcoes ou variaveis
+[3] - utilizar 4 espacos para identação
+[4] - linhas em branco
+    - separar funcoes e definicoes de classe com duas linha em branco
+    - metodos dentro de uma classe devem ser separados com uma unica linha em branco
+"""
