cb2e6259e9a7a2f002bf71f357d9ee30aee7433c
George Salu <george.salu10@gmail.com>
2026-01-10 19:18:17 -0300

    geek-universisty - sessao 2 - dir e help

--- geek-university/2_introducao/dir_help.py
+++ geek-university/2_introducao/dir_help.py
@@ -1,8 +1,8 @@
 """
 Utilitarios Python para auxiliar na programação
 
-dir -> apresenta todos os atributos/propriedades e funcções/metodos disponiveis para determinado tipo de dado ou variavel.
+dir -> apresenta todos os atributos/propriedades e funções/metodos disponiveis para determinado tipo de dado ou variavel.
     dir(tipo de dado/variavel)
-help -> apresenta a documentacao/como utilizar os atributos/propriedades e funcções/metodos disponiveis para determinado tipo de dado ou variavel.
+help -> apresenta a documentacao/como utilizar os atributos/propriedades e funções/metodos disponiveis para determinado tipo de dado ou variavel.
     help(tipo de dado/variavel)
 """
