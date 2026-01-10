22a340ad06f68e1172cd723e3c689a5e790d1099
George Salu <george.salu10@gmail.com>
2026-01-09 21:16:45 -0300

    algoritmo-logica - secao 9 - cria metodo exibir_forca

--- algoritmo-logica/9_projeto_jogo_da_forca/app.py
+++ algoritmo-logica/9_projeto_jogo_da_forca/app.py
@@ -6,5 +6,78 @@ def selecionar_palavra():
     palavra = random.choice(palavras)
     return palavra.upper()
 
-teste = selecionar_palavra()
-print(teste)
+# status do jogo
+def exibir_forca(tentativas):
+    estagios = [  # Fim de jogo
+        """
+            --------
+            |      |
+            |      O
+            |     \\|/
+            |      |
+            |     / \\
+            -
+        """,
+        # Falta 1 tentativa
+        """
+            --------
+            |      |
+            |      O
+            |     \\|/
+            |      |
+            |     / 
+            -
+        """,
+        # Faltam 2 tentativas
+        """
+            --------
+            |      |
+            |      O
+            |     \\|/
+            |      |
+            |      
+            -
+        """,
+        # Faltam 3 tentativas
+        """
+            --------
+            |      |
+            |      O
+            |     \\|
+            |      |
+            |     
+            -
+        """,
+        # Faltam 4 tentativas
+        """
+            --------
+            |      |
+            |      O
+            |      |
+            |      |
+            |     
+            -
+        """,
+        # Faltam 5 tentativas
+        """
+            --------
+            |      |
+            |      O
+            |    
+            |      
+            |     
+            -
+        """,
+        # Estado inicial
+        """
+            --------
+            |      |
+            |      
+            |    
+            |      
+            |     
+            -
+        """
+    ]
+
+    return estagios[tentativas]
