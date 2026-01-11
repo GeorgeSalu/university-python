1265e2cba115599a909e4a64604837f6df2775a0
George Salu <george.salu10@gmail.com>
2026-01-11 13:56:40 -0300

    geek-universisty - sessao 3 - tipo string #01

---
+++ geek-university/3_variaveis_tipos_de_dados/tipo_string.py
@@ -0,0 +1,8 @@
+"""
+tipo string
+
+em python, um dado é considerado do tipo string sempre que
+- estiver entre aspas simples
+- estiver entre aspas duplas
+- estiver entre aspas simples triplas
+"""
