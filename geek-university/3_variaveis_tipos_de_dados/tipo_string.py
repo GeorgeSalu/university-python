a933ee117b2081f629ff13a4f36215735236e0e2
George Salu <george.salu10@gmail.com>
2026-01-11 14:51:45 -0300

    geek-universisty - sessao 3 - tipo string #02

--- geek-university/3_variaveis_tipos_de_dados/tipo_string.py
+++ geek-university/3_variaveis_tipos_de_dados/tipo_string.py
@@ -6,3 +6,19 @@ em python, um dado é considerado do tipo string sempre que
 - estiver entre aspas duplas
 - estiver entre aspas simples triplas
 """
+nome1 = 'geek university'
+print(nome1)
+print(type(nome1))
+
+nome2 = "geek university"
+print(nome2)
+print(type(nome2))
+
+nome3 = 'geek \nuniversity'
+print(nome3)
+print(type(nome3))
+
+nome4 = """geek 
+university"""
+print(nome4)
+print(type(nome4))
