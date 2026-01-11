ab0d1526c02704f783c7fab582317693f674b3a1
George Salu <george.salu10@gmail.com>
2026-01-10 23:12:57 -0300

    geek-universisty - sessao 3 - tipo float

---
+++ geek-university/3_variaveis_tipos_de_dados/tipo_float.py
@@ -0,0 +1,16 @@
+"""
+tipo float
+tipo real, decimal
+casas decimais
+obs: o separador de casas decimais na programacao é o ponto e não a virgula
+"""
+valor = 1.44
+print(valor)
+print(type(valor))
+
+"""
+obs: ao converter valores float para inteiros, nos perdemos a precisao
+"""
+res = int(valor)
+print(res)
+print(type(res))
