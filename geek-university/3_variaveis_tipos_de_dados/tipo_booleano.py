9a2b93ed5e5c230c5446428af65b82e21d5560a6
George Salu <george.salu10@gmail.com>
2026-01-11 13:51:43 -0300

    geek-universisty - sessao 3 - tipo booleano #02

--- geek-university/3_variaveis_tipos_de_dados/tipo_booleano.py
+++ geek-university/3_variaveis_tipos_de_dados/tipo_booleano.py
@@ -11,14 +11,14 @@ print(ativo)
 
 # operacoes basicas
 """
-negacao not
+negacao (not)
 fazendo a negação, se o valor boolena for verdadeiro o resultado sera falso
 se for falso o resultado sera verdadeiro, ou seja sempre o contrario
 """
 print(not ativo)
 logado = False
 """
-ou or
+ou (or)
 é uma operacao binaria, ou seja depende de dois valores, ou um ou outro deve ser verdadeiro
 True or True -> True
 False or False -> True
@@ -26,3 +26,13 @@ False or True -> True
 False or False -> False 
 """
 print(ativo or logado)
+
+"""
+e (and)
+também é uma operacao binaria, ou seja depende de dois valores. ambos os valores devem ser verdadeiro
+
+True and True -> True
+True and False -> False
+False and True -> False
+False and False -> False
+"""
