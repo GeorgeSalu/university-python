cbd49c9ce3a0514c17a3bbe44f9a819008476287
George Salu <george.salu10@gmail.com>
2026-01-11 13:45:43 -0300

    geek-universisty - sessao 3 - tipo booleano #01

---
+++ geek-university/3_variaveis_tipos_de_dados/tipo_booleano.py
@@ -0,0 +1,28 @@
+"""
+tipo booleano
+algebra booleano, criada por george boole
+2 constantes, verdadeiro e falso
+True -> Vrdadeiro
+False -> Falso
+obs: sempre com a inicial maiuscula
+"""
+ativo = False
+print(ativo)
+
+# operacoes basicas
+"""
+negacao not
+fazendo a negação, se o valor boolena for verdadeiro o resultado sera falso
+se for falso o resultado sera verdadeiro, ou seja sempre o contrario
+"""
+print(not ativo)
+logado = False
+"""
+ou or
+é uma operacao binaria, ou seja depende de dois valores, ou um ou outro deve ser verdadeiro
+True or True -> True
+False or False -> True
+False or True -> True
+False or False -> False 
+"""
+print(ativo or logado)
