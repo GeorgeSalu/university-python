bac7abe670916c637abe2dc771699098dffc9355
George Salu <george.salu10@gmail.com>
2026-01-12 14:40:05 -0300

    geek-universisty - sessao 4 - entendendo e explorando os ranges #02

--- geek-university/5_estrutura_repeticao/entendendo_range.py
+++ geek-university/5_estrutura_repeticao/entendendo_range.py
@@ -9,8 +9,21 @@ mas sim de maneira especifica
 formas gerais:
 range(valor_de_parada)
 obs: valor_de_parada não inclusive (inicio padrao 0, e passos de 1 em 1)
-"""
 
 # forma 1
+range(valor_de_parada)
 for num in range(11):
     print(num)
+
+# forma 2
+range(valor_de_inicio,valor_de_parada)
+for num in range(2,11):
+    print(num)
+
+# forma 3
+range(valor_de_inicio,valor_de_parada, passo)
+for num in range(1,11,2):
+    print(num)
+
+"""
+
