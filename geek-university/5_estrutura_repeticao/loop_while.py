27e3ce22353bb920a8b1f74bddf6d68349716de7
George Salu <george.salu10@gmail.com>
2026-01-12 14:55:31 -0300

    geek-universisty - sessao 4 - loop while

---
+++ geek-university/5_estrutura_repeticao/loop_while.py
@@ -0,0 +1,15 @@
+"""
+loop while
+
+forma geral
+while expressao_booleana:
+    // execução do loop
+
+o bloco do while sera repetido enquanto a expressao booleana for verdadeira
+expressao booleana é toda expressao onde o resultado é verdadeiro ou falsop
+obs: em um loop while, é importante que cuidemos do criterio de parada
+"""
+numero = 1
+while numero <= 10:
+    print(numero)
+    numero += 1
