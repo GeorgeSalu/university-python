570e37b0fe44fa35d837014205e1c76ccce9c1df
George Salu <george.salu10@gmail.com>
2026-01-12 09:35:42 -0300

    geek-universisty - sessao 3 - exercicio 2

---
+++ geek-university/3_variaveis_tipos_de_dados/exercicio_2.py
@@ -0,0 +1,10 @@
+"""
+2. Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles.
+"""
+numero1: int = int(input("informe o primeiro inteiro: "))
+numero2: int = int(input("informe o segundo inteiro: "))
+numero3: int = int(input("informe o terceiro inteiro: "))
+
+soma : int = numero1 + numero2 + numero3
+
+print(f"a soma dos numeros inteiros {numero1} e {numero2} e {numero3} é {soma}")
