b3533fc422219fbbdd2b505262d23a35f1abf360
George Salu <george.salu10@gmail.com>
2026-01-12 14:55:38 -0300

    geek-universisty - sessao 4 - loop while

--- geek-university/5_estrutura_repeticao/loop_while.py
+++ geek-university/5_estrutura_repeticao/loop_while.py
@@ -6,7 +6,7 @@ while expressao_booleana:
     // execução do loop
 
 o bloco do while sera repetido enquanto a expressao booleana for verdadeira
-expressao booleana é toda expressao onde o resultado é verdadeiro ou falsop
+expressao booleana é toda expressao onde o resultado é verdadeiro ou falso
 obs: em um loop while, é importante que cuidemos do criterio de parada
 """
 numero = 1
