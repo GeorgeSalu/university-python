9cdad755ed4165e90a7155639468a0e41968f3b0
George Salu <george.salu10@gmail.com>
2026-01-12 11:25:37 -0300

    geek-universisty - sessao 4 - exercicio 3 (solucao)

--- geek-university/4_estruturas_condicionais/exercicio_3.py
+++ geek-university/4_estruturas_condicionais/exercicio_3.py
@@ -1,3 +1,9 @@
 """
 3. Faça um programa que recebe um número inteiro e informe se este número é par ou ímpar.
 """
+numero: int = int(input("informe um numero inteiro: "))
+
+if numero % 2 == 0:
+    print(f"o numero {numero} eh par")
+else:
+    print(f"o numero é impar")
