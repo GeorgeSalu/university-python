907e5f9fc4e14047e63d43b50b09e75df495f328
George Salu <george.salu10@gmail.com>
2026-01-12 11:23:48 -0300

    geek-universisty - sessao 4 - exercicio 2 (solucao)

--- geek-university/4_estruturas_condicionais/exercicio_2.py
+++ geek-university/4_estruturas_condicionais/exercicio_2.py
@@ -3,3 +3,11 @@
 a raiz quadrada do número e apresente-a. Se o número for negativo, mostre uma mensagem dizendo que o
 número é inválido.
 """
+import math
+
+numero: int = int(input("informe um numero inteiro: "))
+
+if numero > 0:
+    print(f"a raiz quadrada de {numero} e igual a {math.sqrt(numero)}")
+else:
+    print(f"o numero {numero} é invalido")
