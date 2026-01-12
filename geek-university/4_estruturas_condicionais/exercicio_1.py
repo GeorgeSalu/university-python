03113e16a0ae6f16eba3eb7543a3d1c7546104ca
George Salu <george.salu10@gmail.com>
2026-01-12 11:11:20 -0300

    geek-universisty - sessao 4 - exercicio 1 (solucao)

--- geek-university/4_estruturas_condicionais/exercicio_1.py
+++ geek-university/4_estruturas_condicionais/exercicio_1.py
@@ -1,3 +1,12 @@
 """
 1. Faça um programa que receba dois números inteiros e mostre qual deles é o maior.
 """
+numero1 = int(input("informe o primeiro numero: "))
+numero2 = int(input("informe o segundo numero: "))
+
+if numero1 > numero2:
+    print(f"o numero {numero1} eh maior que o numero {numero2}")
+elif numero1 == numero2:
+    print(f"os numeros {numero1} e {numero2} são iguais")
+else:
+    print(f"o numero {numero2} eh maior que {numero1}")
