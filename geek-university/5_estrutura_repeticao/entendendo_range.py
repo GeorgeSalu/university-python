3ddd22b7b405899cfa7399daf0f652e29cffe9d5
George Salu <george.salu10@gmail.com>
2026-01-12 14:21:24 -0300

    geek-universisty - sessao 4 - entendendo e explorando os ranges #01

---
+++ geek-university/5_estrutura_repeticao/entendendo_range.py
@@ -0,0 +1,16 @@
+"""
+Ranges
+- precisamos conhecer o loop for para usar os ranges
+- precisamos conhecer o range para trabalhar melhor com loops for
+
+Ranges são utilizados para gerar sequencias numericas, não de forma aletoria
+mas sim de maneira especifica
+
+formas gerais:
+range(valor_de_parada)
+obs: valor_de_parada não inclusive (inicio padrao 0, e passos de 1 em 1)
+"""
+
+# forma 1
+for num in range(11):
+    print(num)
