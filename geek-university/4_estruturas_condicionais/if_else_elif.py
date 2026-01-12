de6924b87a186ec79b5161237b55fa8b642ef885
George Salu <george.salu10@gmail.com>
2026-01-12 10:07:04 -0300

    geek-universisty - sessao 4 - if else e elif

---
+++ geek-university/4_estruturas_condicionais/if_else_elif.py
@@ -0,0 +1,14 @@
+"""
+estruturas condicionais
+if(se), else, elif
+"""
+idade = 18
+
+if idade < 18:
+    print("menor de idade")
+elif idade == 18:
+    print("tem 18 anos")
+elif idade == 26:
+    print("tem 26 anos")
+else:
+    print("maior de idade")
