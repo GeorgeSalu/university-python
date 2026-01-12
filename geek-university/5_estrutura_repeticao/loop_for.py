6047837b41e5923fcddb2c9b11c652b5b74a63e9
George Salu <george.salu10@gmail.com>
2026-01-12 11:30:04 -0300

    geek-universisty - sessao 4 - loop for #01

---
+++ geek-university/5_estrutura_repeticao/loop_for.py
@@ -0,0 +1,18 @@
+"""
+loop for
+"""
+nome = "geek university"
+lista = [1, 2, 3, 4, 5]
+numeros = range(1,10)
+
+# interando em uma string
+for letra in nome:
+    print(letra)
+
+# interando em uma lista
+for numero in lista:
+    print(numero)
+
+# interando em um range
+for numero in numeros:
+    print(numero)
