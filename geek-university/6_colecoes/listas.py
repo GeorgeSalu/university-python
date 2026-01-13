de7b71d50f116e889a57b47eadaad2123eb1f3bd
George Salu <george.salu10@gmail.com>
2026-01-13 10:25:11 -0300

    geek-universisty - sessao 6 - listas #01

---
+++ geek-university/6_colecoes/listas.py
@@ -0,0 +1,10 @@
+"""
+listas
+listas em python funcionam como vetores/matrizes (arrays) em outroas linguagens, com a diferença
+de serem dinamico e também podermos ter qualquer tipo de dado
+
+linguagens c/java: arrays
+ - possuem tamanho e tipo de dado fixo;
+ ou seja, nestas linguagens se você criar um array do tipo int e com tamanho 5, este array
+ sera sempre do tipo inteiro e podera sempre ter sempre no maximo 5 valores
+"""
