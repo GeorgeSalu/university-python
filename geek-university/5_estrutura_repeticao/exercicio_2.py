ed5aeedb305cac2d9e7eb41aa69c71cb3f659be3
George Salu <george.salu10@gmail.com>
2026-01-13 09:37:12 -0300

    geek-universisty - sessao 4 - exercicio 2 (solucao)

--- geek-university/5_estrutura_repeticao/exercicio_2.py
+++ geek-university/5_estrutura_repeticao/exercicio_2.py
@@ -2,7 +2,7 @@
 2. Faça um programa que utilize o comando while para mostrar na tela uma contagem regressiva, iniciando
 em 10 e terminando em 0. Mostre também uma mensagem “FIM!” após a contagem.
 """
-contador = 10
+contador: int = 10
 
 while contador >= 0:
     print(contador)
