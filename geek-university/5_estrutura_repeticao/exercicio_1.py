b2b612fd2142e501ee79d8192260ff5106b036f8
George Salu <george.salu10@gmail.com>
2026-01-12 21:46:07 -0300

    geek-universisty - sessao 4 - exercicio 1 (solucao)

--- geek-university/5_estrutura_repeticao/exercicio_1.py
+++ geek-university/5_estrutura_repeticao/exercicio_1.py
@@ -1,3 +1,12 @@
 """
 1. Faça um programa que determine e mostre os cinco primeiros múltiplos de 3, considerando números maiores que 0.
 """
+contador = 0
+indice = 1
+
+while contador < 5:
+    if indice % 3 == 0:
+        print(f"o numero {indice} eh multiplo de 3")
+        contador += 1
+    indice += 1
+    
