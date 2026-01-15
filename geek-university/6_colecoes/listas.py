92336bb0ea9dd19a51b6ba6515588b379f27ef08
George Salu <george.salu10@gmail.com>
2026-01-14 22:05:26 -0300

    geek-universisty - sessao 6 - listas #02

--- geek-university/6_colecoes/listas.py
+++ geek-university/6_colecoes/listas.py
@@ -7,4 +7,16 @@ linguagens c/java: arrays
  - possuem tamanho e tipo de dado fixo;
  ou seja, nestas linguagens se você criar um array do tipo int e com tamanho 5, este array
  sera sempre do tipo inteiro e podera sempre ter sempre no maximo 5 valores
+
+ Já em python
+ - dinamico: não possuem tamanho fixo; ou seja, podemos criar a lista e simplesmente ir adicionando elementos
+ - Qualquer tipo de dado: não possuem tipo de dado fixo: ou seja, podemos colocar qualquer tipo de dado
+
+ as listas em python são representass por colchetes: []
 """
+type([])
+lista1 = [1,2,3,4,5,4,3,22,11]
+lista2 = ['g','e','e','k',' ','u','n','i','v','e','r','s','i','t','y']
+lista3 = []
+lista4 = list(range(11))
+lista5 = list('geek university')
