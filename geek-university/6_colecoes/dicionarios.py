da66b0a5d270b31ee6579c98e0ce9ed44b458150
George Salu <george.salu10@gmail.com>
2026-01-16 12:33:26 -0300

    geek-universisty - sessao 6 - dicionarios #02

--- geek-university/6_colecoes/dicionarios.py
+++ geek-university/6_colecoes/dicionarios.py
@@ -12,6 +12,24 @@ obs: sobre dicionarios
  - tanto chave quanto valor podem ser de qualquer tipo de dado
  - podemos misturar tipos de dados
 """
+#  criacao de dicionarios
+# forma 1 (mais comum)
 paises = {'br': 'brasil', 'eua': 'estados unidos', 'py':'paraguai'}
 print(paises)
 print(type(paises))
+
+# forma 2 (menos comum)
+paises = dict(br='brasil', eua='estados unidos', py='paraguai')
+print(paises)
+print(type(paises))
+
+# acessando elementos
+# forma 1 - acessando via chave, da mesma forma que lista/tupla
+print(paises['br'])
+print(paises['eua'])
+
+# obs: caso tentarmos fazer um acesso utilizando uma chave que não existe, teremos o erro KeyError
+
+# forma 2 - acessando via get - recomendado
+print(paises.get('br'))
+print(paises.get('eua'))
