42d368fcc1c9b53685b0490a47c7b72f3f215db5
George Salu <george.salu10@gmail.com>
2026-01-16 10:26:20 -0300

    geek-universisty - sessao 6 - dicionarios #01

---
+++ geek-university/6_colecoes/dicionarios.py
@@ -0,0 +1,17 @@
+"""
+dicionarios
+
+obs: em algumas linguagens de programacao, os dicionarios pyhton são conhecidos como mapas
+
+dicionarios são coleções do tipo chave/valor
+dicionarios são representados por chaves {}
+print(type({}))
+
+obs: sobre dicionarios
+ - chave e valor são separados por dois pontos 'chave:valor'
+ - tanto chave quanto valor podem ser de qualquer tipo de dado
+ - podemos misturar tipos de dados
+"""
+paises = {'br': 'brasil', 'eua': 'estados unidos', 'py':'paraguai'}
+print(paises)
+print(type(paises))
