702e525a7e23985cd6b03ca5a2f21a917f03c0a1
George Salu <george.salu10@gmail.com>
2026-01-16 19:17:32 -0300

    geek-universisty - sessao 6 - conjuntos #01

---
+++ geek-university/6_colecoes/conjuntos.py
@@ -0,0 +1,13 @@
+"""
+Conjuntos
+- Conjuntos em qualquer linguagem de programação, estamos fazendo referência a teoria dos conjuntos da matematica
+- aqui no python, os conjuntos são chamados de Sets
+Dito isto, da mesma forma quena matematica
+- Sets (conjuntos) não possuem valores duplicados
+- Sets (conjuntos) não possuem valores ordenados
+- Elementos não são acessados via indice, ou seja, conjuntos não são indexados
+
+Conjuntos são bons para se utilizar quando precisamos armazenar elementos,
+mas não nos importamos com a ordenação deles. Quando não precisamos se preocupar com
+chaves, valores e itens duplicados
+"""
