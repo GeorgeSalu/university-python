a7e5d52e4e04f6cdf12d50527142659ef99a194d
George Salu <george.salu10@gmail.com>
2026-01-16 18:56:56 -0300

    geek-universisty - sessao 6 - mapas #02

--- geek-university/6_colecoes/mapas.py
+++ geek-university/6_colecoes/mapas.py
@@ -14,3 +14,28 @@ for chave in receita:
 
 for chave in receita:
     print(f'em {chave} recebi R$ {receita[chave]}')
+
+# acessando as chaves
+print(receita.keys())
+
+for chave in receita.keys():
+    print(receita[chave])
+
+# acessando os valores
+print(receita.values())
+
+for valor in receita.values():
+    print(valor)
+
+
+# desempacotamento de dicionarios
+for chave, valor in receita.items():
+    print(f'chave= {chave} valor= {valor}')
+
+# soma, valor maximo, valor minimo, tamanho
+# se os valores forem todos inteiros ou reais
+
+print(sum(receita.values()))
+print(max(receita.values()))
+print(min(receita.values()))
+print(len(receita.values()))
