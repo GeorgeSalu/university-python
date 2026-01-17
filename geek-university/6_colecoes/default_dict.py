0d14298900a64bbb67f8c9278e3b7bfc3bca10f1
George Salu <george.salu10@gmail.com>
2026-01-17 15:12:11 -0300

    geek-universisty - sessao 6 - default dict #01

---
+++ geek-university/6_colecoes/default_dict.py
@@ -0,0 +1,13 @@
+"""
+Modulo Collections = Default Dict
+
+# recap dicionario
+dicionario = {'curso':'curso de python'}
+print(dicionario)
+print(dicionario['curso'])
+print(dicionario['outro']) # KeyError
+
+Default Dict -> Ao criar um dicionario utilizando-o, nós informamos um valor default, podendo
+utilizar um lambda para isso, esse valor sera utilizado sempre que não houver um valor definido.
+Caso tentemos acessar uma chave que não existe, essa chave será criada e o valor default será atribuído
+"""
