f8fed996a239fccf1d9758ded2a1bccb76f6c336
George Salu <george.salu10@gmail.com>
2026-01-17 12:09:24 -0300

    geek-universisty - sessao 6 - counter #01

---
+++ geek-university/6_colecoes/counter.py
@@ -0,0 +1,8 @@
+"""
+Modulo Collections - counter
+Collections -> Heigh-performance Container Datetypes
+
+Counter -> Recebe um interavel como parametro e cria um objeto do tipo Collections Counter que é parecido
+com um dicionário, contendo como chave o elemento da lista passada como parametro e como valor a quantidade
+de ocorrencias desse elemento
+"""
