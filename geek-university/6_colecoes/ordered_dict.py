cd0f78da67e14a2de854e334df77714c646ec760
George Salu <george.salu10@gmail.com>
2026-01-17 19:41:59 -0300

    geek-universisty - sessao 6 - ordered dict #01

---
+++ geek-university/6_colecoes/ordered_dict.py
@@ -0,0 +1,10 @@
+"""
+Modulo Collections: Ordered Dict
+
+# num dicionário a ordem de inserção dos elementos não e garantida
+dicionario = {'a': 1, 'b': 2, 'c': 3}
+print(dicionario)
+
+for chave, valor in dicionario.items():
+    print(f'chave: {chave}, valor: {valor}')
+"""
