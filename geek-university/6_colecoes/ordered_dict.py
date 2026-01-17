ce33022864ca8770a3c527c75a2b7acd343b8441
George Salu <george.salu10@gmail.com>
2026-01-17 20:12:59 -0300

    geek-universisty - sessao 6 - ordered dict #02

--- geek-university/6_colecoes/ordered_dict.py
+++ geek-university/6_colecoes/ordered_dict.py
@@ -7,4 +7,13 @@ print(dicionario)
 
 for chave, valor in dicionario.items():
     print(f'chave: {chave}, valor: {valor}')
+
+OrderedDict -> É um dicionario que nos garante a ordem de inserção dos elementos
 """
+# fazendo o import
+from collections import OrderedDict
+
+dicionario = OrderedDict({'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5})
+
+for chave, valor in dicionario.items():
+    print(f'chave: {chave}, valor: {valor}')
