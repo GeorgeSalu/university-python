69fad36a9dd2ba83a7393197d3ff465958e8d30e
George Salu <george.salu10@gmail.com>
2026-01-17 20:14:53 -0300

    geek-universisty - sessao 6 - ordered dict #03

--- geek-university/6_colecoes/ordered_dict.py
+++ geek-university/6_colecoes/ordered_dict.py
@@ -17,3 +17,17 @@ dicionario = OrderedDict({'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5})
 
 for chave, valor in dicionario.items():
     print(f'chave: {chave}, valor: {valor}')
+
+# entendendo a diferenca entre Dict e OrderedDict
+# dicionarios comuns
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 2, 'a': 1}
+
+print(dict1 == dict2)
+
+# OrderedDict
+
+odict1 = OrderedDict({'a': 1, 'b': 2})
+odict2 = OrderedDict({'b': 2, 'a': 1})
+print(odict1 == odict2)
