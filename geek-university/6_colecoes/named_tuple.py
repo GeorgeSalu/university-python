c4b022d2fb37c1505efdf646f25a7bfd94954386
George Salu <george.salu10@gmail.com>
2026-01-19 09:22:05 -0300

    geek-universisty - sessao 6 - named tupla #01

---
+++ geek-university/6_colecoes/named_tuple.py
@@ -0,0 +1,9 @@
+"""
+Modulo Collections - Named Tuple
+
+# Recap tupla
+tupla = (1,2,3,4)
+print(tupla[1])
+
+Named Tuple -> São tuplas, diferenciadas, onde, especificamos um nome para mesma e também parametros
+"""
