6ec6f6f4917373ca5100f7b3f8754f0562698c37
George Salu <george.salu10@gmail.com>
2026-01-19 09:29:59 -0300

    geek-universisty - sessao 6 - named tupla #02

--- geek-university/6_colecoes/named_tuple.py
+++ geek-university/6_colecoes/named_tuple.py
@@ -7,3 +7,29 @@ print(tupla[1])
 
 Named Tuple -> São tuplas, diferenciadas, onde, especificamos um nome para mesma e também parametros
 """
+from collections import namedtuple
+
+# precisamos definir o nome e parametro
+# forma 1 - declaração Named Tuple
+cachorro = namedtuple('cachorro', 'idade raca nome')
+
+# forma 2 - declaração Named Tuple
+cachorro = namedtuple('cachorro', 'idade, raca, nome')
+
+# forma 3 - declaração Named Tuple
+cachorro = namedtuple('cachorro', ['idade', 'raca', 'nome'])
+
+# usando
+ray = cachorro(idade= 2, raca='chow-chow', nome='ray')
+print(ray)
+
+# acessando os dados
+# forma 1
+print(ray[0])
+print(ray[1])
+print(ray[2])
+
+# forma 2
+print(ray.idade)
+print(ray.raca)
+print(ray.nome)
