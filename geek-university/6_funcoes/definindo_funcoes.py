46ac75f4c0713e3c678336e3ed6a4071a8a5912a
George Salu <george.salu10@gmail.com>
2026-01-20 09:11:02 -0300

    geek-universisty - sessao 7 - definindo funcoes #01

---
+++ geek-university/6_funcoes/definindo_funcoes.py
@@ -0,0 +1,14 @@
+"""
+Definindo funções
+- Funções são pequenas partes de codigo que realizam tarefas específicas
+- Pode ou não receber entradas de dados e retornar uma saida de dados
+- Muto uteis para executar procedimentos similares por repetidas vezes
+
+obs: Se você escrever uma função que realiza várias tarefas dentro dela;
+é bom fazer uma verificação para que a sua função seja simplificada
+"""
+# exemplo de utilizacao de função
+cores = ['verde','amarelo','azul','branco']
+
+# utilizando a função integrada do python
+print(cores)
