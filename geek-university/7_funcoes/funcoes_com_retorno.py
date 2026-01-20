a667f8c702cedded9e49acded1b3e0d7a2f8457e
George Salu <george.salu10@gmail.com>
2026-01-20 12:14:30 -0300

    geek-universisty - sessao 7 - funcoes com retorno #02

--- geek-university/7_funcoes/funcoes_com_retorno.py
+++ geek-university/7_funcoes/funcoes_com_retorno.py
@@ -3,6 +3,12 @@ Funçoes com retorno
 obs:
 - em python, quando uma função não retorna nenhum valor, o retorno é None
 - funções python que retornam valores,devem retornar estes valores com a palavra reservada return
+- não precisamos necesariamente criar uma variavel para receber o retorno de uma função. podemos passr a execução da função para outras funções
+
+obs: sobre a palavra reservada return
+- ela finaliza a função, ou seja, ela sai da execução da função
+- podemos ter,numa função, diferentes returns
+- podemos, numa função, retornar qualquer tipo de dado e ate mesmo multiplos valores
 """
 def quadrado_de_7():
     return 7 * 7
