9609768171f9c2798c535478570f1bc534416673
George Salu <george.salu10@gmail.com>
2026-01-20 14:29:20 -0300

    geek-universisty - sessao 7 - funcoes com parametros

---
+++ geek-university/7_funcoes/funcoes_com_parametro.py
@@ -0,0 +1,14 @@
+"""
+Funcoes com parametros (de entrada)
+- Funções que recebem dados para serem processados dentro da mesma;
+
+Se a gente pensar num programa qualquer, geralmente temos:
+entrada -> processamento -> saida
+
+Se a gente pensar numa função,ja sabemos que temos funções que:
+- não possuem entrada
+- não possuem saida
+- possuem entrada, mas nãp possuem said
+- não possuem entrada m possuem saida
+- possuem entrada e saida
+"""
