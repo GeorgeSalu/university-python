2d03de6b53bc738a4103fef784a7d4dd056e6533
George Salu <george.salu10@gmail.com>
2026-01-20 10:05:16 -0300

    geek-universisty - sessao 7 - definindo funcoes #02

--- geek-university/7_funcoes/definindo_funcoes.py
+++ geek-university/7_funcoes/definindo_funcoes.py
@@ -6,9 +6,35 @@ Definindo funções
 
 obs: Se você escrever uma função que realiza várias tarefas dentro dela;
 é bom fazer uma verificação para que a sua função seja simplificada
+
+Em python a forma geral de definir uma funcção é:
+
+def nome_da_funcao(parametros_entradas):
+    bloco_da_funcao
+
+onde:
+nome_da_funcao -> sempre, com letras minusculas, e se for nome composto, separado por underline
+parametros_entradas -> opcionais, onde tendo, mas de um,cada um separado por vírgula, podendo ser opcionais ou não
+bloco_da_funcao -> também chamado de corpo da funcção ou implementeção, é onde o processamento da função acontece.
+
+obs: veja que para definir uma função, tuilizamos a palavra reservada 'def' informando ao python que estamos definindo uma
+função. também abrimos o bloco de codigo com o ja conhecido dois pontos : que é utilizado em python para definir blocos
 """
 # exemplo de utilizacao de função
 cores = ['verde','amarelo','azul','branco']
 
 # utilizando a função integrada do python
 print(cores)
+
+curso = 'programao em python'
+print(curso)
+
+cores.append('roxo')
+print(cores)
+
+# curso.append('mais dados...') # AttributeError
+
+cores.clear()
+print(cores)
+
+# print(help(print))
