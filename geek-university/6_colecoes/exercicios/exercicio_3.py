c30e87ae6573f07b13f1927dfe7c48205f2dd769
George Salu <george.salu10@gmail.com>
2026-01-20 09:02:29 -0300

    geek-universisty - sessao 6 - exercicio 3 (solucao)

--- geek-university/6_colecoes/exercicios/exercicio_3.py
+++ geek-university/6_colecoes/exercicios/exercicio_3.py
@@ -1,3 +1,17 @@
 """
 3. Faça um programa que leia 10 valores, armazene-os numa lista e apresente quantos valores pares ele possui.
 """
+lista: list[int] = []
+contador_pares = 0
+
+while len(lista) < 10:
+    valor: int = int(input(f'informe o valor {len(lista)+1}/10: '))
+    lista.append(valor)
+
+    if valor % 2 == 0:
+        contador_pares += 1
+
+if contador_pares > 0:
+    print(f'a lista {lista} possui {contador_pares} pares')
+else:
+    print(f'a lista não possui valores pares')
