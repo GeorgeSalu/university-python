18a8dfff1a09fdf537fee2d78d29fde50281dd18
George Salu <george.salu10@gmail.com>
2026-01-20 14:00:09 -0300

    geek-universisty - sessao 7 - funcoes com retorno #03

--- geek-university/7_funcoes/funcoes_com_retorno.py
+++ geek-university/7_funcoes/funcoes_com_retorno.py
@@ -10,8 +10,27 @@ obs: sobre a palavra reservada return
 - podemos ter,numa função, diferentes returns
 - podemos, numa função, retornar qualquer tipo de dado e ate mesmo multiplos valores
 """
+# exemplo 1
 def quadrado_de_7():
     return 7 * 7
 
 ret = quadrado_de_7()
 print(f'retorno: {ret}')
+
+# exemplo 2
+def nova_funcao():
+    variavel = False
+    if variavel:
+        return 4
+    elif variavel is None:
+        return 3
+    return 'b'
+
+print(nova_funcao())
+
+# exemplo 3
+def outra_funcao():
+    return 2,3,4,5
+
+print(outra_funcao())
+print(type(outra_funcao()))
