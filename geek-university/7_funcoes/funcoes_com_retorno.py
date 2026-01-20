7f6730b706a2b718fdaff317d7012c066bb91581
George Salu <george.salu10@gmail.com>
2026-01-20 11:53:15 -0300

    geek-universisty - sessao 7 - funcoes com retorno #01

---
+++ geek-university/7_funcoes/funcoes_com_retorno.py
@@ -0,0 +1,11 @@
+"""
+Funçoes com retorno
+obs:
+- em python, quando uma função não retorna nenhum valor, o retorno é None
+- funções python que retornam valores,devem retornar estes valores com a palavra reservada return
+"""
+def quadrado_de_7():
+    return 7 * 7
+
+ret = quadrado_de_7()
+print(f'retorno: {ret}')
