7bd3a946444c26ffe1024ec5b35b8ba3dabb7000
George Salu <george.salu10@gmail.com>
2026-01-20 16:28:23 -0300

    geek-universisty - sessao 7 - funcoes com parametro padrao #01

---
+++ geek-university/7_funcoes/funcoes_com_parametro_padrao.py
@@ -0,0 +1,24 @@
+"""
+Funcoes com parametro padrao (Default Parameters)
+
+_ Funções onde a passagem de parametro seja opcional
+
+# exemplo de função onde a passagem de parametro seja opcional
+print('geek university')
+print()
+
+# exemplo de função onde a passagem de parametro seja obrigatori
+def quadrado(numero):
+    return numero * numero
+
+print(quadrado(3))
+print(quadrado()) # TypeError
+"""
+# obs: em python os parametros com valores default devem sempre estar ao final da declaração
+
+def teste(potencia, num=2):
+    return num ** potencia
+
+print(teste(6))
+
+# exemplos mas complexos
