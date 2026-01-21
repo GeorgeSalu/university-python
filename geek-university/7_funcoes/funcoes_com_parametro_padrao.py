9d2cb06be91faa1c353b91d3751bcb8c6ae5936c
George Salu <george.salu10@gmail.com>
2026-01-21 19:41:24 -0300

    geek-universisty - sessao 7 - funcoes com parametro padrao #03

--- geek-university/7_funcoes/funcoes_com_parametro_padrao.py
+++ geek-university/7_funcoes/funcoes_com_parametro_padrao.py
@@ -34,3 +34,39 @@ print(mostrar_informacao(instrutor=True))
 print(mostrar_informacao(True))
 print(mostrar_informacao('Ozzy'))
 print(mostrar_informacao(nome='stephany'))
+
+# por que utilizar parametros com valor default ?
+# - nos permite ser, mas flexivel nas funções
+# - evita erros com parametros incorretos
+# - nos permite trabalhar com exemplos, mas legiveis de codigo
+
+# quais tipos de daos podemos utilizar como valores default para parametros ?
+# - qualquer numero de dados : numero, string, float, boolean, etc...
+
+# exemplos
+
+def soma(num1, num2):
+    return num1 + num2
+
+def mat(num1, num2, fun=soma):
+    return fun(num1, num2)
+
+def subtracao(num1, num2):
+    return num1 - num2
+
+print(mat(3,4))
+print(mat(3,4, subtracao))
+
+# podemos ter funcoes que são declaradas dentro ed funcções, e também tem uma forma especial de escopo de varaivel
+
+def fora():
+    contador = 0
+
+    def dentro():
+        nonlocal contador
+
+        contador += 1
+        return contador
+    return dentro()
+
+print(fora())
