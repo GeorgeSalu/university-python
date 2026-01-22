feec129140bcfa4b2bc91ae05a53463266fab7f8
George Salu <george.salu10@gmail.com>
2026-01-22 14:55:49 -0300

    geek-universisty - sessao 7 - exercicio 3 (solucao)

--- geek-university/7_funcoes/exercicios/exercicio_3.py
+++ geek-university/7_funcoes/exercicios/exercicio_3.py
@@ -1,3 +1,11 @@
 """
 3. Faça um programa que tenha uma função que receba uma lista de inteiros e retorne o maior valor.
 """
+
+def maior(inteiro: list[int]) -> int:
+    return max(inteiro)
+
+
+if __name__ == '__main__':
+    lista: list[int] = [2,3,4,3,60,4]
+    print(f'o maior valor na lista {lista} eh {maior(lista)}')
