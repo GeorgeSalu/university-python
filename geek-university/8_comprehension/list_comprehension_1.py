87eb485ec418fb08a30056344770850d85cfa984
George Salu <george.salu10@gmail.com>
2026-01-22 18:29:55 -0300

    geek-universisty - sessao 8 - list comprehension #02

--- geek-university/8_comprehension/list_comprehension_1.py
+++ geek-university/8_comprehension/list_comprehension_1.py
@@ -11,3 +11,32 @@ numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 res = [numero * 10 for numero in numeros]
 print(res)
+
+"""
+Para entender melhor o que esta acontecendo devemos dividir a expressão em duas partes
+- a primeira parte: for numero in numeros
+- a segunda parte: numero * 10
+"""
+
+res = [numero / 2 for numero in numeros]
+print(res)
+
+def funcao(valor):
+    return valor * valor
+
+
+res = [funcao(numero) for numero in numeros]
+print(res)
+
+# list comprehension vs loop
+# loop
+numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+numeros_dobrados = []
+for numero in numeros:
+    numero_dobrado = numero * 2
+    numeros_dobrados.append(numero_dobrado)
+
+print(numeros_dobrados)
+
+# list comprehension
+print([numero * 2 for numero in numeros])
