32b788c2b7729f26df844f3c5d047e6f97aa214b
George Salu <george.salu10@gmail.com>
2026-01-22 08:47:32 -0300

    geek-universisty - sessao 7 - entendendo o *args #02

--- geek-university/7_funcoes/args.py
+++ geek-university/7_funcoes/args.py
@@ -12,7 +12,7 @@ Mas o que é o *args ?
 O Parametro *args utilizado numa função, coloca os valores extra informados como entrada
 em uma tupla, então desde já se lembre que tuplas são imutaveis
 """
-# exemplos
+# exemplo 1
 
 def soma_todos_numeros(*args):
     print(args)
@@ -21,3 +21,23 @@ soma_todos_numeros()
 soma_todos_numeros(1)
 soma_todos_numeros(1,2)
 soma_todos_numeros(1,2,3)
+
+# exemplo 2
+
+def verifica_info(*args):
+    if 'geek' in args and 'university' in args:
+        return 'bem-vindo geek'
+    return 'eu não tenho certeza quem voce é'
+
+
+print(verifica_info())
+print(verifica_info(1, True, 'university', 'geek'))
+
+# desempacotar
+
+def soma_numeros(*args):
+    return sum(args)
+
+numeros = (1,2,3,4,5,6,7)
+
+print(soma_numeros(*numeros))
