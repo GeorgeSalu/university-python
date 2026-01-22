13fad2cf70c4746f95da0f4660420a11a9253d22
George Salu <george.salu10@gmail.com>
2026-01-22 10:10:08 -0300

    geek-universisty - sessao 7 - exercicio 1 (solucao)

--- geek-university/7_funcoes/exercicios/exercicio_1.py
+++ geek-university/7_funcoes/exercicios/exercicio_1.py
@@ -1,3 +1,9 @@
 """
 1. Crie um programa que tenha uma função que recebe um parâmetro inteiro e devolve o seu dobro.
 """
+def dobro(numero: int) -> int:
+    return numero * 2
+
+if __name__ == '__main__':
+    valor = 4
+    print(f'o dobro de {valor} eh {dobro(valor)}')
