50fd6858dbfe57d4d4869df1e973cd6c259ae5dd
George Salu <george.salu10@gmail.com>
2026-01-23 20:52:41 -0300

    geek-universisty - sessao 8 - set comprehension #01

---
+++ geek-university/8_comprehension/set_comprehension.py
@@ -0,0 +1,17 @@
+"""
+Set Comprehension
+
+lista = [1,2,3,4,5,6]
+Set = {1,2,3,4,5,6}
+"""
+# exemplos
+numeros = {num for num in range(1,7)}
+print(numeros)
+
+# outro exemplo
+numeros = {x ** 2 for x in range(10)}
+print(numeros)
+
+# desafio: faça uma alteração na estrutura acima para gerar um dicionario ao inves de set
+numeros = {x: x ** 2 for x in range(10)}
+print(numeros)
