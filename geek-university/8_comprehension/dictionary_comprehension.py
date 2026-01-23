86d0c444732b9de5f349672fc114a9ae5d724095
George Salu <george.salu10@gmail.com>
2026-01-23 10:53:28 -0300

    geek-universisty - sessao 8 - dictionarny comprehension #02

--- geek-university/8_comprehension/dictionary_comprehension.py
+++ geek-university/8_comprehension/dictionary_comprehension.py
@@ -14,7 +14,26 @@ dicionario = {'a':1,'b':2,'c':3,'d':4,'e':5}
 # sintaxy
 {chave:valor for valor in iteravel}
 """
-# exemplo
+# exemplo 1
 numeros = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5}
 quadrado = {chave:valor **2 for chave, valor in numeros.items()}
 print(quadrado)
+
+# exemplo 2
+numero = [1,2,3,4,5,6,7,8,9,10]
+quadrados = {valor: valor ** 2 for valor in numero}
+print(quadrados)
+
+
+# exemplo 3
+chaves = 'abcde'
+valores = [1,2,3,4,5]
+
+mistura = {chaves[i]: valores[i] for i in range(0, len(chaves))}
+print(mistura)
+
+# exemplo 4 - com lógica condicional
+numeros = [1,2,3,4,5]
+
+res = {num: ('par' if num % 2 == 0 else 'impar') for num in numeros}
+print(res)
