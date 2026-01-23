c48e11829bfc019b9b4f5ca4a0123948935c305d
George Salu <george.salu10@gmail.com>
2026-01-23 09:07:34 -0300

    geek-universisty - sessao 8 - list comprehension #05

--- geek-university/8_comprehension/list_comprehension_2.py
+++ geek-university/8_comprehension/list_comprehension_2.py
@@ -13,7 +13,14 @@ print(pares)
 print(impares)
 
 # refatorado
+# qualquer número par modulo de 2 é 0 e 0 em python é False, not False = True
 pares = [numero for numero in numeros if not numero % 2]
+
+# qualquer número impar modulo de 2 é 1 e 1 em python e True
 impares = [numero for numero in numeros if numero % 2]
 print(pares)
 print(impares)
+
+# 2
+res = [numero * 2 if numero % 2 == 0 else numero / 2 for numero in numeros]
+print(res)
