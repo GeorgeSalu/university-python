3e866feea3d50918d3179a33aca622ea64a350ad
George Salu <george.salu10@gmail.com>
2026-01-24 20:44:03 -0300

    geek-universisty - sessao 9 - filter #03

--- geek-university/9_lambdas_e_funcoes_integradas/filter.py
+++ geek-university/9_lambdas_e_funcoes_integradas/filter.py
@@ -25,3 +25,23 @@ print(type(res))
 print(list(res))
 
 # obs: assim como na função map(), apos serem utilizados dos dados de filter() eles são excluidos da memória
+
+#exemplo 3
+paises = ['','argentina','','brasil','chile','','colombia','','equador','','','venezuela']
+print(paises)
+
+# forma 1 - filtra os vazios
+res = filter(None, paises)
+print(list(res))
+
+# forma 2 - filtro os vazios
+res = filter(lambda pais: len(pais) > 0, paises)
+print(list(res))
+
+# forma 3 - filtra os vazios
+res = filter(lambda pais: pais != '', paises)
+print(list(res))
+
+# a diferenca entre map() e filter() é:
+# map() -> recebe dois parametros, uma função e um iteravel e retorna um objeto mapeando a função para cada elemento do iteravel
+# filter() -> recebe dois parametros, uma função e iteravel e retorna um objeto filtrando apenas os elementos de acordo com a função
