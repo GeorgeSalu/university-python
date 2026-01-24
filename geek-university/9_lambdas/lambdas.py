419560d0c139017fc981228b05b21afed1183f4d
George Salu <george.salu10@gmail.com>
2026-01-24 09:40:36 -0300

    geek-universisty - sessao 9 - utilizando lambdas #01

---
+++ geek-university/9_lambdas/lambdas.py
@@ -0,0 +1,12 @@
+"""
+Utilizando Lambdas
+
+Conhecidas por exepressões lambdas ou simplesmente lambdas, são funçõe sem nome, ou seja são funções anonimas
+"""
+# função em python
+def funcao(x):
+    return 3 * x + 1
+
+print(funcao(4))
+print(funcao(7))
+
