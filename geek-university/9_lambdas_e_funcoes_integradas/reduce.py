1781e0ae12bcc2323caa6d6d002b46266b6aa6da
George Salu <george.salu10@gmail.com>
2026-01-25 20:20:18 -0300

    geek-universisty - sessao 9 - reduce #01

--- geek-university/9_lambdas_e_funcoes_integradas/reduce.py
+++ geek-university/9_lambdas_e_funcoes_integradas/reduce.py
@@ -11,16 +11,16 @@ Para entender o reduce()
 
 # imagine que voce tem uma coleção de dados:
 dados = [a1,a2,a3,a4,....an]
-# e voce tem uma função que recebe dois parametros
+# e voce tem uma função que recebe dois parâmetros
 def funcao(x, y):
     return x + y)
 
-assim como map() e filter(), a função reduce() recebe dois parametros: a função e o iteravel
+assim como map() e filter(), a função reduce() recebe dois parâmetros: a função e o iterável
 reduce(funcao, dados)
 A funcao reduce(), funciona da seguinte forma:
-- passo 1: res1 = f(a1,a2) # aplica a função nos dois primeiros elementos da colecaçõ e guarda o resultado
+- passo 1: res1 = f(a1,a2) # aplica a função nos dois primeiros elementos da coleção e guarda o resultado
 - passo 2: res2 = f(res1, a3) # aplica a função passando o resultado do passo1 mais o terceiro elemento e guarda o res
-  Isso é repetido ate o fimnal.
+  Isso é repetido ate o final.
   passo 3: res3 = f(res2, a4)
   .
   .
@@ -29,6 +29,6 @@ A funcao reduce(), funciona da seguinte forma:
 ou seja, em cada passo ela aplica a função passando como primeiro argumento o resultado da aplicação anterior. no final
 reduce() irá retornar o resultado final
 
-alternativamente, poderiamos ver a função reduce() como:
+alternativamente, poderíamos ver a função reduce() como:
 funcao(funcao(funcao(a1, a2), a3), a4), ...), an)
 """
