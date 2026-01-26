5cc334614c168545efc4ad4a3be74a4f1dfe7f71
George Salu <george.salu10@gmail.com>
2026-01-26 15:54:05 -0300

    geek-universisty - sessao 9 - generators #02

--- geek-university/9_lambdas_e_funcoes_integradas/generators.py
+++ geek-university/9_lambdas_e_funcoes_integradas/generators.py
@@ -1,5 +1,5 @@
 """
-Generators
+Generator Expression
 
 Em aul anteriores nos estudamos
 - list comprehension
@@ -17,3 +17,51 @@ print(any(nome[0] == 'c' for nome in nomes))
 
 nomes = ['carlos','camila','carla','cassiano','cristiana','vanessa']
 print(any(nome[0] == 'c' for nome in nomes))
+
+# list comprehensions
+res = [nome[0] == 'c' for nome in nomes]
+print(type(res))
+print(res)
+
+# generator
+res = (nome[0] == 'c' for nome in nomes)
+print(type(res))
+print(res)
+
+# qual é a utilizade de getsizeof() ? -> retorna a quantidade de bytes em memória do elemento passado como parametro
+from sys import getsizeof
+
+# mostra quantos bytes a string 'geek' esta ocupando em memória, quanto maior a string, mais espaco ocupa
+print(getsizeof('geek'))
+print(getsizeof('university'))
+print(getsizeof(9))
+print(getsizeof(91))
+print(getsizeof(929384433))
+print(getsizeof(True))
+
+# gerando uma lista de números com list comprehension
+list_comp = getsizeof([x * 10 for x in range(1000)])
+
+# gerando uma lista de números com set comprehension
+set_comp = getsizeof({x * 10 for x in range(1000)})
+
+# gerando uma lista de números com dict comprehension
+dict_comp = getsizeof({x:x * 10 for x in range(1000)})
+
+# gerando uma lista de números com generators
+gen = getsizeof(x * 10 for x in range(1000))
+
+print(f'para fazer a mesma tarefa gastamos em memoria : ')
+print(f'list comprehension: {list_comp}')
+print(f'set comprehension: {set_comp}')
+print(f'dict comprehension: {dict_comp}')
+print(f'generator comprehension: {gen}')
+
+# eu posso iterar no generator expression
+
+gen = (x * 10 for x in range(1000))
+print(type(gen))
+print(gen)
+
+for num in gen:
+    print(num)
