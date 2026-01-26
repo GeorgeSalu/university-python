852f0f2158d538f82ac245dde70f44d23fc35bcf
George Salu <george.salu10@gmail.com>
2026-01-26 16:41:18 -0300

    geek-universisty - sessao 9 - sorted #02

--- geek-university/9_lambdas_e_funcoes_integradas/sorted.py
+++ geek-university/9_lambdas_e_funcoes_integradas/sorted.py
@@ -7,4 +7,17 @@ so funciona em listas.
 Podemos utilizar o sorted() com qualquer iteravel
 
 Como o proprio nome diz, sorted() serve para ordenar.
+
+obs: o sorted() sempre retorna uma lista com os elementos do iteravel ordenados
 """
+# exemplo
+numeros = [1,2,102,9,4,6]
+print(numeros)
+print(sorted(numeros)) # ordena do menor para o maior
+print(numeros)
+
+# adionando parametros ao sorted()
+numeros = [6,1,8,2]
+print(numeros)
+print(sorted(numeros, reverse=True)) # ordena do maior para o maior
+
