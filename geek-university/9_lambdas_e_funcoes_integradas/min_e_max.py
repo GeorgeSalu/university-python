7d1aa34584daeb4bdb546b56ff817831cbc86765
George Salu <george.salu10@gmail.com>
2026-01-26 18:31:34 -0300

    geek-universisty - sessao 9 - max #01

---
+++ geek-university/9_lambdas_e_funcoes_integradas/min_e_max.py
@@ -0,0 +1,20 @@
+"""
+Min e Max
+
+max() -: retorn o maior valor em iteravel ou o maior de dois, ou mais elementos
+"""
+# exemplos
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(max(lista))
+
+tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(max(tupla))
+
+conjunto = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(max(conjunto))
+
+dicionario = {'a':1, 'b':2, 'c':3}
+print(max(dicionario))
+
+dicionario = {'a':1, 'b':2, 'c':3}
+print(max(dicionario.values()))
