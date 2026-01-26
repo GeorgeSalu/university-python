fd2f4b2cb31eac44b8fd95b9569bed187637d778
George Salu <george.salu10@gmail.com>
2026-01-26 15:57:36 -0300

    geek-universisty - sessao 9 - sorted #01

---
+++ geek-university/9_lambdas_e_funcoes_integradas/sertod.py
@@ -0,0 +1,10 @@
+"""
+Sorted
+
+obs: não confunda, apesar do nome,com a função sort() que já estudamos em listas, o sort()
+so funciona em listas.
+
+Podemos utilizar o sorted() com qualquer iteravel
+
+Como o proprio nome diz, sorted() serve para ordenar.
+"""
