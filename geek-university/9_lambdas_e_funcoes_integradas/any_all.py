d7485d25d7863f77c3e2417abef01e70e8a66e08
George Salu <george.salu10@gmail.com>
2026-01-26 09:21:22 -0300

    geek-universisty - sessao 9 - any

--- geek-university/9_lambdas_e_funcoes_integradas/any_all.py
+++ geek-university/9_lambdas_e_funcoes_integradas/any_all.py
@@ -2,6 +2,7 @@
 Any e All
 
 all() -> retorna True se todos os elementos do iteravel são verdadeiros ou ainda se o iteravel está vazio
+any() -> retorna True se qualquer elemento do iteravel for verdadeiro, se o iteravel etiver vazio, retorna False
 """
 # exemplo all
 print(all([0,1,2,3,4])) # todos os números são verdadeiros ? False
@@ -15,3 +16,7 @@ nomes = ['carlos','camila','carla','cassiano','cristina']
 print(all(nome[0] == 'c' for nome in nomes))
 
 # obs: um iteravel vazio convertido em boolean é False m o all() entende como True
+
+# exemplo any
+print(any([0,1,2,3,4])) # True
+print(any([0, False, {}, (), []]))
