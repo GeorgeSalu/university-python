c372f7830d04c3a404aa0c21aac9d3ec6cbe37ca
George Salu <george.salu10@gmail.com>
2026-01-27 09:37:51 -0300

    geek-universisty - sessao 9 - min #01

--- geek-university/9_lambdas_e_funcoes_integradas/min_e_max.py
+++ geek-university/9_lambdas_e_funcoes_integradas/min_e_max.py
@@ -1,9 +1,10 @@
 """
 Min e Max
 
-max() -: retorn o maior valor em iteravel ou o maior de dois, ou mais elementos
+max() -> retorn o maior valor em iterável ou o maior de dois, ou mais elementos
+min() -> retorna o menor valor num iterável ou o menor de dois, ou mais elementos
 """
-# exemplos
+# exemplos max()
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(max(lista))
 
@@ -22,3 +23,42 @@ print(max(dicionario.values()))
 print(max(4,67,54))
 print(max('a','ab','abc'))
 print(max('a','b','c','d'))
+
+# exemplos min()
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(min(lista))
+
+tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(min(tupla))
+
+conjunto = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(min(conjunto))
+
+dicionario = {'a':1, 'b':2, 'c':3}
+print(min(dicionario))
+
+dicionario = {'a':1, 'b':2, 'c':3}
+print(min(dicionario.values()))
+
+print(min(4,67,54))
+print(min('a','ab','abc'))
+print(min('a','b','c','d'))
+print(min('geek university'))
+
+# outros exemplos
+nomes = ['arya','samson','dora','tim','ollivander']
+print(max(nomes))   # tim
+print(min(nomes))   # arya
+print(max(nomes, key=lambda nome: len(nome)))   # ollivander
+print(min(nomes, key=lambda nome: len(nome)))   # tim
+
+
+musicas = [
+    {"titulo": "thunderstructk", "tocou": 3},
+    {"titulo": "Dead skin mask", "tocou": 2},
+    {"titulo": "Back in black", "tocou": 4},
+    {"titulo": "too old to rock", "tocou": 32},
+]
+
+print(max(musicas, key=lambda musica: musica["tocou"]))
+print(min(musicas, key=lambda musica: musica["tocou"]))
