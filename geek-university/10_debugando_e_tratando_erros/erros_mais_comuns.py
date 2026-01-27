a7dfe44af732397aefaa9a3fb26786f09f64a595
George Salu <george.salu10@gmail.com>
2026-01-27 13:32:17 -0300

    geek-universisty - sessao 10 - erros mais comuns em python #01

---
+++ geek-university/10_debugando_e_tratando_erros/erros_mais_comuns.py
@@ -0,0 +1,19 @@
+"""
+Erros mais comuns em python
+
+Atenção! É importante prestar atenção e aprender a ler as saidas de erros geradas pela execução
+do nosso codigo
+
+Os erros mais comuns:
+SyntaxError -> ocorre quando o python encontra um erro de sintaxe. ou seja, você escreveu algo que o
+python não reconhece como parte da linguagem
+
+exemplos SyntaxError:
+a)
+    def funcao:
+        print('geek')
+b)
+    def = 1
+c)
+    return
+"""
