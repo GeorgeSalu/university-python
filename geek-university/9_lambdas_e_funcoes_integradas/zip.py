2ebfc21ad467c6df84f250ef343793088694c261
George Salu <george.salu10@gmail.com>
2026-01-27 12:16:34 -0300

    geek-universisty - sessao 9 - zip #02

--- geek-university/9_lambdas_e_funcoes_integradas/zip.py
+++ geek-university/9_lambdas_e_funcoes_integradas/zip.py
@@ -22,3 +22,22 @@ print(set(zip1))
 
 zip1 = zip(lista1, lista2)
 print(dict(zip1))
+
+# obs: o zip utiliza como parametro o menor tamanho em iteravel, isso significa que se estiver
+# trabalhando com iteraveis de tamanhos diferentes, ira para quando os elementos do menor iteravel acabar
+lista3 = [1, 2, 3, 4, 5]
+
+zip1 = zip(lista1, lista2, lista3)
+print(list(zip1))
+
+# podemos utilizar diferentes iteraveis com zip
+tupla = 1,2,3,5
+lista = [6,7,8,9,10]
+dicionario = {'a': 1, 'b': 2, 'c': 3}
+
+zt = zip(tupla, lista, dicionario.values())
+print(list(zt))
+
+# lista de tuplas
+dados = [(0,1), (1,2), (2,3), (3,4)]
+print(list(zip(*dados)))
