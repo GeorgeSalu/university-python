8bfb39625824425a7e06e6caf2661b499c4a51ae
George Salu <george.salu10@gmail.com>
2026-01-28 11:12:49 -0300

    geek-universisty - sessao 10 - levantando os proprios erros com raise #01

---
+++ geek-university/10_debugando_e_tratando_erros/raise.py
@@ -0,0 +1,13 @@
+"""
+Levantando os proprios erros com raise
+
+raise -> Lança exceções
+
+obs: O raise não é uma função. é uma palavra reservada, assim como def ou qualquer outra em python
+Para simplificar, pense no raise como sendo util pra que possamos criar as nossas proprias exceções e mensagens
+de error.
+
+A forma geral de utilização é:
+raise TipoDoErro('mensagem de erro')
+"""
+raise ValueError('valor incorreto')
