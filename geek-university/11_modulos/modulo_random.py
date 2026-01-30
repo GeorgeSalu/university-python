618afb751e3f515600b040c58c04a249b8fe2c12
George Salu <george.salu10@gmail.com>
2026-01-29 22:15:07 -0300

    geek-universisty - sessao 11 - modulo random #01

---
+++ geek-university/11_modulos/modulo_random.py
@@ -0,0 +1,14 @@
+"""
+Modulo Random e o que são modulos?
+
+- Em Python, modulos nada mais são do que outros arquivos Pyhon
+
+Modulo Random -> Possui variaveis funções para geração de números pseudoaleatorio
+"""
+# obs: existem duas formas de se utilizar um módulo ou função deste
+# domra 1 - importando todo o modulo (não remocomendado)
+import random
+
+# obs: ao realizar o import de todo o modulo, todas as funções, atributos, classes e propriedades que estiverem
+# dentro do modulo ficarãp disponiveis (ficarão na memoria). caso você saiba quais funções voce precisa utilizar
+# deste modulo, então esta seria a forma ideial de utilização. nos veremos uma forma melhor na Forma 2
