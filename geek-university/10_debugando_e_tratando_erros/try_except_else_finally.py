d60e47bba9ae07608770f2a1ac6abb9df3843dcc
George Salu <george.salu10@gmail.com>
2026-01-29 21:19:04 -0300

    geek-universisty - sessao 10 - try/except/else/finally #02

--- geek-university/10_debugando_e_tratando_erros/try_except_else_finally.py
+++ geek-university/10_debugando_e_tratando_erros/try_except_else_finally.py
@@ -4,9 +4,23 @@ Dica de quando e onde tratar codigo:
 TODA ENTRADA DEVE SER TRATADA!
 obs: A função do usuário é destruir o seu sistema
 """
+# else -> é executado somente se não ocorrer o error
 try:
     num = int(input('informe um numero: '))
 except ValueError:
     print(f'valor incorreto')
+else:
+    print(f'o numero digitado foi {num}')
 
-print(f'o numero digitado foi {num}')
+# finally
+try:
+    num = int(input('informe um numero: '))
+except ValueError:
+    print('voce não digitou um valor valido')
+else:
+    print(f'voce digitou o numero {num}')
+finally:
+    print('executando o finally')
+
+# obs: o bloco finally é sempre executado, independente houve-se execução ou não
+# o finally, geralmente é utilizado para fechar ou desalocar recursos
