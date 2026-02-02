6fa8b9b2d7b7f0c60d369b04133608bf27db157e
George Salu <george.salu10@gmail.com>
2026-02-02 11:44:27 -0300

    geek-universisty - sessao 11 - dunedr main e dunder name

---
+++ geek-university/11_modulos/dunder.py
@@ -0,0 +1,10 @@
+"""
+Dunder Name e Dunder Main
+Dunder -> Double Under
+Dunder Name -> __name__
+Dunder Main -> __main__
+
+Em python, são utilizados dunder para criar funções, atributos, propriedades e etc utilizando
+Double Under para não gerar conflito com os nomes desses elementos na programação
+
+"""
