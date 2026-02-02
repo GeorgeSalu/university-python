89488c74363b24b2345f4a310b642c93ecdec58b
George Salu <george.salu10@gmail.com>
2026-02-02 14:18:06 -0300

    geek-universisty - sessao 12 - leitura de arquivos #01

---
+++ geek-university/12_arquivos/leitura_de_arquivos.py
@@ -0,0 +1,10 @@
+"""
+Leitura de arquivos
+
+Para ler o conteudo de um arquivo em python, utilizamos a função integrada open()
+que literalmente significa 'abrir'
+
+open() -> na forma mais simples de utilização nos passamos apenas um parametro de
+entrada, que neste caso é o nome do arquivo a ser lido. Essa função retorna um
+_io.TextIoWrapper e é com ele que trabalhamos então
+"""
