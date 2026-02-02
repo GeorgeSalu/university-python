63f8c74d428032d005ee31a98f67e0f18138fd5b
George Salu <george.salu10@gmail.com>
2026-02-02 09:52:56 -0300

    geek-universisty - sessao 11 - modulos customizados

---
+++ geek-university/11_modulos/modulos_customizados.py
@@ -0,0 +1,9 @@
+"""
+Modulos customizados
+
+Como modulos python nada mais são do que arquivos python, então TODOS os arquivos que criamos
+neste curso são modulos python prontos para serem utilizados.
+"""
+from funcoes_com_parametro import soma_impares
+
+print(soma_impares([1,2,3,4]))
