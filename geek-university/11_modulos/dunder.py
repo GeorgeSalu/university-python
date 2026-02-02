e1788b3974abdf5a0fef2ffed40bee5051a73b71
George Salu <george.salu10@gmail.com>
2026-02-02 14:09:39 -0300

    geek-universisty - sessao 11 - dunedr main e dunder name #1

--- geek-university/11_modulos/dunder.py
+++ geek-university/11_modulos/dunder.py
@@ -7,4 +7,7 @@ Dunder Main -> __main__
 Em python, são utilizados dunder para criar funções, atributos, propriedades e etc utilizando
 Double Under para não gerar conflito com os nomes desses elementos na programação
 
+# em python, se executarmos um módulo python diretamente na linha de comando, internamente o
+python atribuirá a variavel __name__ o valor __main__ indicando que este modulo é o modulo
+de execução principal
 """
