5c7205358cc8563ef805998140bacd846cd37bfa
George Salu <george.salu10@gmail.com>
2026-02-02 09:28:34 -0300

    geek-universisty - sessao 11 - trabalhando com modulos builtins #02

--- geek-university/modulos_builtin.py
+++ geek-university/modulos_builtin.py
@@ -5,3 +5,33 @@ Trabalhando com modulos builtin (modulos integrados, que ja vem instalados no py
 import random as rdm
 
 print(rdm.random())
+
+# podemos importar todas as funções de um módulo utilizando o *
+from random import *
+
+print(random())
+
+# importando todo o modulo
+import random
+
+print(random.random())
+
+# utilizando alias (apelidos) para modulos/funcoes
+from random import randint as rdi
+
+print(rdi(5, 89))
+
+# costumamos utilizar tuple para colocar multiplos imports de um mesmo modulo
+from random import (
+    random,
+    randint,
+    shuffle,
+    choice
+)
+
+print(random())
+print(randint(1,100))
+lista = ['geek','university','python']
+shuffle(lista)
+print(lista)
+print(choice('university'))
