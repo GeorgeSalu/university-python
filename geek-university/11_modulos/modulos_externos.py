14f9390c00118031faff658b9b88a0edcea967f2
George Salu <george.salu10@gmail.com>
2026-02-02 09:57:38 -0300

    geek-universisty - sessao 11 - instalando pacotes externos

---
+++ geek-university/11_modulos/modulos_externos.py
@@ -0,0 +1,16 @@
+"""
+Modulos externos
+Utilizamos o gerenciados de pacotes python chamado pip - python istaller package
+você pode conhecer todos os pacotes externos oficiais em https://pypi.org
+colorama -> é utilizado para permitir impressão de textos coloridos no terminal
+Instalando um módulo
+pip install nome-do-modulo
+# instalando o pacote colorama
+pip install colorama
+# utilizando o pacote instalado
+from colorama import init, Fore
+
+init()
+
+print(Fore.MAGENTA+'geek university')
+"""
