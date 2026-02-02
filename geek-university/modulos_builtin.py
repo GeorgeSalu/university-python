65903b0ec5ce7bdddd4fc1cfd660b1fb3ff408ae
George Salu <george.salu10@gmail.com>
2026-02-02 09:10:40 -0300

    geek-universisty - sessao 11 - trabalhando com modulos builtins #01

---
+++ geek-university/modulos_builtin.py
@@ -0,0 +1,7 @@
+"""
+Trabalhando com modulos builtin (modulos integrados, que ja vem instalados no python)
+"""
+# utilizando alias (apelidos) para modulos/funções
+import random as rdm
+
+print(rdm.random())
