bbb5117e3091a98f6e09ac3a6002732b8270f0b8
George Salu <george.salu10@gmail.com>
2026-02-03 17:26:19 -0300

    geek-universisty - sessao 13 - escrevendo um interador customizado #02

--- geek-university/13_iteradores_e_geradores/iterador_customizado.py
+++ geek-university/13_iteradores_e_geradores/iterador_customizado.py
@@ -6,6 +6,18 @@ class Contador:
         self.menor = menor
         self.maior = maior
 
+    def __iter__(self):
+        return self
 
-con = Contador(1,61)
-print(con)
+    def __next__(self):
+        if self.menor < self.maior:
+            numero = self.menor
+            self.menor = self.menor +1
+            return numero
+        raise StopIteration
+
+for n in Contador(1,61):
+    print(n)
+
+for n in range(1,61):
+    print(n)
