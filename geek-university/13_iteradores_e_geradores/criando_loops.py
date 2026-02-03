544b890b0f6dbaa34bfbb8417a4f401d358be286
George Salu <george.salu10@gmail.com>
2026-02-03 15:00:52 -0300

    geek-universisty - sessao 13 - criando sua propria versao de loop #01

---
+++ geek-university/13_iteradores_e_geradores/criando_loops.py
@@ -0,0 +1,13 @@
+"""
+Criando a sua propria versão de loop
+"""
+# exemplo 1
+for num in [1,2,3,4,5,6]:
+    print(num)
+
+for letra in 'abc':
+    print(letra)
+
+iter([1,2,3,4,5,6])
+iter(['abc'])
+
