5b309389c57029d46dbca8b83884540518fcfd23
George Salu <george.salu10@gmail.com>
2026-02-03 17:34:19 -0300

    geek-universisty - sessao 13 - geradores #01

---
+++ geek-university/13_iteradores_e_geradores/geradores.py
@@ -0,0 +1,20 @@
+"""
+Geradores
+- Geradores (Generators) são Iterators (Iteradores)
+obs: O contrario não é verdadeiro. ou seja, nem todo iterator é um generator
+
+Outras informações
+- Generators podem ser criados com funções geradoras
+- Funções geradoras utilizam a palavr reservada yield
+- Generators podem ser criados com expressões geradoras
+
+Diferenças entre funções e Generator Functions (Funções Geradoras)
+Funções
+    -> utilizam return
+    -> retorna uma vez
+    -> quando executada, retorna valor
+Generator Functions
+    -> utilizam yield
+    -> podem utilizar yield multiplas vezes
+    -> quando executada, retorn um generator
+"""
