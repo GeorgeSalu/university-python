81d424871b13b0a583223b0440c72c1f858ac32b
George Salu <george.salu10@gmail.com>
2026-02-03 14:27:22 -0300

    geek-universisty - sessao 12 - exercicio_3 (solucao)

--- geek-university/12_leitura_e_escrita_em_arquivos/exercicios/exercicio_3.py
+++ geek-university/12_leitura_e_escrita_em_arquivos/exercicios/exercicio_3.py
@@ -2,3 +2,10 @@
 3. Faça um programa que receba do usuário o nome de um arquivo texto e mostre na tela quantas linhas este
 arquivo possui.
 """
+arquivo: str = input('Informe o nome do arquivo para abrir: ')
+
+with open(arquivo, 'r') as arq:
+    linhas = arq.readlines()
+
+
+print(f'Existe {len(linhas)} linhas do arquivo')
