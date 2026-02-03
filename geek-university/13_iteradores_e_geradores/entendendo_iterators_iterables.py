e0ad67f29c00754c1e2e6f62e032cd894e5ed916
George Salu <george.salu10@gmail.com>
2026-02-03 14:55:47 -0300

    geek-universisty - sessao 13 - entendendo iterators e iteraveis #02

--- geek-university/13_iteradores_e_geradores/entendendo_iterators_iterables.py
+++ geek-university/13_iteradores_e_geradores/entendendo_iterators_iterables.py
@@ -6,9 +6,9 @@ Iterator ->
     - Um objeto que retorna um dado, sendo um elemento por vez quando uma função next() é chamada
 
 Iterable ->
-    - Um objeto que irá retornar um iterator qunado a função iter() for chamada
+    - Um objeto que irá retornar um iterator quando a função iter() for chamada
 """
-nome = 'geek' # é um iterable, mas nõ é um iterator
+nome = 'geek' # é um iterable, mas não é um iterator
 numeros = [1,2,3,4,5,6]
 
 it1 = iter(nome)
