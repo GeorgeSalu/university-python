ba83505b8980b412a914ee69b3e7b835696e7864
George Salu <george.salu10@gmail.com>
2026-02-03 10:27:06 -0300

    geek-universisty - sessao 12 - entendendo iterators e iterables #01

---
+++ geek-university/13_iteradores/entendendo_iterators_iterables.py
@@ -0,0 +1,12 @@
+"""
+Entendendo Iterators e Iterables
+
+Iterator ->
+    - Um objeto que pode ser iterado.
+    - Um objeto que retorna um dado, sendo um elemento por vez quando uma função next() é chamada
+
+Iterable ->
+    - Um objeto que irá retornar um iterator qunado a função iter() for chamada
+"""
+nome = 'geek'
+numeros = [1,2,3,4,5,6]
