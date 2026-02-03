45b6921949aadb6037436b5640beb1171afb11e3
George Salu <george.salu10@gmail.com>
2026-02-03 14:18:00 -0300

    geek-universisty - sessao 12 - exercicio_1 (solucao)

--- geek-university/12_leitura_e_escrita_em_arquivos/exercicios/exercicio_1.py
+++ geek-university/12_leitura_e_escrita_em_arquivos/exercicios/exercicio_1.py
@@ -5,3 +5,19 @@ b) Permita que o usuário grave diversos caracteres nesse arquivo, até que o us
 c) Feche o arquivo
 d) Abra, leia o arquivo e escreva na tela todos os caracteres armazenados.
 """
+with open('arq.txt', 'a') as arquivo:
+    while True:
+        caracter: str = input("informe um caractere ou 0 pra sair: ")
+
+        if caracter != '0':
+            arquivo.write(f'{caracter}\n')
+        else:
+            break
+
+
+with open('arq.txt', 'r') as arquivo:
+    linhas = arquivo.readlines()
+
+
+for linha in linhas:
+    print(linha)
