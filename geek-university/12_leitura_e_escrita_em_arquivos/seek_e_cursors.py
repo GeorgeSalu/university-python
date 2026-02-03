c325dca7b6042ddca221252b957b2dc266d95fc5
George Salu <george.salu10@gmail.com>
2026-02-03 09:34:03 -0300

    geek-universisty - sessao 12 - seek e cursor #01

---
+++ geek-university/12_leitura_e_escrita_em_arquivos/seek_e_cursors.py
@@ -0,0 +1,15 @@
+"""
+Seek e Cursors
+
+seek() -> é utilizado para movimentar o cursos pelo arquivo
+"""
+arquivo = open('texto.txt')
+print(arquivo.read())
+
+# seek() -> a função seek() é utilizada para movimentação do cursor pelo arquivo. Ela recebe um
+# parametro que indica onde queremos colocar o cursor.
+
+# Movimentando o cursor pelo arquivo com a função seek()
+arquivo.seek(0)
+
+print(arquivo.read())
