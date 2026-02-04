13e02f3061df2b0a03926a5a5d167cfe8faf97cd
George Salu <george.salu10@gmail.com>
2026-02-03 22:04:47 -0300

    geek-universisty - sessao 15 - classes #01

---
+++ geek-university/15_orientacao_a_objetos/classes.py
@@ -0,0 +1,14 @@
+"""
+POO - Classes
+
+Em POO,Classes nada mais são do que modelos dos objetos do mundo real sendo representados
+computacionalmente
+
+Imagine que você queira fazer um sistema para automatizar o controle das lampadas da sua casa
+"""
+class Lampada:
+    pass
+
+
+lamp = Lampada()
+print(type(lamp))
