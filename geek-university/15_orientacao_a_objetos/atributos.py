e4a3db4a543244112f7062e994b485d8c9d7f1d6
George Salu <george.salu10@gmail.com>
2026-02-05 12:28:10 -0300

    geek-universisty - sessao 15 - atributos #01

---
+++ geek-university/15_orientacao_a_objetos/atributos.py
@@ -0,0 +1,13 @@
+"""
+POO - atributos
+
+Atributos -> Representam as caracteristicas do objeto. Ou seja, pelos atributos
+conseguimo-nos representar computacionalmente os estados de um objeto
+
+Em python, dividimos os atributos em 3 grupos:
+    - atributos de instância
+    - atributos de classe
+    - atributos dinamicos
+
+# atributos de instância: são atributos declarados dentro do metodo construtor
+"""
