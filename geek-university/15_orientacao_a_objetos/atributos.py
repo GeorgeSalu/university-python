dbcfee3a6ec992fe717368e02c4ac67abda830a6
George Salu <george.salu10@gmail.com>
2026-02-05 14:35:16 -0300

    geek-universisty - sessao 15 - atributos #02

--- geek-university/15_orientacao_a_objetos/atributos.py
+++ geek-university/15_orientacao_a_objetos/atributos.py
@@ -10,4 +10,31 @@ Em python, dividimos os atributos em 3 grupos:
     - atributos dinamicos
 
 # atributos de instância: são atributos declarados dentro do metodo construtor
+# obs: metodo contrutor: é um metodo especial utilizado para construção do objeto
 """
+class Lampada:
+    def __init__(self, voltagem, cor):
+        self.voltagem = voltagem
+        self.cor = cor
+        self.ligada = False
+
+
+class ContaCorrente:
+    def __init__(self, numero, limite, saldo):
+        self.numero = numero
+        self.limite = limite
+        self.saldo = saldo
+
+
+class Produto:
+    def __init__(self, nome, descricao, valor):
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+
+
+class Usuario:
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
