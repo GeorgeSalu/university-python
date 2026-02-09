ecb0426293b2a8c5bd5b81188223bd3120c21757
George Salu <george.salu10@gmail.com>
2026-02-09 09:34:50 -0300

    geek-universisty - sessao 15 - metodos #02

--- geek-university/15_orientacao_a_objetos/metodos.py
+++ geek-university/15_orientacao_a_objetos/metodos.py
@@ -9,6 +9,12 @@ classe
 # metodo de instância
 # o metodo dunder init __init__ é um metodo especial chamado de construtor e
 a sua função e construir o objeto a partir da classe
+
+obs: todo elemento em python que inicia e finaliza com duplo underline é chamado de dunder (Double Underline)
+obs: os metodos/funções dunder em python são chamados de metodos magicos
+ATENÇÃO! por mais que possamos criar nossas proprias funções utilizando dunder (underline
+no inicio e no fim) não é aconselhado. python possui varios metodos com esta forma de nomeclatura
+e pode ser que mudemos o comportamento dessas funções magicas da linguagem, então evite ao maximo
 """
 # metodos de instância
 class Lampada:
@@ -31,10 +37,19 @@ class ContaCorrente:
 
 
 class Produto:
+
+    contador = 0
+
     def __init__(self, nome, descricao, valor):
+        self.__id = Produto.contador + 1
         self.__nome = nome
         self.__descricao = descricao
         self.__valor = valor
+        Produto.contador = self.__id
+
+    def desconto(self, porcentagem):
+        """Retorna o valor do produto com desconto"""
+        return (self.__valor * (100 - porcentagem)) / 100
 
 
 class Usuario:
@@ -42,3 +57,4 @@ class Usuario:
         self.nome = nome
         self.email = email
         self.senha = senha
+
