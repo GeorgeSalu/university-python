cbcf6cd81b5b6b2a3b2129d2a342e89d3eeabaca
George Salu <george.salu10@gmail.com>
2026-02-12 10:01:03 -0300

    coder-python - fundamentos python - comentarios

---
+++ coder-python/fundamentos/comentarios.py
@@ -0,0 +1,20 @@
+# minhas variaveis
+salario = 3450.45
+despesas = 2456.2
+
+"""
+a ideia e calcular o 
+quando vai sobrar no
+final do mes
+"""
+print(salario - despesas)
+
+# print('fim')
+print('fim de verdade') # comentario aqui tambem vale
+
+"""
+a ideia é calcular o
+quando vai sobrar no 
+final do mes
+"""
+print('agora sim o fim de verdade')
