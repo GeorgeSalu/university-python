ee3c223043e919acddfc05e545da63465af7bb0a
George Salu <george.salu10@gmail.com>
2026-02-12 10:07:35 -0300

    coder-python - fundamentos python - operadores aritmeticos

---
+++ coder-python/fundamentos/operadores_aritmeticos.py
@@ -0,0 +1,19 @@
+print(2 + 3)
+print(4 - 7)
+print(2 * 5.3)
+print(9.4 / 3)
+print(9.4 // 3)
+print(2 ** 8)
+print(10 % 3)
+
+a = 12
+b = a
+print(a + b)
+
+# minha variaveis 
+salario = 3450.45
+despesas = 2456.2
+
+# resposta do desafio
+percentual_comproetido = despesas / salario * 100
+percentual_comproetido
