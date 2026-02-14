e466a29ce38c9148c7da439acf9a511749688415
George Salu <george.salu10@gmail.com>
2026-02-14 12:04:39 -0300

    coder-python - fundamentos python - operador de identidade

--- coder-python/fundamentos/mas_operadores.py
+++ coder-python/fundamentos/mas_operadores.py
@@ -2,3 +2,19 @@
 lista = [1,2,3,4,'ana','carla']
 print(2 in lista)
 print('ana' not in lista)
+
+# operador de identidade
+x = 3
+y = x
+z = 3
+x is y
+y is z
+z is not z
+
+lista_a = [1,2,3]
+lista_b = lista_a
+lista_c = [1,2,3]
+
+print(lista_a is lista_b)
+print(lista_b is lista_c)
+print(lista_a is not lista_c)
