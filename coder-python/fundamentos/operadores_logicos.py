64ad7a959484c3120cf4421523e826946bf3e109
George Salu <george.salu10@gmail.com>
2026-02-14 11:18:13 -0300

    coder-python - fundamentos python - operadores de logicos

---
+++ coder-python/fundamentos/operadores_logicos.py
@@ -0,0 +1,15 @@
+# tabela verdade do AND
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# tabela verdade do OR
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# operador de negação
+print(not True)
+print(not False)
