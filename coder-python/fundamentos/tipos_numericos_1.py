54c6a32d75b773704f5d38007d35fad9dc1d4682
George Salu <george.salu10@gmail.com>
2026-02-14 22:38:18 -0300

    coder-python - fundamentos python - numeros

---
+++ coder-python/fundamentos/tipos_numericos_1.py
@@ -0,0 +1,16 @@
+print(dir(int))
+print(dir(float))
+
+a = 5
+b = 2.5
+print(a / b)
+print(a + b)
+print(a * b)
+
+print(type(a))
+print(type(b))
+print(type(a - b))
+
+print(b.is_integer())
+print(5.0.is_integer())
+
