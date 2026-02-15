124c9b3c34f9be3b5d988d6aa08e6d0f8a9615c2
George Salu <george.salu10@gmail.com>
2026-02-14 22:52:24 -0300

    coder-python - fundamentos python - strings #01

---
+++ coder-python/fundamentos/tipo_string.py
@@ -0,0 +1,16 @@
+print(dir(str))
+nome = 'saulo pedro'
+print(nome)
+print(nome[0])
+
+texto = """
+Texto em multiplas 
+linhas
+"""
+print(texto)
+
+doc = '''
+tambem é possivel 
+com 3 spas simples
+'''
+print(doc)
