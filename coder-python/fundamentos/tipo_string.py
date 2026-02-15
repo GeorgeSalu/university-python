33a2591b2902f2581f9aa3581312c0ce0396149a
George Salu <george.salu10@gmail.com>
2026-02-14 23:12:56 -0300

    coder-python - fundamentos python - strings #04

--- coder-python/fundamentos/tipo_string.py
+++ coder-python/fundamentos/tipo_string.py
@@ -39,3 +39,14 @@ print('ing' in frase)
 print(len(frase))
 print(frase.lower())
 print(frase.upper())
+
+# metodos magicos
+a = '123'
+b = ' de oliveira 4'
+print(a + b)
+print(a.__add__(b))
+print(str.__add__(a, b))
+print(len(a))
+print(a.__len__())
+print('1' in a)
+print(a.__contains__('1'))
