5eb0532783e78a731c5af51611dfa5954bd1b8f7
George Salu <george.salu10@gmail.com>
2026-02-16 11:53:13 -0300

    coder-python - fundamentos python - listas #01

---
+++ coder-python/fundamentos/listas.py
@@ -0,0 +1,15 @@
+lista = []
+print(type(lista))
+print(dir(lista))
+print(len(lista))
+lista.append(1)
+lista.append(5)
+print(lista)
+print(len(lista))
+
+nova_lista = [1,2,4,5,'ana','bia']
+print(nova_lista)
+nova_lista.remove(5)
+print(nova_lista)
+nova_lista.reverse()
+print(nova_lista)
