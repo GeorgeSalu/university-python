d24c07c1cb68b896096c7b60b3237818b85d0682
George Salu <george.salu10@gmail.com>
2026-02-16 20:54:00 -0300

    coder-python - estruturas de controle - desafio if else (enunciado)

---
+++ coder-python/estruturas_controle/if_else_1.py
@@ -0,0 +1,11 @@
+# conceito          nota
+# A                 de 10.0 a 9.1
+# A-                de 9.0 a 8.1
+# B                 de 8.0 a 7.1
+# B-                de 7.0 a 6.1
+# C                 de 6.0 a 5.1
+# C-                de 5.0 a 4.1
+# D                 de 4.0 a 3.1
+# D-                de 3.0 a 2.1
+# E                 de 2.0 a 1.1
+# E-                de 1.0 a 0.0
