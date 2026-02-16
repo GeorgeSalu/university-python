016600514f0b4380f212742005f3f12f063496fc
George Salu <george.salu10@gmail.com>
2026-02-16 12:45:06 -0300

    coder-python - fundamentos python - dicionarios #02

--- coder-python/fundamentos/dicionario.py
+++ coder-python/fundamentos/dicionario.py
@@ -15,3 +15,12 @@ print(pessoa.items())
 print(pessoa.get('nome'))
 print(pessoa.get('idade'))
 print(pessoa.get('tags'))
+
+pessoa2 = {'nome': 'Prof. Alberto', 'idade': 43, 'cursos': ['React', 'Python']}
+print(pessoa2)
+pessoa2['idade'] = 44
+print(pessoa2)
+pessoa2.pop('idade')
+print(pessoa2)
+pessoa2.clear()
+print(pessoa2)
