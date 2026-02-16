2ded192455352cd74c529829e131dcc2b16a6cb0
George Salu <george.salu10@gmail.com>
2026-02-16 12:41:34 -0300

    coder-python - fundamentos python - dicionarios #01

---
+++ coder-python/fundamentos/dicionario.py
@@ -0,0 +1,17 @@
+pessoa = {'nome': 'Prof(a). Ana', 'idade': 38,'cursos': ['Inglês', 'Português']}
+print(pessoa)
+print(type(pessoa))
+print(dir(pessoa))
+print(len(pessoa))
+
+print(pessoa['nome'])
+print(pessoa['idade'])
+print(pessoa['cursos'])
+print(pessoa['cursos'][1])
+
+print(pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+print(pessoa.get('nome'))
+print(pessoa.get('idade'))
+print(pessoa.get('tags'))
