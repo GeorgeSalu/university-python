3064451e3852fa676fc4760623391c7108153bed
George Salu <george.salu10@gmail.com>
2026-02-17 20:38:47 -0300

    coder-python - estruturas de controle - desafio for

---
+++ coder-python/estruturas_controle/for_4.py
@@ -0,0 +1,6 @@
+# Função sortear_dado numeros entre 1 e 6
+# for com range 1 a 6
+# se for impar continue
+# se numero for par e igual ao valor sorteado pela função dado
+# imprimier 'ACERTOU' e depois chamar o break
+# se não acertar chamar o else.... print('Nao acertou o numero')
