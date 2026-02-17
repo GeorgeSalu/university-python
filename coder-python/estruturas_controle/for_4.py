b1a6ad69be3cce8c3b41b32539077b6f638e247c
George Salu <george.salu10@gmail.com>
2026-02-17 20:44:17 -0300

    coder-python - estruturas de controle - desafio for (solucao)

--- coder-python/estruturas_controle/for_4.py
+++ coder-python/estruturas_controle/for_4.py
@@ -2,5 +2,19 @@
 # for com range 1 a 6
 # se for impar continue
 # se numero for par e igual ao valor sorteado pela função dado
-# imprimier 'ACERTOU' e depois chamar o break
+# imprimir 'ACERTOU' e depois chamar o break
 # se não acertar chamar o else.... print('Nao acertou o numero')
+from random import randint
+
+def sortear_dado():
+    return randint(1,6)
+
+for i in range(1,7):
+    if i % 2 == 1:
+        continue
+
+    if sortear_dado() == i:
+        print('ACERTOU', i)
+        break
+else:
+    print('nao acertou o numero')
