438ed5af4371c83f83067da2097b796f73d35e79
George Salu <george.salu10@gmail.com>
2026-02-19 15:27:34 -0300

    coder-python - estruturas de controle projetos - usando while infinito

--- coder-python/estruturas_controle_projetos/fibonacci_v1.py
+++ coder-python/estruturas_controle_projetos/fibonacci_v1.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python3
 
 # 0,1,1,2,3,5,8,13,21
 def fibonacci():
@@ -10,3 +11,5 @@ def fibonacci():
         penultimo = ultimo
         ultimo = proximo
 
+if __name__ == '__main__':
+    fibonacci()
