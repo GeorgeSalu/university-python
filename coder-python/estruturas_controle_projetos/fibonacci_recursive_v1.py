70b94aae68358438e9645a1b714295bbce3d65a3
George Salu <george.salu10@gmail.com>
2026-02-20 13:25:17 -0300

    coder-python - estruturas de controle projetos - versao recursiva de fibonacci

---
+++ coder-python/estruturas_controle_projetos/fibonacci_recursive_v1.py
@@ -0,0 +1,11 @@
+def fibonacci(quantidade, sequencia=(0,1)):
+    # impotante: condição de parada
+    if len(sequencia) == quantidade:
+        return sequencia
+    return fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
+
+
+if __name__ == "__main__":
+    # lista os 20 primeiros numeros da sequencia
+    for fib in fibonacci(20):
+        print(fib)
