c225aec0960e92c32e961e12c72e0e17736892d3
George Salu <george.salu10@gmail.com>
2026-02-20 09:44:27 -0300

    coder-python - estruturas de controle projetos - substituindo while por for

--- coder-python/estruturas_controle_projetos/fibonacci_v7.py
+++ coder-python/estruturas_controle_projetos/fibonacci_v7.py
@@ -3,10 +3,8 @@
 # 0,1,1,2,3,5,8,13,21
 def fibonacci(quantidade):
     resultado = [0,1]
-    while True:
+    for i in range(2, quantidade):
         resultado.append(sum(resultado[-2:]))
-        if len(resultado) == quantidade:
-            break
     return resultado
 
 
