c6afe2b766e5f27bc47cc634fd3b6b6cfaf73b7c
George Salu <george.salu10@gmail.com>
2026-02-21 19:21:29 -0300

    coder-python - comprehension - list comprehension #01

--- coder-python/list_comprehension/comprehension_v1.py
+++ coder-python/list_comprehension/comprehension_v1.py
@@ -1,3 +1,9 @@
 # [ expressao for item in lista if condicional ]
 dobros = [i * 2 for i in range(10)]
 print(dobros)
+
+# versão "normal"
+dobros = []
+for i in range(10):
+    dobros.append(i * 2)
+print(dobros)
