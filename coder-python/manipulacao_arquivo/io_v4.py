fad16a1c3bd4ff164dd65e4750acc5e1f983fe34
George Salu <george.salu10@gmail.com>
2026-02-21 11:35:54 -0300

    coder-python - manipualaco de arquivos - usando try finally

--- coder-python/manipulacao_arquivo/io_v4.py
+++ coder-python/manipulacao_arquivo/io_v4.py
@@ -4,6 +4,8 @@ try:
     arquivo = open('pessoas.csv')
     for registro in arquivo:
         print('Nome: {} Idade: {}'.format(*registro.strip().split(',')))
+except IndexError:
+    pass
 finally:
     print('finally')
     arquivo.close()
