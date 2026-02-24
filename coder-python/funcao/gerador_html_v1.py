c00185ef0dc8db5e6659231af0cf1e9518f20fb4
George Salu <george.salu10@gmail.com>
2026-02-24 14:20:12 -0300

    coder-python - funcoes - parametros opcionais

---
+++ coder-python/funcao/gerador_html_v1.py
@@ -0,0 +1,9 @@
+def tag_bloco(text0, classe='success'):
+    return f'<div class="{classe}">{text0}</div>'
+
+
+if __name__ == '__main__':
+    # testes (assertions)
+    assert tag_bloco('Incluindo com sucesso') == '<div class="success">Incluindo com sucesso</div>'
+    assert tag_bloco('impossivel excluir','error') == '<div class="error">impossivel excluir</div>'
+    print(tag_bloco('bloco'))
