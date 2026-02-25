f3d48fe270475227043c1f49b8fe6651f992660f
George Salu <george.salu10@gmail.com>
2026-02-25 10:22:00 -0300

    coder-python - funcoes - packing e unpacking #01

--- coder-python/funcoes/unpacking.py
+++ coder-python/funcoes/unpacking.py
@@ -14,10 +14,15 @@ def soma_n(*numeros):
 if __name__ == '__main__':
     print(soma_2(2,3))
     print(soma_3(2,3,4))
+    
+    # packing
     print(soma_n(1))
     print(soma_n(1,1))
     print(soma_n(2,3,4))
+
+    # unpacking
     tupla_nums = (1,2,3)
     print(soma_3(*tupla_nums))
+
     lista_nums = [1,2,3]
     print(soma_3(*lista_nums))
