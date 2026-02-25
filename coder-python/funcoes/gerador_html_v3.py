30c3b9fa24397d00fcc95248d17d37b3b518e832
George Salu <george.salu10@gmail.com>
2026-02-25 12:14:28 -0300

    coder-python - funcoes - packing e unpacking #02

--- coder-python/funcoes/gerador_html_v3.py
+++ coder-python/funcoes/gerador_html_v3.py
@@ -4,7 +4,7 @@ def tag_bloco(texto, classe='success',inline=False):
     return f'<{tag} class="{classe}">{texto}</{tag}>'
 
 def tag_lista(*itens):
-    lista = ''.join((f'<li>{item}</li>' for item in itens))
+    lista = ''.join(f'<li>{item}</li>' for item in itens)
     return f'<ul>{lista}</ul>'
 
 
