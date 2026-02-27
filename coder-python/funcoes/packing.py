4af2dcafcf2895a637313b5989fe6de7b658c541
George Salu <george.salu10@gmail.com>
2026-02-27 10:06:36 -0300

    coder-python - funcoes - packing nomeado #01

---
+++ coder-python/funcoes/packing.py
@@ -0,0 +1,8 @@
+# **kwargs
+def resultado_f1(**podium):
+    for posicao, piloto in podium.items():
+        print(f'{posicao} -> {piloto}')
+
+
+if  __name__ == '__main__':
+    resultado_f1(primeiro='l. hamilton', segundo='vestapem',terceiro='vettel')
