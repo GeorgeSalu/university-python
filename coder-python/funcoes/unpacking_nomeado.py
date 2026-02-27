7c26c0607b6b87993cf095c57685ac52147ad187
George Salu <george.salu10@gmail.com>
2026-02-27 11:45:41 -0300

    coder-python - funcoes - unpacking nomeado #01

---
+++ coder-python/funcoes/unpacking_nomeado.py
@@ -0,0 +1,10 @@
+# **kwargs
+def resultado_f1(primeiro, segundo, terceiro):
+    print(f'1) {primeiro}')
+    print(f'2) {segundo}')
+    print(f'3) {terceiro}')
+
+
+if  __name__ == '__main__':
+    podium = {'segundo': 'vestappen','primeiro': 'hamilton', 'terceiro': 'vettel'}
+    resultado_f1(**podium)
