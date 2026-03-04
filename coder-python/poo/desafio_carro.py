53ede6177beb8c00fe1c24c935687c1e723a657d
George Salu <george.salu10@gmail.com>
2026-03-04 17:09:20 -0300

    coder-python - programacao orientada a objetos - desafio classe carro

--- coder-python/poo/desafio_carro.py
+++ coder-python/poo/desafio_carro.py
@@ -4,8 +4,8 @@ class Carro:
 if __name__ == '__main__':
     c1 = Carro(180)
 
-    for i in range(25):
+    for _ in range(25):
         print(c1.acelerar(8))
 
-    for i in range(10):
+    for _ in range(10):
         print(c1.frear(delta=20))
