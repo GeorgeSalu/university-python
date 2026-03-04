88a0d8fb54d35ce9f219d34a9ad73bac489b3184
George Salu <george.salu10@gmail.com>
2026-03-04 15:39:07 -0300

    coder-python - programacao orientada a objetos - membros #03

--- coder-python/poo/data.py
+++ coder-python/poo/data.py
@@ -1,5 +1,5 @@
 class Data:
-    def to_str(self):
+    def __str__(self):
         return f'{self.dia}/{self.mes}/{self.ano}'
 
 
@@ -7,10 +7,10 @@ d1 = Data()
 d1.dia = 5
 d1.mes = 12
 d1.ano = 2019
-print(d1.to_str())
+print(d1)
 
 d2 = Data()
 d2.dia = 7
 d2.mes = 11
 d2.ano = 2020
-print(d2.to_str())
+print(d2)
