1666632dd0dea74f199df8c3cd64dab51d9ec5cc
George Salu <george.salu10@gmail.com>
2026-03-09 14:55:34 -0300

    coder-python - programacao orientada a objetos - implementação do vencimento

--- coder-python/poo/todo_v4.py
+++ coder-python/poo/todo_v4.py
@@ -9,18 +9,20 @@ class Projeto:
     def __iter__(self):
         return self.tarefas.__iter__()
 
-    def add(self,descricao, vencimento=None):
+    def add(self, descricao, vencimento=None):
         self.tarefas.append(Tarefa(descricao, vencimento))
 
     def pendentes(self):
         return [tarefa for tarefa in self.tarefas if not tarefa.feito]
 
     def procurar(self, descricao):
-        # possivel IndexError
-        return [tarefa for tarefa in self.tarefas if tarefa.descricao == descricao][0]
+        # Possível IndexError
+        return [tarefa for tarefa in self.tarefas
+                if tarefa.descricao == descricao][0]
 
     def __str__(self):
-        return f'{self.nome} ({len(self.pendentes())} tarefs pendentes'
+        return f'{self.nome} ({len(self.pendentes())} tarefa(s) pendente(s))'
+
 
 class Tarefa:
     def __init__(self, descricao, vencimento=None):
@@ -35,34 +37,40 @@ class Tarefa:
     def __str__(self):
         status = []
         if self.feito:
-            status.append('(Concluida)')
+            status.append('(Concluída)')
         elif self.vencimento:
             if datetime.now() > self.vencimento:
                 status.append('(Vencida)')
             else:
                 dias = (self.vencimento - datetime.now()).days
                 status.append(f'(Vence em {dias} dias)')
-        return f'{self.descricao} '+' '.join(status)
+
+        return f'{self.descricao} ' + ' '.join(status)
+
 
 def main():
     casa = Projeto('Tarefas de Casa')
     casa.add('Passar roupa', datetime.now())
-    casa.add('Lavar Prato', datetime.now() + timedelta(days=1, minutes=12))
+    casa.add('Lavar prato')
     print(casa)
 
-    casa.procurar('Lavar Prato').concluir()
+    casa.procurar('Lavar prato').concluir()
     for tarefa in casa:
         print(f'- {tarefa}')
     print(casa)
 
-    mercado = Projeto('Compras no Mercado')
-    mercado.add('frutas secas')
-    mercado.add('carne')
-    mercado.add('tomate', datetime.now() + timedelta(days=1, minutes=12))
+    mercado = Projeto('Compras no mercado')
+    mercado.add('Frutas secas')
+    mercado.add('Carne')
+    mercado.add('Tomate', datetime.now() + timedelta(days=3, seconds=1))
     print(mercado)
 
+    comprar_carne = mercado.procurar('Carne')
+    comprar_carne.concluir()
     for tarefa in mercado:
         print(f'- {tarefa}')
+    print(mercado)
+
 
 if __name__ == '__main__':
     main()
