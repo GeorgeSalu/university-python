20c1293b07929ca1bfbf9c1d9c14495f9f5fb5a3
George Salu <george.salu10@gmail.com>
2026-03-13 10:26:35 -0300

    coder-python - programacao orientada a objetos avancado - membros de classe vs instancia #03

--- coder-python/poo_avancada/evolucao_v1.py
+++ coder-python/poo_avancada/evolucao_v1.py
@@ -8,3 +8,12 @@ class Humano:
 
     def das_cavernas(self):
         self.especie = 'Homo Neanderthal'
+
+if __name__ == '__main__':
+    jose = Humano('jose')
+    grokn = Humano('grokn')
+    grokn.das_cavernas()
+
+    print(f'Humano.especie: {Humano.especie}')
+    print(f'jose.nome: {jose.especie}')
+    print(f'grokn.nome: {grokn.especie}')
