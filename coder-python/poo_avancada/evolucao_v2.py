dd119c2845243f9b3bcbacff468756db7586a706
George Salu <george.salu10@gmail.com>
2026-03-14 11:30:11 -0300

    coder-python - programacao orientada a objetos avancado - tipos de metodos

---
+++ coder-python/poo_avancada/evolucao_v2.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+class Humano:
+    # atributo de classe
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def das_cavernas(self):
+        self.especie = 'Homo Neanderthalensis'
+        return self
+
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus','Neanderthalensis', 'Sapiens')
+        return ('Australopiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    @classmethod
+    def is_evolido(cls):
+        return cls.especie == cls.especies()[-1]
+
+
+class Neanderthal(Humano):
+    especie = Humano.especies()[-2]
+
+class HomoSapiens(Humano):
+    especie = Humano.especies()[-1]
+
+
+if __name__ == '__main__':
+    jose = HomoSapiens('jose')
+    grokn = Neanderthal('grokn')
+    print(f'Evolucao (a partir da classe): {", ".join(HomoSapiens.especies())}')
+    print(f'Homo sapiens evoluido ? {HomoSapiens.is_evolido()}')
+    print(f'Neanderthal evoluido ? {Neanderthal.is_evolido()}')
+    print(f'Jose evoluido ? {jose.is_evolido()}')
+    print(f'Grok evoluido ? {grokn.is_evolido()}')
