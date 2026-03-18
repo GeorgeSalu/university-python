e3b468fb750d6627387fd70839f4e2e170d55517
George Salu <george.salu10@gmail.com>
2026-03-17 22:14:26 -0300

    coder-python - programacao orientada a objetos avancado - propriedades #01

---
+++ coder-python/poo_avancada/evolucao_v3.py
@@ -0,0 +1,45 @@
+#!/usr/local/bin/python3
+class Humano:
+    # atributo de classe
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+        self._idade = None
+
+    def get_idade(self):
+        return self._idade
+
+    def set_idade(self, idade):
+        if idade < 0:
+            raise ValueError('Idade deve ser um numero positivo')
+        self._idade = idade
+
+
+    def das_cavernas(self):
+        self.especie = 'Homo Neanderthalensis'
+        return self
+
+    # metodo estatico
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus','Neanderthalensis', 'Sapiens')
+        return ('Australopiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    # metodo de classe
+    @classmethod
+    def is_evolido(cls):
+        return cls.especie == cls.especies()[-1]
+
+
+class Neanderthal(Humano):
+    especie = Humano.especies()[-2]
+
+class HomoSapiens(Humano):
+    especie = Humano.especies()[-1]
+
+
+if __name__ == '__main__':
+    jose = HomoSapiens('jose')
+    jose.set_idade(40)
+    print(f'Nome: {jose.nome} Idade: {jose.get_idade()}')
