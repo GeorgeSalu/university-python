5e0ce044e23026bba414714d599f07b34f4cbbc4
George Salu <george.salu10@gmail.com>
2026-03-19 09:54:43 -0300

    coder-python - programacao orientada a objetos avancado - desafio POO

---
+++ coder-python/poo_avancada/contador_objetos.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3
+class ClasseSimples:
+    pass
+
+
+if __name__ == '__main__':
+    lista = [ClasseSimples(), ClasseSimples()]
+    print(ClasseSimples.contador) # esperado 2
