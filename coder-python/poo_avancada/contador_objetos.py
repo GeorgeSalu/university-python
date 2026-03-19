f7631c5857d8763ef59ec8beb994e59046a96c95
George Salu <george.salu10@gmail.com>
2026-03-19 12:21:28 -0300

    coder-python - programacao orientada a objetos avancado - desafio POO resposta #01

--- coder-python/poo_avancada/contador_objetos.py
+++ coder-python/poo_avancada/contador_objetos.py
@@ -1,7 +1,13 @@
 #!/usr/local/bin/python3
 class ClasseSimples:
-    pass
+    contador = 0
 
+    def __init__(self):
+        self.contar()
+
+    @classmethod
+    def contar(cls):
+        cls.contador += 1
 
 if __name__ == '__main__':
     lista = [ClasseSimples(), ClasseSimples()]
