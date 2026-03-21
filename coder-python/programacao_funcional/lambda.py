78629e4c42633b0943ebd1c19053ede7e862a466
George Salu <george.salu10@gmail.com>
2026-03-21 12:30:25 -0300

    coder-python - programacao funcional - funcoes lambda

---
+++ coder-python/programacao_funcional/lambda.py
@@ -0,0 +1,17 @@
+from poo.loja import compra
+
+compras = (
+    { 'quantidade': 2, 'preco': 10 },
+    { 'quantidade': 3, 'preco': 20 },
+    { 'quantidade': 5, 'preco': 14 }
+)
+
+totais = tuple(
+    map(
+        lambda compra: compra['quantidade'] * compra['preco'],
+        compras
+    )
+)
+
+print('precos totais: ', totais)
+print('total geral: ', sum(totais))
