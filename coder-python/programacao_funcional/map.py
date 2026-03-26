1eb44e37e2b31211e44dabb268b0c2549df46964
George Salu <george.salu10@gmail.com>
2026-03-26 10:01:20 -0300

    coder-python - programacao funcional - map #02

--- coder-python/programacao_funcional/map.py
+++ coder-python/programacao_funcional/map.py
@@ -2,3 +2,15 @@
 lista1 = [1,2,3]
 dobro = map(lambda x: x * 2, lista1)
 print(list(dobro))
+
+lista_2 = [
+    { 'nome': 'joao', 'idade': 31 },
+    { 'nome': 'maria', 'idade': 37 },
+    { 'nome': 'jose', 'idade': 41 },
+]
+
+so_nomes = map(lambda p: p['nome'], lista_2)
+print(list(so_nomes))
+
+so_idade = map(lambda p: p['idade'], lista_2)
+print(list(so_idade))
