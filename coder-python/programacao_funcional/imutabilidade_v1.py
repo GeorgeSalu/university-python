8318ad05388fea1de21401da00c909b5f1fefd7e
George Salu <george.salu10@gmail.com>
2026-03-27 14:44:54 -0300

    coder-python - programacao funcional - desafio imutabilidade (resp #02)

--- coder-python/programacao_funcional/imutabilidade_v1.py
+++ coder-python/programacao_funcional/imutabilidade_v1.py
@@ -12,4 +12,19 @@ setlocale(LC_ALL, 'pt_BR')
 # 3. (reduce) juntar tudo para imprimir
 meses_31 = filter(lambda mes: mdays[mes] == 31, range(1, 13))
 meses_nomes = map(lambda mes: month_name[mes], meses_31)
-print(list(meses_nomes))
+juntar_meses = reduce(lambda todos, nome_mes: f'{todos}\n- {nome_mes}', meses_nomes, 'Meses com 31 dias')
+print(juntar_meses)
+
+print(
+    reduce(
+        lambda todos, nome_mes: f'{todos}\n- {nome_mes}',
+        map(
+            lambda mes: month_name[mes],
+            filter(
+                lambda mes: mdays[mes] == 31,
+                range(1, 13)
+            )
+        ),
+        'Meses com 31 dias'
+    )
+)
