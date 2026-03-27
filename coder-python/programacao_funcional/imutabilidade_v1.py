0781be3748617a2ee65eadb26e4b5111fe1dc373
George Salu <george.salu10@gmail.com>
2026-03-27 14:18:08 -0300

    coder-python - programacao funcional - desafio imutabilidade (resp #01)

--- coder-python/programacao_funcional/imutabilidade_v1.py
+++ coder-python/programacao_funcional/imutabilidade_v1.py
@@ -1,9 +1,15 @@
 #!/usr/local/bin/python3
 from locale import setlocale, LC_ALL
-from calendar import month_name
+from calendar import mdays, month_name
 from functools import reduce
 
 # portuguues do Brasil
 setlocale(LC_ALL, 'pt_BR')
 
-# listar todos os meses do ano com 31 dias
+# Listar todos os meses do ano com 31 dias
+# 1. (filter) pegar os indices de todos os meses com 31 dias 1,2,3....
+# 2. (map) transformar os indices em nome
+# 3. (reduce) juntar tudo para imprimir
+meses_31 = filter(lambda mes: mdays[mes] == 31, range(1, 13))
+meses_nomes = map(lambda mes: month_name[mes], meses_31)
+print(list(meses_nomes))
