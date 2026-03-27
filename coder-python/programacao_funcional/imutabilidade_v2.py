760391b2743962e1317b9f2d67ba687c1897ce7a
George Salu <george.salu10@gmail.com>
2026-03-27 15:10:49 -0300

    coder-python - programacao funcional - desafio imutabilidade (resp #03)

--- coder-python/programacao_funcional/imutabilidade_v2.py
+++ coder-python/programacao_funcional/imutabilidade_v2.py
@@ -6,12 +6,12 @@ from functools import reduce
 # Portuguese do Brasil
 setlocale(LC_ALL, 'pt_BR')
 
-def mes_com_31(mes): return mdays[mes] == 31
+def mes_com_31_dias(mes): return mdays[mes] == 31
 
 def get_nome_mes(mes): return month_name[mes]
 
 def juntar_meses(todos, nome_mes): return f'{todos}\n- {nome_mes}'
 
 print(reduce(juntar_meses,
-             map(get_nome_mes, filter(mes_com_31, range(1, 13))),
+             map(get_nome_mes, filter(mes_com_31_dias, range(1, 13))),
              'Meses com 31 dias'))
