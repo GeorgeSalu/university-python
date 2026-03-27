db8db616315b86ddb95482b9563717a3267b7802
George Salu <george.salu10@gmail.com>
2026-03-27 15:24:25 -0300

    coder-python - programacao funcional - abordagem imperativa

---
+++ coder-python/programacao_funcional/imperativo.py
@@ -0,0 +1,12 @@
+#listar todos os meses do ano com 31 dias
+from locale import setlocale, LC_ALL
+from calendar import mdays, month_name
+
+# Portuguese do Brasil
+setlocale(LC_ALL, 'pt_BR')
+
+# lista todos os meses do ano com 31 dias
+print('meses com 31 dias')
+for mes in range(1, 13):
+    if mdays[mes] == 31:
+        print(f'- {month_name[mes]}')
