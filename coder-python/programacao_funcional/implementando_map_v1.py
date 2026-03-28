3481ee79c32a7119780cfb5c8470c8ec2cdf61a8
George Salu <george.salu10@gmail.com>
2026-03-28 04:11:09 -0300

    coder-python - programacao funcional - implementando map #01

--- coder-python/programacao_funcional/implementando_map_v1.py
+++ coder-python/programacao_funcional/implementando_map_v1.py
@@ -7,4 +7,5 @@ def mapear(funcao, lista):
 
 
 if __name__ == '__main__':
-    print(list(mapear(lambda x:x ** 2, [2,3,4])))
+    resultado = mapear(lambda x:x ** 2, [2,3,4])
+    print(list(resultado))
