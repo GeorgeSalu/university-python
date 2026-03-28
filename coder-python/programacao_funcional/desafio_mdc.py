2b6154695cacea4f7eafdb2ab780fad61d25ee2b
George Salu <george.salu10@gmail.com>
2026-03-28 10:52:30 -0300

    coder-python - programacao funcional - desafio mdc

---
+++ coder-python/programacao_funcional/desafio_mdc.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+def mdc(numeros):
+    pass
+
+if __name__ == "__main__":
+    print(mdc([21,7]))
+    print(mdc([125,40]))
+    print(mdc([9,564,66,3]))
+    print(mdc([55,22]))
+    print(mdc([15, 150]))
+    print(mdc([7,9]))
