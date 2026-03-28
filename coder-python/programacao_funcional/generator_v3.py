826e728a653083ddcc9c4d7c1322fc0f9398688e
George Salu <george.salu10@gmail.com>
2026-03-28 03:19:30 -0300

    coder-python - programacao funcional - desafio generators #03

---
+++ coder-python/programacao_funcional/generator_v3.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# desafio: implementar uma função que sempre vai retornar um proximo valor
+def sequence():
+    pass
+
+
+if __name__ == '__main__':
+    seq = sequence()
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
