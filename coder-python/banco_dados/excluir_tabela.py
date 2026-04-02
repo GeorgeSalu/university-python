34a0bef56c3765767e3ae7839aed0f1e3ab7962d
George Salu <george.salu10@gmail.com>
2026-04-02 14:01:53 -0300

    coder-python - banco de dados - excluir tabela se ela existir

--- coder-python/banco_dados/excluir_tabela.py
+++ coder-python/banco_dados/excluir_tabela.py
@@ -4,6 +4,6 @@ from bd import nova_conexao
 with nova_conexao() as conexao:
     try:
         cursor = conexao.cursor()
-        cursor.execute("DROP TABLE emails")
+        cursor.execute("DROP TABLE IF EXISTS emails")
     except ProgrammingError as e:
         print(f'Erro: {e.msg}')
