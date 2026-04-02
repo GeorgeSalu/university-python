5db6181571ed090e3235f9301fd8ea00c9560f16
George Salu <george.salu10@gmail.com>
2026-04-02 14:02:42 -0300

    coder-python - banco de dados - melhor nome de variavel

--- coder-python/banco_dados/excluir_tabela.py
+++ coder-python/banco_dados/excluir_tabela.py
@@ -5,5 +5,5 @@ with nova_conexao() as conexao:
     try:
         cursor = conexao.cursor()
         cursor.execute("DROP TABLE IF EXISTS emails")
-    except ProgrammingError as e:
-        print(f'Erro: {e.msg}')
+    except ProgrammingError as error:
+        print(f'Erro: {error.msg}')
