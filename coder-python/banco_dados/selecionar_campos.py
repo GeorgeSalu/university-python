fbd1bf1bb662100f37074faa8dce430cbf6b7ec8
George Salu <george.salu10@gmail.com>
2026-04-03 14:12:23 -0300

    coder-python - banco de dados - filtrando colunas

---
+++ coder-python/banco_dados/selecionar_campos.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+from bd import nova_conexao
+
+sql = 'SELECT id, nome, tel FROM contatos'
+
+with nova_conexao() as conexao:
+
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+
+        for contato in cursor.fetchall():
+            print('\t'.join(str(campo) for campo in contato))
