43dafb415d590b664cfcf80591dc41ef03fbde01
George Salu <george.salu10@gmail.com>
2026-04-04 19:54:20 -0300

    coder-python - banco de dados - consultar um registro

--- coder-python/banco_dados/selecionar_um_registro.py
+++ coder-python/banco_dados/selecionar_um_registro.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 from bd import nova_conexao
 
-sql = 'SELECT tel, nome FROM contatos LIMIT 3 OFFSET 10'
+sql = 'SELECT tel, nome FROM contatos LIMIT 2 OFFSET 1'
 
 with nova_conexao() as conexao:
         cursor = conexao.cursor()
