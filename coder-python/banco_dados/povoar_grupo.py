d62acdfdc9e0de4b5cc198ce48fa92999553d23d
George Salu <george.salu10@gmail.com>
2026-04-06 14:24:43 -0300

    coder-python - banco de dados - desafio povoa grupo (solucao)

--- coder-python/banco_dados/povoar_grupo.py
+++ coder-python/banco_dados/povoar_grupo.py
@@ -1,1 +1,18 @@
-# desafio: criar script para inserir dados na tabela grupo
+from mysql.connector.errors import ProgrammingError
+from bd import nova_conexao
+
+sql = 'INSERT INTO grupos (descricao) VALUES (%s)'
+args = (
+    ('Casa',),
+    ('Trabalho',)
+)
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.executemany(sql, args)
+        conexao.commit()
+    except ProgrammingError as e:
+        print(f'Erro: {e.msg}')
+    else:
+        print(f'Foram incluidos {cursor.rowcount} registros.')
