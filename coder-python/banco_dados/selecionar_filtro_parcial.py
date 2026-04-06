8b7c5be234555dd6cac8263433b2b5c29c0baff2
George Salu <george.salu10@gmail.com>
2026-04-06 09:11:23 -0300

    coder-python - banco de dados - filtrar linhas #02

---
+++ coder-python/banco_dados/selecionar_filtro_parcial.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+from bd import nova_conexao
+
+sql = "SELECT tel, nome FROM contatos WHERE nome LIKE '%a%'"
+
+with nova_conexao() as conexao:
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+
+        for x in cursor:
+            print(x)
