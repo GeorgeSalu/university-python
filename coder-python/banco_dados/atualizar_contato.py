bc7d04b864e4facc3abb5f32289f718dbd658797
George Salu <george.salu10@gmail.com>
2026-04-06 11:42:27 -0300

    coder-python - banco de dados - desafio atualizacao de contato (solucao)

--- coder-python/banco_dados/atualizar_contato.py
+++ coder-python/banco_dados/atualizar_contato.py
@@ -1,1 +1,16 @@
-# desafio: atualizar contato
+#!/usr/local/bin/python3
+from mysql.connector.errors import ProgrammingError
+from bd import nova_conexao
+
+sql = "UPDATE contatos SET nome = %s WHERE id = %s"
+args = ('Bia alterada',2)
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql, args)
+        conexao.commit()
+    except ProgrammingError as e:
+        print(f'ERRO: {e.msg}')
+    else:
+        print(f'{cursor.rowcount} registro(s) deletado(s).')
