051818a46c37c11966a24819b58910b556d91d06
George Salu <george.salu10@gmail.com>
2026-04-06 10:20:44 -0300

    coder-python - banco de dados - consulta com input

---
+++ coder-python/banco_dados/selecionar_sqm_sql_injection.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+from bd import nova_conexao
+
+sql = "SELECT tel, nome FROM contatos WHERE nome LIKE %s"
+
+with nova_conexao() as conexao:
+        nome = input('Contato a localizar: ')
+        args = (f'%{nome}%',)
+
+        cursor = conexao.cursor()
+        cursor.execute(sql, args)
+
+        for x in cursor:
+            print(x)
