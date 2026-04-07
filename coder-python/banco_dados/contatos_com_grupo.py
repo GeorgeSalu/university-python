cf6c1f3c99050d757ab82b5484c9656d67aedc39
George Salu <george.salu10@gmail.com>
2026-04-07 10:31:20 -0300

    coder-python - banco de dados - consulta com join #01

---
+++ coder-python/banco_dados/contatos_com_grupo.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3
+from mysql.connector.errors import ProgrammingError
+from bd import nova_conexao
+
+sql = """
+    SELECT
+        grupos.descricao as grupo,
+        contatos.nome as contato
+    FROM contatos
+    INNER JOIN grupos ON contatos.grupo_id = grupos.id
+    ORDER BY grupo, contato
+"""
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor(dictionary=True)
+        cursor.execute(sql)
+        contatos = cursor.fetchall()
+    except ProgrammingError as e:
+        print(f'Erro: {e}')
+    else:
+        for contato in contatos:
+            print(f'{contato["grupo"]} - {contato["contato"]}')
