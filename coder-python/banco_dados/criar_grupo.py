f7a96c4ce8f1b1aedb14fbb93df660e066c02393
George Salu <george.salu10@gmail.com>
2026-04-14 14:57:30 -0300

    coder-python - banco de dados - melhoro nome de variavel criar_grupo.py

--- coder-python/banco_dados/criar_grupo.py
+++ coder-python/banco_dados/criar_grupo.py
@@ -2,7 +2,7 @@
 from mysql.connector import ProgrammingError
 from bd import nova_conexao
 
-criar_tabela_grupo = """
+criar_tabela_grupos = """
     CREATE TABLE IF NOT EXISTS grupos (
         id INT AUTO_INCREMENT PRIMARY KEY,
         descricao VARCHAR(30)
@@ -20,7 +20,7 @@ try:
     with nova_conexao() as conexao:
         try:
             cursor = conexao.cursor()
-            cursor.execute(criar_tabela_grupo)
+            cursor.execute(criar_tabela_grupos)
             cursor.execute(alterar_tabela_contato_1)
             cursor.execute(alterar_tabela_contato_2)
         except ProgrammingError as erro:
