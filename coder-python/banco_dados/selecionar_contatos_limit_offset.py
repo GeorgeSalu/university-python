b41bcd8a451616647c6e3c12d7f8d728a7dc0168
George Salu <george.salu10@gmail.com>
2026-04-14 15:55:32 -0300

    coder-python - banco de dados - melhora nome variavel except

--- coder-python/banco_dados/selecionar_contatos_limit_offset.py
+++ coder-python/banco_dados/selecionar_contatos_limit_offset.py
@@ -10,8 +10,8 @@ with nova_conexao() as conexao:
         cursor = conexao.cursor()
         cursor.execute(sql, args)
         contatos = cursor.fetchall()
-    except ProgrammingError as e:
-        print(f'Erro: {e.msg}')
+    except ProgrammingError as erro:
+        print(f'Erro: {erro.msg}')
     else:
         for contato in contatos:
             print(f'id : {contato[2]:2d} - Nome: {contato[0]:20s} - Telefone: {contato[1]}')
