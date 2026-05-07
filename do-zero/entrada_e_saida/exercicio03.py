d5f09864a8d14752d09a5b3cb57611998eca3cb5
George Salu <george.salu10@gmail.com>
2026-05-07 14:26:58 -0300

    do zero ao avancado - entrada e saida - exercicio 03

---
+++ do-zero/entrada_e_saida/exercicio03.py
@@ -0,0 +1,4 @@
+# exercício 2
+# desenvolva um algoritmo que solicite ao usuario o preco de um produto
+# e um percentual de desconto a ser aplicado a ele
+# calcule e exiba o valor do desconto e o preço final do produto
