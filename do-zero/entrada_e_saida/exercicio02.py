604373e419647254a7f4eb52867b928761417e03
George Salu <george.salu10@gmail.com>
2026-05-07 14:00:28 -0300

    do zero ao avancado - entrada e saida - exercicio 02

---
+++ do-zero/entrada_e_saida/exercicio02.py
@@ -0,0 +1,4 @@
+# exercício 2
+# desenvolva m algoritmo que solicite ao usuário uma quantidade de dias,
+# de horas, de minutos e de segundos
+# calcule o total de segundos resultante e imprima n tela para o usuario
