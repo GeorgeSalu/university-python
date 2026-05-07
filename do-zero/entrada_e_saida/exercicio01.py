89664eadf25a1b9edb868bfbe598d36fce9b89b5
George Salu <george.salu10@gmail.com>
2026-05-07 13:58:51 -0300

    do zero ao avancado - entrada e saida - exercicio 01 (solucao)

--- do-zero/entrada_e_saida/exercicio01.py
+++ do-zero/entrada_e_saida/exercicio01.py
@@ -1,3 +1,9 @@
 # exercício 1
 # desenvolva um algoritmo que solicite ao usuario dois numeros inteiros
 # imprima a soma destes numeros na tela
+
+x = int(input("Digite o primeiro numero: "))
+y = int(input("Digite o segundo numero: "))
+
+res = f"O resultado da soma de {x} com {y} sera {x + y}"
+print(res)
