51d49dbb07a2c5aa341e67506ee037829c2ffd90
George Salu <george.salu10@gmail.com>
2026-05-07 09:40:11 -0300

    do zero ao avancado - entrada e saida - fatiamento de strings

--- do-zero/entrada_e_saida/fatiamento_de_strings.py
+++ do-zero/entrada_e_saida/fatiamento_de_strings.py
@@ -1,3 +1,5 @@
 s1 = "Homem de ferro"
 print(s1[0:5])
+# podemos reescrever o fatiamento acima, omitindo o início da string, ao fazer isso estamos dizendo que a string
+# será pega desde o seu início até o caracter indicado
 print(s1[:5])
