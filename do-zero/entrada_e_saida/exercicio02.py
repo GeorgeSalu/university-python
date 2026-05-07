e54c4211a93f149312160d46a73fa8f84a24ee7e
George Salu <george.salu10@gmail.com>
2026-05-07 14:24:11 -0300

    do zero ao avancado - entrada e saida - exercicio 02 (solucao)

--- do-zero/entrada_e_saida/exercicio02.py
+++ do-zero/entrada_e_saida/exercicio02.py
@@ -2,3 +2,13 @@
 # desenvolva m algoritmo que solicite ao usuário uma quantidade de dias,
 # de horas, de minutos e de segundos
 # calcule o total de segundos resultante e imprima n tela para o usuario
+
+d = int(input("quantos dias: "))
+h = int(input("quantos horas: "))
+m = int(input("quantos minutos: "))
+s = int(input("quantos segundos: "))
+
+total = s + (m * 60) + (h * 60 * 60) + (d * 24 * 60 * 60)
+
+res = f"O total de segundos calculado é de {total}"
+print(res)
