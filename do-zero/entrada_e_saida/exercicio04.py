6b88f5884956beb525c0ebad5c42b35fc5cacc83
George Salu <george.salu10@gmail.com>
2026-05-07 20:47:35 -0300

    do zero ao avancado - entrada e saida - exercicio 04 (solucao)

--- do-zero/entrada_e_saida/exercicio04.py
+++ do-zero/entrada_e_saida/exercicio04.py
@@ -2,3 +2,8 @@
 # desenvolva um algoritmo que converta uma temperatura em Celsius (C) para Faherenheit (F).
 # a equação de conversão é
 # 9 x Celsius, tudo dividido por 5 e somado com 32
+
+c = float(input("Digite a temperatura em Celsius: "))
+f = (9 * c) / 5 + 32
+
+print(f"Celsius: {c} Fahrenheit: {f}")
