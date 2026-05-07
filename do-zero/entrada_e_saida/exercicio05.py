846d806b2738303b980b0391622aff27fc807d71
George Salu <george.salu10@gmail.com>
2026-05-07 20:51:36 -0300

    do zero ao avancado - entrada e saida - exercicio 05

--- do-zero/entrada_e_saida/exercicio05.py
+++ do-zero/entrada_e_saida/exercicio05.py
@@ -1,4 +1,4 @@
 # Exercício 5
-# desenvolva um algoritmo que seja capaz de calcular a soma e a subtracao entre 2 valores com virgula
-# crie duas variaveis de teste, uma que teste se a soma é maior do que 10
-# outra que esta se a subtracao é maior do que 0. imprima tudo na tela
+# desenvolva um algoritmo que seja capaz de calcular a soma e a subtracao entre 2 valores com vírgula
+# crie duas variáveis de teste, uma que teste se a soma é maior do que 10
+# outra que testa se a subtração é maior do que 0. Imprima tudo na tela
