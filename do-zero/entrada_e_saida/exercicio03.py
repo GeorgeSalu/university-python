724ed4eadd5cf061ab78a499c7157d73efdf9ac6
George Salu <george.salu10@gmail.com>
2026-05-07 17:01:30 -0300

    do zero ao avancado - entrada e saida - exercicio 03 (solucao)

--- do-zero/entrada_e_saida/exercicio03.py
+++ do-zero/entrada_e_saida/exercicio03.py
@@ -2,3 +2,13 @@
 # desenvolva um algoritmo que solicite ao usuario o preco de um produto
 # e um percentual de desconto a ser aplicado a ele
 # calcule e exiba o valor do desconto e o preço final do produto
+
+preco = float(input("digite o preco do produto : "))
+p = float(input("digite o percentual de desconto : "))
+
+desconto = preco * (p/100)
+final = preco - desconto
+
+print(f"O preco do produto é {preco}, desconto aplicado de {p}%")
+print(f"Valor de desconto calculado: {desconto}")
+print(f"valor final do produto: {final}")
