5fa1205491b6ce50cd2a735b8ff8de27744742c0
George Salu <george.salu10@gmail.com>
2026-05-06 21:29:34 -0300

    do zero ao avancado - entrada e saida - comentarios

---
+++ do-zero/entrada_e_saida/comentarios.py
@@ -0,0 +1,14 @@
+# isto é um comentario de uma unicc linha
+print("isto não é um comentario")
+
+'''
+isto é um comentario 
+de multiplas linhas
+'''
+print("isto não é um comentario")
+
+"""
+isto é um comentario 
+de multiplas linhas
+"""
+print("isto não é um comentario")
