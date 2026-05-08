70c889a897dcbc6b4345915e6d0f4446642cd621
George Salu <george.salu10@gmail.com>
2026-05-07 21:36:22 -0300

    do zero ao avancado - estruturas condicionais - exercicio 01 (solucao)

--- do-zero/estruturas_condicionais/exercicio01.py
+++ do-zero/estruturas_condicionais/exercicio01.py
@@ -3,3 +3,15 @@
 # crie duas variáveis de teste, uma que teste se a soma é maior do que 10
 # outra que testa se a subtração é maior do que 0. Imprima tudo na tela
 
+x = float(input("Digite o primeiro numero : "))
+y = float(input("Digite o segundo numero : "))
+
+soma = x + y
+subtracao = x - y
+print(f"Soma : {soma:.2f} Subtracao: {subtracao:.2f}")
+
+if soma > 10:
+    print(f"A soma é maior do que 10")
+
+if subtracao < 0:
+    print(f"A subtracao é menor do que 0")
