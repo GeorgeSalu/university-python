b2264b9146d6826d75fdb664058770c035d52075
George Salu <george.salu10@gmail.com>
2026-05-07 21:34:30 -0300

    do zero ao avancado - estruturas condicionais - exercicio 01

---
+++ do-zero/estruturas_condicionais/exercicio01.py
@@ -0,0 +1,5 @@
+# Exercício 5
+# desenvolva um algoritmo que seja capaz de calcular a soma e a subtracao entre 2 valores com vírgula
+# crie duas variáveis de teste, uma que teste se a soma é maior do que 10
+# outra que testa se a subtração é maior do que 0. Imprima tudo na tela
+
