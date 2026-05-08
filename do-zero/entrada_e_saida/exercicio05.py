74da583042bab39cea0234e453db866085862830
George Salu <george.salu10@gmail.com>
2026-05-07 21:02:19 -0300

    do zero ao avancado - entrada e saida - exercicio 05 (solucao)

--- do-zero/entrada_e_saida/exercicio05.py
+++ do-zero/entrada_e_saida/exercicio05.py
@@ -2,3 +2,15 @@
 # desenvolva um algoritmo que seja capaz de calcular a soma e a subtracao entre 2 valores com vírgula
 # crie duas variáveis de teste, uma que teste se a soma é maior do que 10
 # outra que testa se a subtração é maior do que 0. Imprima tudo na tela
+
+x = float(input("Digite o primeiro numero : "))
+y = float(input("Digite o segundo numero : "))
+
+soma = x + y
+subtracao = x - y
+testeA = soma > 10
+testeB = subtracao < 0
+
+print(f"Soma : {soma:.2f} Subtracao: {subtracao:.2f}")
+print(f"A soma é maior que 10 ? {testeA}")
+print(f"A subtracao é menor do que 0 ? {testeB}")
