078e50fc8303ef55cf38f025156311dc5383cfc9
George Salu <george.salu10@gmail.com>
2026-05-08 09:32:18 -0300

    do zero ao avancado - estruturas condicionais - exercicio 02 (solucao)

--- do-zero/estruturas_condicionais/exercicio02.py
+++ do-zero/estruturas_condicionais/exercicio02.py
@@ -3,3 +3,11 @@
 # para fins de simplificação, despreze o dia e o mes do ano
 # após o cálculo, verifique se a idade é maior ou igual a 18 ano e apresente na tela uma mensagem
 # imformando que já é possivel tirar a carteira de motorista caso seja maior
+
+ano_nascimento = int(input("Qual o seu ano de nascimento ? "))
+ano_atual = int(input("Em qual ano estamos ? "))
+idade = ano_atual - ano_nascimento
+
+print(f"Voce tem {idade} anos de idade.")
+if idade >= 18:
+    print("vc pode ter carteira de motorista")
