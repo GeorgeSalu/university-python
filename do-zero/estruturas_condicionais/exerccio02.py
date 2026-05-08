f709b1d11fca39ab12004d9752b11916fae28be0
George Salu <george.salu10@gmail.com>
2026-05-07 21:47:31 -0300

    do zero ao avancado - estruturas condicionais - exercicio 02

---
+++ do-zero/estruturas_condicionais/exerccio02.py
@@ -0,0 +1,5 @@
+# Exercício 2
+# desenvolva um algoritmo que solicite o seu ano de nascimento e o ano atual. Calcule a sua idade e apresente na tela
+# para fins de simplificação, despreze o dia e o mes do ano
+# após o cálculo, verifique se a idade é maior ou igual a 18 ano e apresente na tela uma mensagem
+# imformando que já é possivel tirar a carteira de motorista caso seja maior
