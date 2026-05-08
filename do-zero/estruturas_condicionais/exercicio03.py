e0cf69a5059d11d34c40ad7675cecbe40e2d97b6
George Salu <george.salu10@gmail.com>
2026-05-08 10:04:15 -0300

    do zero ao avancado - estruturas condicionais - exercicio 03

---
+++ do-zero/estruturas_condicionais/exercicio03.py
@@ -0,0 +1,5 @@
+# Exercício 3
+# Uma empresa concedeu um bonus de 20% para todos os seus funcionaios com, mas de 5anos de empresa.
+# Todos os outros que não se enquadram nesta categoria receberam uma bonificação de 10%, somente.
+# Escreva um algoritmo que leia o salário do funcionario e o seu tempo de empresa,
+# e apresente a bonificação de cada funcionario na tela
