21adab9d0183ecda5117702a2857dab232227587
George Salu <george.salu10@gmail.com>
2026-05-08 09:35:26 -0300

    do zero ao avancado - estruturas condicionais - condicional composta if-else

---
+++ do-zero/estruturas_condicionais/condicional_compost.py
@@ -0,0 +1,7 @@
+x = int(input("digite um valor inteiro: "))
+y = int(input("digite outro valor inteiro: "))
+
+if x > y:
+    print("O primeiro valor é maior que o segundo")
+else:
+    print("o segundo valor é maior que o primeiro")
