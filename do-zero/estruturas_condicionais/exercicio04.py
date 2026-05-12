c59bad8629c00212ca834df167249a2a9f37bf4b
George Salu <george.salu10@gmail.com>
2026-05-12 16:53:52 -0300

    do zero ao avancado - estruturas condicionais - exercicio 04 (solucao)

--- do-zero/estruturas_condicionais/exercicio04.py
+++ do-zero/estruturas_condicionais/exercicio04.py
@@ -1,4 +1,4 @@
-# Exercício 3
+# Exercício 4
 # Um aluno, para passar de ano,precisa estar aprovado em todas as materias que ele esta cursando
 # assuma que a média para aprovação é a partir de 7, e que o aluno cursa 3 materias, somente
 # escreva um algoritmo que leia a nota final do aluno em casa materia, e informe na tela se ele passou de ano ou não
