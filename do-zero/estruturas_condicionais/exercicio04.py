6c3ee9cbe4a1bbcd40659c53e0b4a28b20c17dcd
George Salu <george.salu10@gmail.com>
2026-05-12 16:52:56 -0300

    do zero ao avancado - estruturas condicionais - exercicio 04 (solucao)

--- do-zero/estruturas_condicionais/exercicio04.py
+++ do-zero/estruturas_condicionais/exercicio04.py
@@ -2,3 +2,12 @@
 # Um aluno, para passar de ano,precisa estar aprovado em todas as materias que ele esta cursando
 # assuma que a média para aprovação é a partir de 7, e que o aluno cursa 3 materias, somente
 # escreva um algoritmo que leia a nota final do aluno em casa materia, e informe na tela se ele passou de ano ou não
+
+m1 = float(input("Qual a primeira nota ? : "))
+m2 = float(input("Qual a segunda nota ? : "))
+m3 = float(input("Qual a terceira nota ? : "))
+
+if (m1 >= 7) and (m2 >= 7) and (m3 >= 7):
+    print("Aprovado")
+else:
+    print("Reprovado")
