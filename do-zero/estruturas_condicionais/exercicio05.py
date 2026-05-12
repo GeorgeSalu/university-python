663f7401770787c738b2423795346dc686914510
George Salu <george.salu10@gmail.com>
2026-05-12 16:56:03 -0300

    do zero ao avancado - estruturas condicionais - exercicio 05

---
+++ do-zero/estruturas_condicionais/exercicio05.py
@@ -0,0 +1,4 @@
+# Exercício 5
+# Escreva um algoritmo que le um valor inteiro qualquer
+# Após verifique se este valor está contido dentro dos seguintes intervalos: -100 < x < -1 ou 1 < x < 100
+# imprima na tela uma mensagem caso ele esteja em um dos intervalos
