26346e6986aea0f76b2e3bdca3c1aa9965e28ce9
George Salu <george.salu10@gmail.com>
2026-05-13 17:52:12 -0300

    do zero ao avancado - estruturas condicionais - condicional de multipla escolha

---
+++ do-zero/estruturas_condicionais/condicional_multipla.py
@@ -0,0 +1,9 @@
+x = int(input("digite um valor inteiro : "))
+y = int(input("digite outro valor inteiro : "))
+
+if x > y:
+    print(f"o primeiro valor e maior que o segundo")
+elif x < y:
+    print(f"o segundo valor e maior que o primeiro")
+else:
+    print(f"ambos são iguais")
