daab20cf66bccaf6151272352d308c36bd8fe01a
George Salu <george.salu10@gmail.com>
2026-05-13 15:25:49 -0300

    do zero ao avancado - estruturas condicionais - exercicio 05 (solucao)

--- do-zero/estruturas_condicionais/exercicio05.py
+++ do-zero/estruturas_condicionais/exercicio05.py
@@ -2,3 +2,7 @@
 # Escreva um algoritmo que le um valor inteiro qualquer
 # Após verifique se este valor está contido dentro dos seguintes intervalos: -100 < x < -1 ou 1 < x < 100
 # imprima na tela uma mensagem caso ele esteja em um dos intervalos
+
+x = int(input("digite um valor inteiro : "))
+if ((x > 1) and (x < 100)) or ((x < -1) and (x > -100)):
+    print("um dos criterios de intervalo foi atingido")
