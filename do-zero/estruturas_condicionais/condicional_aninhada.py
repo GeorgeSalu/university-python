530e68b74044f8da8581cccc5fd8c7022f2dbeac
George Salu <george.salu10@gmail.com>
2026-05-13 15:28:36 -0300

    do zero ao avancado - estruturas condicionais - condicionais aninhadas

---
+++ do-zero/estruturas_condicionais/condicional_aninhada.py
@@ -0,0 +1,10 @@
+x = int(input("digite um valor inteiro : "))
+y = int(input("digite outro valor inteiro : "))
+
+if x > y:
+    print("o primeiro valor é maior que o segundo")
+else:
+    if x < y:
+        print("o segundo valor é maior que o primeiro")
+    else:
+        print("ambos numeros são iguais")
