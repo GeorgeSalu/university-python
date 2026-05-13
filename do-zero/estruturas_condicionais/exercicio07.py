abb35885f73f919560a84a87c1ecb6ccb397fb32
George Salu <george.salu10@gmail.com>
2026-05-13 15:38:34 -0300

    do zero ao avancado - estruturas condicionais - exercicio 07

---
+++ do-zero/estruturas_condicionais/exercicio07.py
@@ -0,0 +1,6 @@
+# Exercício 7
+# Escreva um algoritmo que o usuário escolhe se ele quer comprar maças, larnjas ou bananas.
+# Devera ser apresentado na tela um menu com a opção 1 para maça, 2 para laranja e 3 para banana
+# Após escolhida a fruta, deve-se digitar quantas unidades se quer comprar
+# O algoritmo deve calcular o preço total a pagar do produto escolhido e mostrá-lo na tela
+# Considere que uma maça custa 2,30, uma laranja 3,60 e uma bananas 1,85
