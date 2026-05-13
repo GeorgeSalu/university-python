93cacd0e2afba536ecd7fe61b0023a1a7e71269b
George Salu <george.salu10@gmail.com>
2026-05-13 18:45:56 -0300

    do zero ao avancado - estruturas condicionais - exercicio 09

--- do-zero/estruturas_condicionais/exercicio09.py
+++ do-zero/estruturas_condicionais/exercicio09.py
@@ -1,3 +1,3 @@
-# Exercício 8
+# Exercício 9
 # Escreva um algoritmo que leia dois valores numericos e que pergunte ao usuario qual operação ele deseja realizar:
 # adição(+), subtração(-), multiplicação(*) ou divisão(/) exiba na tela o resultado da operação desejada
