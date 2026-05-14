098795714ef368bf00250ce1fb1763b5f4a565f7
George Salu <george.salu10@gmail.com>
2026-05-14 14:12:59 -0300

    do zero ao avancado - estruturas condicionais - exercicio 09 (solucao)

--- do-zero/estruturas_condicionais/exercicio09.py
+++ do-zero/estruturas_condicionais/exercicio09.py
@@ -1,3 +1,29 @@
 # Exercício 9
 # Escreva um algoritmo que leia dois valores numericos e que pergunte ao usuario qual operação ele deseja realizar:
 # adição(+), subtração(-), multiplicação(*) ou divisão(/) exiba na tela o resultado da operação desejada
+
+print("calculadora")
+print("+ adição")
+print("- subtracao")
+print("* multiplicacao")
+print("/ divisao")
+print("digite outra tecla para sair")
+
+op = input("Qual operação deseja realizar ? ")
+x = int(input("digite o primeiro numero : "))
+y = int(input("digite o segundo numero : "))
+
+if (op == "+"):
+    res = x + y
+    print(f"Resultado: {x} + {y} = {res} ")
+elif (op == "-"):
+    res = x - y
+    print(f"Resultado: {x} - {y} = {res} ")
+elif (op == "*"):
+    res = x * y
+    print(f"Resultado: {x} * {y} = {res} ")
+elif (op == "/"):
+    res = x / y
+    print(f"Resultado: {x} / {y} = {res} ")
+else:
+    print("Operacao invalida")
