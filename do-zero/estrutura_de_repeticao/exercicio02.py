9d6e002df553c0ec8ee5442385fdf1b83a1c6aeb
George Salu <george.salu10@gmail.com>
2026-05-14 16:04:12 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 02 (solucao)

--- do-zero/estrutura_de_repeticao/exercicio02.py
+++ do-zero/estrutura_de_repeticao/exercicio02.py
@@ -3,3 +3,12 @@
 # onde os limites de início e fim dessa sequência são determinados pelo usuário que executa o programa
 # crie um algoritmo que leia os valores de início e de fim e imprima na tela o intervalo de números
 # pares correspondentes
+
+inicial = int(input("digite o valor inicial da contagem ? "))
+final = int(input("digite o valor final da contagem ? "))
+
+i = inicial
+while i <= final:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
