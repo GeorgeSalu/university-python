678dc486c32e04337413b52361d8ea3308d5b6eb
George Salu <george.salu10@gmail.com>
2026-05-14 15:51:23 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 02

---
+++ do-zero/estrutura_de_repeticao/exercicio02.py
@@ -0,0 +1,4 @@
+# suponhamos que desejamos exibir uma série de números na tela
+# onde os limites de início e fim dessa sequência são determinados pelo usuário que executa o programa
+# crie um algoritmo que leia os valores de início e de fim e imprima na tela o intervalo de números
+# pares correspondentes
