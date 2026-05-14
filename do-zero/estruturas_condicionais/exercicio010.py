18e2b8757698be8963b891ed64a713945670ca28
George Salu <george.salu10@gmail.com>
2026-05-14 14:19:38 -0300

    do zero ao avancado - estruturas condicionais - exercicio 10

---
+++ do-zero/estruturas_condicionais/exercicio010.py
@@ -0,0 +1,9 @@
+# Exercício 9
+# Uma loja de departamentos está oferecendo diferentes formas de pagamento
+# conforme opções listadas a seguir
+# faça um algoritmo que leia o valor total de uma compra e calcule o valor de pagamento final
+# conforme a opção escolhida
+# pagamento a vista - conceder desconto de 5 %
+# pagamento em 3x - valor não sofre alterações
+# pagamento em 5x - acréscimo de 2%
+# pagamento em 10x - acréscimo de 8%
