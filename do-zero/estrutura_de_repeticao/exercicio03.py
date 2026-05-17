e7bbb3d94fa8b581c9e3ebc20ac44f3f923e6c13
George Salu <george.salu10@gmail.com>
2026-05-17 09:43:10 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 03 (solucao)

--- do-zero/estrutura_de_repeticao/exercicio03.py
+++ do-zero/estrutura_de_repeticao/exercicio03.py
@@ -1,3 +1,11 @@
 # Exercício 3
 # crie um programa que calcule a tabuada de um número escolhido pelo usuario
 # imprima na tela a tabuada deste numero de 1 a 10
+
+num = int(input("Digite um numero para calcular sua tabuada: "))
+
+print(f"Tabuada do numero {num} ")
+i = 1
+while i <= 10:
+    print(f"{num} x {i} = {num * i}")
+    i = i + 1
