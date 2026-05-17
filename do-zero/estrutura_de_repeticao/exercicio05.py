c51984dae8ef58b446ac6b74915b229bb5c3837a
George Salu <george.salu10@gmail.com>
2026-05-17 11:10:09 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 05 (solucao)

--- do-zero/estrutura_de_repeticao/exercicio05.py
+++ do-zero/estrutura_de_repeticao/exercicio05.py
@@ -3,3 +3,14 @@
 # Porém, para calcular a multiplicação, utilize somente operações de soma e subtração
 # Lembrando que uma multiplicação pode ser considerada como uma somatorio sucessivo
 # utilize esta logica para construir o seu algoritmo.
+
+x = int(input("Digite um valor: "))
+y = int(input("Digite um segundo valor: "))
+
+multi = 0
+i = 1
+while i <= x:
+    multi = multi + y
+    i = i + 1
+
+print(f"Resultado da multiplicacao: {x}x{y} = {multi}")
