c7a2ea8345bafc6b08ba98efd0fe2fe989fc3a05
George Salu <george.salu10@gmail.com>
2026-05-17 09:44:31 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 04

---
+++ do-zero/estrutura_de_repeticao/exercicio04.py
@@ -0,0 +1,3 @@
+# Exercício 4
+# crie um programa que calcule a soma de 5 valores inteiros, cada valor a ser somado é digitado pelo usuario
+# imprima a soma na tela, após a soma,calcule também a média dos valores e mostre na tela
