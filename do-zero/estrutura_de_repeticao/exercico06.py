7182f7abb7f17297e692f8d00e692335c0cd8e2f
George Salu <george.salu10@gmail.com>
2026-05-17 11:13:12 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 06

---
+++ do-zero/estrutura_de_repeticao/exercico06.py
@@ -0,0 +1,7 @@
+# Exercício 6
+# Escreva um algoritmo que obtenha do usuario um valor inicial e um valor final
+# para este intervalo especificado pelo usuario, calcule e mostre na tela:
+#! A quantidade de números inteiros e positivos
+#! A quantidade de números pares
+# A quantidade de números impares
+#! A respetiva média de cada um dos itens anteriores
