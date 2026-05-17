ba38b5e5d6a20d06100f506fd38bd47f48f82446
George Salu <george.salu10@gmail.com>
2026-05-17 10:22:53 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 04 (solucao)

--- do-zero/estrutura_de_repeticao/exercicio04.py
+++ do-zero/estrutura_de_repeticao/exercicio04.py
@@ -1,3 +1,14 @@
 # Exercício 4
 # crie um programa que calcule a soma de 5 valores inteiros, cada valor a ser somado é digitado pelo usuario
 # imprima a soma na tela, após a soma,calcule também a média dos valores e mostre na tela
+
+soma = 0
+i = 1
+while i <= 5:
+    x = int(input(f"Digite um valor {i}: "))
+    soma = soma + x
+    i = i + 1
+
+print(f"Somatorio: {soma}")
+media = soma / 5
+print(f"Media final: {media}")
