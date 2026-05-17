5bafaf21e00e42f4ed96f886f1e495aea4473ca0
George Salu <george.salu10@gmail.com>
2026-05-17 10:25:29 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 05

--- do-zero/estrutura_de_repeticao/exercicio05.py
+++ do-zero/estrutura_de_repeticao/exercicio05.py
@@ -1,4 +1,4 @@
-# Exercício 4
+# Exercício 5
 # Escreva um algoritmo que leia dois valores e imprima na tela o resultado da multiplicacao ed ambos
 # Porém, para calcular a multiplicação, utilize somente operações de soma e subtração
 # Lembrando que uma multiplicação pode ser considerada como uma somatorio sucessivo
