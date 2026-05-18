124a45c7fc0340024755c8a5a05e5a662e904698
George Salu <george.salu10@gmail.com>
2026-05-18 11:04:24 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 07 (solucao)

--- do-zero/estrutura_de_repeticao/exercicio07.py
+++ do-zero/estrutura_de_repeticao/exercicio07.py
@@ -1,3 +1,7 @@
 # Exercício 7
 # Faça um programa para exibir na tela uma contagem regressiva do lançamento de um foguete
 # iniciando em 10 até 0, escreva após o 0, a palavra: Fogo!
+
+for contador in range(10,-1,-1):
+    print(contador)
+print('fogo')
