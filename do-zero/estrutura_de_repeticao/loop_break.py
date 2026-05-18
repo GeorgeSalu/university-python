c130c0ff54f469cb4fd7c042d503664e2d7bd972
George Salu <george.salu10@gmail.com>
2026-05-18 09:57:51 -0300

    do zero ao avancado - estruturas de repeticao - interrompendo loop com break

---
+++ do-zero/estrutura_de_repeticao/loop_break.py
@@ -0,0 +1,8 @@
+print("digite uma mensagem que irei repetir para voce")
+print("para encerrar escreva sair")
+while True:
+    texto = input('')
+    print(texto)
+    if texto == 'sair':
+        break
+print("finalizando")
