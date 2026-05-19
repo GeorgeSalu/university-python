b02d4b5eefb6c3d280fb8e36321bc54deec63aef
George Salu <george.salu10@gmail.com>
2026-05-19 13:21:40 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 12

---
+++ do-zero/estrutura_de_repeticao/exercicio12.py
@@ -0,0 +1,7 @@
+# Exercício 10
+# Escreva um algoritmo que obtenha do usuario uma frase de tamanho entre 10 e 30 caractres
+# (faça a validação deste dado)
+#
+# após a frase ter sido digitada corretamente,
+# faça a impressão dela na tela de maneira exata como foi digitada e, em seguida,
+# remova os espaços da frase e imprima novamente, sem espaços
