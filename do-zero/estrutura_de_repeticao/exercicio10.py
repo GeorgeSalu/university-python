d2f562b37df93bbf34acc66a12e17dfff2b92def
George Salu <george.salu10@gmail.com>
2026-05-19 09:49:32 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 10

---
+++ do-zero/estrutura_de_repeticao/exercicio10.py
@@ -0,0 +1,7 @@
+# Exercício 9
+# Escreva um algoritmo que repetidamente pergunte ao usuario qual sua idade e seu sexo (M ou F)
+# para cada resposta o programa deve imprimier a mensagem
+# "Boa noite, Senhor, sua idade é <IDADE>" caso genero masculino
+# "Boa noite, Senhora, sua idade é <IDADE>" caso genero feminino
+#
+# O progrma deve encerrar quando o usuario digitar uma idade negativa
