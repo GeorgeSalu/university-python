889b2b8f5d2b468ebceb3fd177bb9cd63a55be90
George Salu <george.salu10@gmail.com>
2026-05-19 08:40:24 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 08 (solucao)

--- do-zero/estrutura_de_repeticao/exercicio08.py
+++ do-zero/estrutura_de_repeticao/exercicio08.py
@@ -1,3 +1,14 @@
 # Exercício 7
 # Escreva um algoritmo que calcule a media dos números pares de 1 ate 100 (1 e 100 inclusos)
 # implemente o laço usando for
+
+soma = 0
+qtd = 0
+
+for i in range(1,101,1):
+    if i % 2 == 0:
+        soma = soma + i
+        qtd = qtd + 1
+
+media = soma / qtd
+print(f"A media dos pares de 1 ate 100 é : {media}")
