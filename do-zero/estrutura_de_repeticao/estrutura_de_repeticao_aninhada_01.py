3e2671ac3226beebef7f055d9ef906a08d266f08
George Salu <george.salu10@gmail.com>
2026-05-19 08:58:37 -0300

    do zero ao avancado - estruturas de repeticao - estruturas de repeticao aninhadas 01

---
+++ do-zero/estrutura_de_repeticao/estrutura_de_repeticao_aninhada_01.py
@@ -0,0 +1,8 @@
+tabuada = 1
+while tabuada <= 10:
+    print(f"tabuada do {tabuada}")
+    i = 1
+    while i <= 10:
+        print(f"{tabuada} X {i} = {tabuada * i}")
+        i += 1
+    tabuada += 1
