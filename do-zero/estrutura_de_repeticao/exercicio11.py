452b78ddc4223be7c20dd62b52e6abbf22dd7c98
George Salu <george.salu10@gmail.com>
2026-05-19 13:19:23 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 11 (solucao)

--- do-zero/estrutura_de_repeticao/exercicio11.py
+++ do-zero/estrutura_de_repeticao/exercicio11.py
@@ -1,3 +1,13 @@
 # Exercício 10
 # escreva um algoritmo que encontre todos os números primos de 2 ate 99
 # imprima todos eles na tela
+
+print("primos de 2 ate 99")
+for numero in range(2, 100, 1):
+    flag = 0
+    for i in range(2, numero, 1):
+        if numero % i == 0:
+            flag = 1
+            break
+    if flag == 0:
+        print(numero)
