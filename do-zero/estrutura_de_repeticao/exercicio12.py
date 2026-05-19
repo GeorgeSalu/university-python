7cbf9576e3fc5861b56234907a6e8d49e63f762b
George Salu <george.salu10@gmail.com>
2026-05-19 13:46:57 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 12 (solucao)

--- do-zero/estrutura_de_repeticao/exercicio12.py
+++ do-zero/estrutura_de_repeticao/exercicio12.py
@@ -5,3 +5,15 @@
 # após a frase ter sido digitada corretamente,
 # faça a impressão dela na tela de maneira exata como foi digitada e, em seguida,
 # remova os espaços da frase e imprima novamente, sem espaços
+
+frase = input("digite uma frase: ")
+tamanho = len(frase)
+while (tamanho < 10) or (tamanho < 30):
+    frase = input("digite uma frase: ")
+    tamanho = len(frase)
+
+print(f"com espacos: {frase}")
+print(f"sem espacos: {frase}", end='')
+for i in range(0, tamanho):
+    if frase[i] != ' ':
+        print(frase[i], end="")
