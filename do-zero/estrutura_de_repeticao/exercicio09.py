9876dedb0ca1c7d60d2475f2d79cf0f5aa24bd48
George Salu <george.salu10@gmail.com>
2026-05-19 08:42:47 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 09

---
+++ do-zero/estrutura_de_repeticao/exercicio09.py
@@ -0,0 +1,4 @@
+# Exercício 9
+# crie um programa que calcule a tabuada de um número escolhido pelo usuario
+# imprima na tela a tabuada deste numero de 1 a 10. Ao realizar a impressão na tela, mostre os valores formatado
+# exemplo com a tabuada de 5: 1x5=5, 2x5=10, 3x5=15
