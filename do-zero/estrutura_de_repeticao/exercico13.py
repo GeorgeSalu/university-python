425993025ae10c9992ff4095d69b80d6b2b02ac4
George Salu <george.salu10@gmail.com>
2026-05-19 13:58:09 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 13

---
+++ do-zero/estrutura_de_repeticao/exercico13.py
@@ -0,0 +1,4 @@
+# Exercício 13
+# escreva um algoritmo que leia inteiros via teclado
+# somente valores positivos devem ser aceites pelo programa
+# o final da execução, informe a média dos valores digitados
