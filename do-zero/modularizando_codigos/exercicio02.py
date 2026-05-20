8d65aed87141d711a3163f8355c2beb0c631ff6e
George Salu <george.salu10@gmail.com>
2026-05-20 13:34:33 -0300

    do zero ao avancado - modularizando codigo - exercicio 02

---
+++ do-zero/modularizando_codigos/exercicio02.py
@@ -0,0 +1,6 @@
+# Exercício 1
+# Escreva uma rotina que crie um laço de repetição que faz uma contagem
+# e imprime esta contagem na tela em uma so linha
+# Porem, como parametro, a função deve receber o valor inicial da contagem, o final, e o passo de iteração
+# deixe os parâmetros inicial e de passo como opcionais
+# você pode fazer o laço com for ou while
