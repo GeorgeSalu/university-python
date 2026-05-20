71127d353a3f49136e36286221cf3a415f314ccd
George Salu <george.salu10@gmail.com>
2026-05-20 10:10:37 -0300

    do zero ao avancado - modularizando codigo - exercicio 01

---
+++ do-zero/modularizando_codigos/exercicio01.py
@@ -0,0 +1,9 @@
+# Exercício 1
+# Escreva uma rotina que crie uma borda ao redor de uma palavra para destacá-la como sendo um título
+# A rotina deve receber como parametro a palavra a ser destacada
+# o tamanho da caixa de texto devera ser adaptável conforme o tamanho da palavra
+# a seguir veja alguns exemplos de como deve ficar a borda na palavra
+#
+#   *-----------*
+#   |    Ola    |
+#   +-----------+
