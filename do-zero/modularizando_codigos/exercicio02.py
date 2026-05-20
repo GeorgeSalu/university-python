1384713c4e840e838ef51489148f6f51ed52c11b
George Salu <george.salu10@gmail.com>
2026-05-20 16:28:03 -0300

    do zero ao avancado - modularizando codigo - exercicio 02 (solucao)

--- do-zero/modularizando_codigos/exercicio02.py
+++ do-zero/modularizando_codigos/exercicio02.py
@@ -4,3 +4,11 @@
 # Porem, como parametro, a função deve receber o valor inicial da contagem, o final, e o passo de iteração
 # deixe os parâmetros inicial e de passo como opcionais
 # você pode fazer o laço com for ou while
+
+def contador(fim, inicio=0,passo=1):
+    for i in range(inicio,fim+1, passo):
+        print(f"{i}", end=" ")
+    print("\n")
+
+contador(20,10,2)
+contador(12)
