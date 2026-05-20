3d0d87148289b5c84717228f1aab119fe09a825c
George Salu <george.salu10@gmail.com>
2026-05-20 08:48:57 -0300

    do zero ao avancado - estruturas de repeticao - exercicio 13 (solucao)

--- do-zero/estrutura_de_repeticao/exercico13.py
+++ do-zero/estrutura_de_repeticao/exercico13.py
@@ -2,3 +2,17 @@
 # escreva um algoritmo que leia inteiros via teclado
 # somente valores positivos devem ser aceites pelo programa
 # o final da execução, informe a média dos valores digitados
+
+soma = 0
+qtd = 0
+
+while True:
+    x = int(input("digite um valor inteiro e positivo: "))
+    if not x:
+        break
+
+    soma += x
+    qtd += 1
+
+media = soma / qtd
+print(f"a media dos valores digitados é {media}")
