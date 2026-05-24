8f014a8ea05bc165682b3b575a578a396de67030
George Salu <george.salu10@gmail.com>
2026-05-24 20:24:04 -0300

    do zero ao avancado - modularizando codigo - retornando valores em funcoes 04

---
+++ do-zero/modularizando_codigos/retorno_de_valores_04.py
@@ -0,0 +1,10 @@
+def calcular(a,b):
+    soma = a + b
+    multiplicacao = a * b
+    return soma, multiplicacao
+
+
+# programa principal
+resultado_soma, resultado_multiplicacao = calcular(4,5)
+print(resultado_soma)
+print(resultado_multiplicacao)
