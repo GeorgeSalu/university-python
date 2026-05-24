94efc0829f5642fa6482dfd621ebe592581f8d9e
George Salu <george.salu10@gmail.com>
2026-05-24 19:47:10 -0300

    do zero ao avancado - modularizando codigo - exercicio 03 (remove parenteses redundantes)

--- do-zero/modularizando_codigos/exercicio03.py
+++ do-zero/modularizando_codigos/exercicio03.py
@@ -6,17 +6,17 @@
 def maior3(v1 = 0, v2 = 0, v3 = 0):
     if v1 and v2 and v3:
         if (v1 > v2) and (v1 > v3):
-            if (v2 > v3):
+            if v2 > v3:
                 print(f"Ordem crescente: {v3}, {v2}, {v1}")
             else:
                 print(f"Ordem crescente: {v2}, {v3}, {v1}")
         elif (v2 > v1) and (v2 > v3):
-            if (v1 > v3):
+            if v1 > v3:
                 print(f"Ordem crescente: {v3}, {v1}, {v2}")
             else:
                 print(f"Ordem crescente: {v1}, {v3}, {v2}")
         elif (v3 > v1) and (v3 > v2):
-            if (v1 > v2):
+            if v1 > v2:
                 print(f"Ordem crescente: {v3}, {v2}, {v1}")
             else:
                 print(f"Ordem crescente: {v2}, {v3}, {v1}")
