f8a5f7904fe0f3054641371b0df585cedba82ae5
George Salu <george.salu10@gmail.com>
2026-05-25 11:08:21 -0300

    do zero ao avancado - tuplas,listas e dicionarios - tuplas 01

---
+++ do-zero/tuplas_listas_dicionarios/tuplas_01.py
@@ -0,0 +1,9 @@
+mochila = ('laptop', 'smartphone', 'power bank','carregadores e cabos')
+print(mochila)
+print(type(mochila))
+
+print(mochila[0])
+print(mochila[2])
+print(mochila[0:2])
+print(mochila[2:])
+print(mochila[-1])
