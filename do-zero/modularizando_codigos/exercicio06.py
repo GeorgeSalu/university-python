1950f265ff499de2daa9ed06263e10a6a56dd220
George Salu <george.salu10@gmail.com>
2026-05-25 09:53:35 -0300

    do zero ao avancado - modularizando codigo - exercicio 06 (solucao)

--- do-zero/modularizando_codigos/exercicio06.py
+++ do-zero/modularizando_codigos/exercicio06.py
@@ -1,3 +1,22 @@
 # Exercício 6
 # Faça uma função que recebe dois valores inteiros e positivos como parametro.
 # Calcule a soma dos n valores inteiros existentes entre eles, inclusive estes números
+
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while x < min or x > max:
+        x = int(input(pergunta))
+    return x
+
+def soma(inicio, fim):
+    soma = 0
+    i = inicio
+    while i <= fim:
+        soma += i
+        i += 1
+    return soma
+
+# programa principal
+x = valida_int('digite um valor inteiro e positivo : ', 1,9999)
+y = valida_int('digite um segundo valor inteiro e positivo : ', 1,9999)
+print(f"Somatoria entre os valores {x} e {y} é {soma(x, y)}")
