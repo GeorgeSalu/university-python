9de6ba59b76df8d7c4564f01e16bc8495824df3a
George Salu <george.salu10@gmail.com>
2026-05-25 10:21:15 -0300

    do zero ao avancado - modularizando codigo - exercicio 07 (solucao)

--- do-zero/modularizando_codigos/exercicio07.py
+++ do-zero/modularizando_codigos/exercicio07.py
@@ -1,3 +1,6 @@
 # Exercício 7
 # Faça uma função lambda que recebe dois valores numéricos como parametro, ao primeiro valor, sempre some 5.
 # Em seguida multiplique ambos e retorne o resultado
+
+calc = lambda a , b: (a + 5) * b
+print(calc(5,10))
