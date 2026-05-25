82fea4aaee58b320296afdf57e29ae6a7fef4639
George Salu <george.salu10@gmail.com>
2026-05-25 12:57:13 -0300

    do zero ao avancado - tuplas,listas e dicionarios - tuplas 03

---
+++ do-zero/tuplas_listas_dicionarios/tuplas_03.py
@@ -0,0 +1,8 @@
+mochila = ('laptop', 'smartphone', 'power bank','carregadores e cabos')
+upgrade = ('kindle','lanche')
+
+mochila_grande = mochila + upgrade
+
+print(mochila)
+print(upgrade)
+print(mochila_grande)
