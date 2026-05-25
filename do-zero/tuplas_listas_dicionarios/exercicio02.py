ac56e42b2f1233eaf8e1ac2161af3b3649431fc2
George Salu <george.salu10@gmail.com>
2026-05-25 15:37:29 -0300

    do zero ao avancado - tuplas,listas e dicionarios - exercicio 02

--- do-zero/tuplas_listas_dicionarios/exercicio02.py
+++ do-zero/tuplas_listas_dicionarios/exercicio02.py
@@ -1,5 +1,5 @@
 # Exercício 2
-# Escreva uma função que contenha dois parametros
+# Escreva uma função que contenha dois parâmetros
 # Essa função recebe como parametro uma string com uma mensagem a ser impressa na tela
 # e outro parametro como sendo uma quantidade arbitraria de números empacotados
 # Dentro dessa função, encontre o maior número dentre todos os números recebidos e escreva na tela
