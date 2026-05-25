8bd2732ba7381bfeff9fbb66c5147a6df95ba4c8
George Salu <george.salu10@gmail.com>
2026-05-25 08:35:41 -0300

    do zero ao avancado - modularizando codigo - exercicio 04

---
+++ do-zero/modularizando_codigos/exercicio4.py
@@ -0,0 +1,4 @@
+# Exercício 4
+# Escreva uma função para validar uma string
+# Essa função recebe como parametro a string, o número minimo e máximo de caracteres
+# retorne verdadeiro se o tamanho da string estiver entre os valores minimo e máximo, e falso caso contrario
