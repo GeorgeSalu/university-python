b98667d1ebdcc3f019cb451b08e0c5201c0d1cab
George Salu <george.salu10@gmail.com>
2026-05-25 09:34:47 -0300

    do zero ao avancado - modularizando codigo - exercicio 05 (solucao)

--- do-zero/modularizando_codigos/exercicio05.py
+++ do-zero/modularizando_codigos/exercicio05.py
@@ -5,3 +5,24 @@
 # Considerando a breve explicação sobre factorial,
 # Escreva uma função que calcule a factorial de um número recebido como paramentro e retorne o seu resultado
 # faça uma validação dos dados atraves de uma outra função, permitindo que somente valores positivos sejam aceites
+
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while x < min or x > max:
+        x = int(input(pergunta))
+    return x
+
+
+def factorial(num):
+    fat = 1
+    if num == 0:
+        return fat
+    for i in range(1, num + 1, 1):
+        fat *= i
+    return fat
+
+
+
+# programa principal
+x = valida_int('digite um valor para calcular a fatorial : ', 0, 999)
+print(f"{x}! = {factorial(x)}")
