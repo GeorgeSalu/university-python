75201b1b043cfcc12f4d565aa20f9afb8e89636a
George Salu <george.salu10@gmail.com>
2026-05-25 15:09:00 -0300

    do zero ao avancado - tuplas,listas e dicionarios - exercicio 01 (solucao)

--- do-zero/tuplas_listas_dicionarios/exercicio01.py
+++ do-zero/tuplas_listas_dicionarios/exercicio01.py
@@ -2,3 +2,11 @@
 # crie um programa que contem uma tupla contendo o nome de 10 linguagens de programação
 # Javascript, Rust, Swift, Python, Kotlin, Go, C#, Dart, Julia e Typescript
 # em que posição esta a linguagem python?, mostre na tela
+
+linguagens = ('Javascript', 'Rust','Swift','Python','Kotlin','Go','C#','Dart','Julia','Typescript')
+
+i = 0
+while linguagens[i] != 'Python':
+    i += 1
+
+print(f'Encontramos o python na {i + 1} posicao')
