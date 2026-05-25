77af02cfce7c94abae49d9ccd343a0ce6ec05aa5
George Salu <george.salu10@gmail.com>
2026-05-25 15:00:51 -0300

    do zero ao avancado - tuplas,listas e dicionarios - desempacotamento de parametros

---
+++ do-zero/tuplas_listas_dicionarios/desempacotamento_parametros.py
@@ -0,0 +1,11 @@
+def soma(*num):
+    soma = 0
+    print(f"Tupla: {num}")
+    for i in num:
+        soma += i
+    return soma
+
+
+# programa principal
+print(f"Resultado: {soma(1,2)}")
+print(f"Resultado: {soma(1,2,3,4,5,6,7,8,9)})")
