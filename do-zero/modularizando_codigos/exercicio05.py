5bcdb60714194039af054f5945776ac73be5d711
George Salu <george.salu10@gmail.com>
2026-05-25 09:16:00 -0300

    do zero ao avancado - modularizando codigo - exercicio 05

---
+++ do-zero/modularizando_codigos/exercicio05.py
@@ -0,0 +1,7 @@
+# Exercício 5
+# Factorial é um número inteiro positivo, representado por n!
+# Calculamos a factorial pela multiplicação desse numero n por todas os seus antecessores ate chegar em 1.
+# Ainda, factorial de 0! Sempre será 1
+# Considerando a breve explicação sobre factorial,
+# Escreva uma função que calcule a factorial de um número recebido como paramentro e retorne o seu resultado
+# faça uma validação dos dados atraves de uma outra função, permitindo que somente valores positivos sejam aceites
