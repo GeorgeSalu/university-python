b64bebddeb1ae9ae14de33f1122c271de03f9361
George Salu <george.salu10@gmail.com>
2026-05-25 09:08:32 -0300

    do zero ao avancado - modularizando codigo - exercicio 04 (solucao)

--- do-zero/modularizando_codigos/exercicio04.py
+++ do-zero/modularizando_codigos/exercicio04.py
@@ -2,3 +2,16 @@
 # Escreva uma função para validar uma string
 # Essa função recebe como parametro a string, o número minimo e máximo de caracteres
 # retorne verdadeiro se o tamanho da string estiver entre os valores minimo e máximo, e falso caso contrario
+
+def valida_string(pergunta, min, max):
+    s1 = input(pergunta)
+    tam = len(s1)
+    while (tam < min) or (tam > max):
+        s1 = input(pergunta)
+        tam = len(s1)
+    return s1
+
+
+# programa principal
+x = valida_string('digite uma string', 10,30)
+print(f"voce digitou a string: {x}. dado valido. encerrando")
