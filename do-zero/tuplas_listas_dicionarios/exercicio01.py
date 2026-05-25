bb4b48a4812eb417fd64efbdca361d16600dfa16
George Salu <george.salu10@gmail.com>
2026-05-25 15:03:07 -0300

    do zero ao avancado - tuplas,listas e dicionarios - exercicio 01

---
+++ do-zero/tuplas_listas_dicionarios/exercicio01.py
@@ -0,0 +1,4 @@
+# Exercício 1
+# crie um programa que contem uma tupla contendo o nome de 10 linguagens de programação
+# Javascript, Rust, Swift, Python, Kotlin, Go, C#, Dart, Julia e Typescript
+# em que posição esta a linguagem python?, mostre na tela
