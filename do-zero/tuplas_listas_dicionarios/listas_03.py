8afadc5ab4d6d4096c6a364fde7494cf28fd4a64
George Salu <george.salu10@gmail.com>
2026-05-26 11:38:06 -0300

    do zero ao avancado - tuplas,listas e dicionarios - manipulando listas

--- do-zero/tuplas_listas_dicionarios/listas_03.py
+++ do-zero/tuplas_listas_dicionarios/listas_03.py
@@ -3,6 +3,9 @@ mochila = ['laptop', 'smartphone', 'power bank','carregadores e cabos']
 print(mochila)
 # append() adiciona no final da lista
 mochila.append('oculos')
-# insert() adiciona em posicao definida
+# insert() adiciona em uma posição definida
 mochila.insert(1, 'canivete')
+print(mochila)
+# remove() remove da lista
+mochila.remove('oculos')
 print(mochila)
