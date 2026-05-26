92a45423a3df597a120aa7cd3ef7b4f8d2835dee
George Salu <george.salu10@gmail.com>
2026-05-26 09:55:09 -0300

    do zero ao avancado - tuplas,listas e dicionarios - manipulando listas

---
+++ do-zero/tuplas_listas_dicionarios/listas_03.py
@@ -0,0 +1,8 @@
+mochila = ['laptop', 'smartphone', 'power bank','carregadores e cabos']
+
+print(mochila)
+# append() adiciona no final da lista
+mochila.append('oculos')
+# insert() adiciona em posicao definida
+mochila.insert(1, 'canivete')
+print(mochila)
