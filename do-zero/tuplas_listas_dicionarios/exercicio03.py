9d8ed811ba309468aa1fd573f1fd68d1bbcef29a
George Salu <george.salu10@gmail.com>
2026-05-28 10:09:40 -0300

    do zero ao avancado - tuplas,listas e dicionarios - exercicio 03 (solucao)

--- do-zero/tuplas_listas_dicionarios/exercicio03.py
+++ do-zero/tuplas_listas_dicionarios/exercicio03.py
@@ -1,2 +1,11 @@
 # Exercício 2
 # Escreva um programa em python que encontre o maior valor dentro de uma lista e imprima-o na tela
+
+x = [2,3,5,6,7,8,4,2,40]
+
+maior = x[0]
+for numero in x:
+    if numero > maior:
+        maior = numero
+
+print(f"o maior valor é {maior}")
