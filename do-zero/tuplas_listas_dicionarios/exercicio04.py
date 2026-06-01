5655999d1de15f1237fba3264ae6badf1c9756ae
George Salu <george.salu10@gmail.com>
2026-06-01 08:50:48 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 04 (solucao)

--- do-zero/tuplas_listas_dicionarios/exercicio04.py
+++ do-zero/tuplas_listas_dicionarios/exercicio04.py
@@ -2,3 +2,17 @@
 # Escreva um algoritmo que crie uma lista vazia e ca adicionando valores referentes
 # a ota de um aluno nesta lista. Quando o usuario desejar parar de digitar nota
 # (digitando um valor negativo, por exemplo), calcule a média das notas digitadas
+
+notas = list()
+x = float(input("Digite uma nota: "))
+while x >= 0:
+    notas.append(x)
+    x = float(input("Digite uma nota: "))
+
+soma = 0
+for vlores in notas:
+    soma += vlores
+
+media = soma / len(notas)
+print(notas)
+print(f"Media das notas digitadas: {media}")
