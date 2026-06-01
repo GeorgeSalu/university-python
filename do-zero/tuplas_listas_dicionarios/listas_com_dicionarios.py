f4cfaa8c9b3d5bc03b37b919ed4017c72ab18d09
George Salu <george.salu10@gmail.com>
2026-06-01 14:09:03 -0300

    o zero ao avancado - tuplas,listas e dicionarios - listas com dicionarios

---
+++ do-zero/tuplas_listas_dicionarios/listas_com_dicionarios.py
@@ -0,0 +1,8 @@
+games = []
+
+game1 = {'nome':'super mario','desenvolvedora':'nintendo','ano':1990}
+game2 = {'nome':'zelda','desenvolvedora':'super nintendo 64','ano':1990}
+game3 = {'nome':'pockemon','desenvolvedora':'game boy','ano':1999}
+
+games = [game1,game2,game3]
+print(games)
