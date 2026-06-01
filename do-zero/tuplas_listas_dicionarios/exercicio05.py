1f4a1c0b42d60643a2d97405a944d0d22e2a4cf9
George Salu <george.salu10@gmail.com>
2026-06-01 09:22:33 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 05 (solucao)

--- do-zero/tuplas_listas_dicionarios/exercicio05.py
+++ do-zero/tuplas_listas_dicionarios/exercicio05.py
@@ -4,3 +4,21 @@
 # se o dado desejado se encontra presente, escreva uma função que receba como parametro uma lista e um dado
 # verifique se o dado esta presente na lista e retorne da função o seu indice, caso ele esteja presente
 # caso contrario retorne -1
+
+def buscaSequencial(lista, dado):
+    x = 0
+    while x < len(lista):
+        if lista[x] == dado:
+            return x
+        x += 1
+    return -1
+
+
+# programa principal
+teste = [6,8,7,9,0,5,3]
+dado = int(input("Digite um valor para buscar : "))
+res = buscaSequencial(teste, dado)
+if res >= 0:
+    print(f"posicao onde o {dado} foi encontrado : {res + 1}")
+else:
+    print(f"dado nao foi encontrado")
