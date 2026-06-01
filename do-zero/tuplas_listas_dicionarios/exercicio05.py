9f6703f48257e455f6d22fea779e6159b9fafdd3
George Salu <george.salu10@gmail.com>
2026-06-01 08:54:29 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 05

---
+++ do-zero/tuplas_listas_dicionarios/exercicio05.py
@@ -0,0 +1,6 @@
+# Exercicio 5
+# O algoritmo mais simples de se buscar um dado numa estrutura de dados é chamado de busca sequencial
+# a busca sequencial é uma varredura simples do primeiro ao último elemento da estrutura, verificando
+# se o dado desejado se encontra presente, escreva uma função que receba como parametro uma lista e um dado
+# verifique se o dado esta presente na lista e retorne da função o seu indice, caso ele esteja presente
+# caso contrario retorne -1
