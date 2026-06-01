75827c76b341d627cf0d9be7b5d12c1e77a129c9
George Salu <george.salu10@gmail.com>
2026-06-01 10:06:24 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 06 (solucao)

--- do-zero/tuplas_listas_dicionarios/exercicio06.py
+++ do-zero/tuplas_listas_dicionarios/exercicio06.py
@@ -7,3 +7,20 @@
 # a pessoa com menor IMC
 # o cálculo do IMC deve ser realizado empregando uma função lambda e é dado como : IMC = peso / (altura^2)
 # onde a massa é dado em quilograma e a altura em metros
+
+pessoas = []
+imc = lambda peso, altura: peso / (altura * 2)
+
+while True:
+    nome = input("Nome")
+    altura = float(input("Altura"))
+    peso = float(input("Peso"))
+    x = imc(peso, altura)
+
+    res = input("Quer continuar? [S/N]")
+    if res in 'Nn':
+        break
+
+print("cadastros ", pessoas)
+print("Total de cadastros : ", len(pessoas))
+
