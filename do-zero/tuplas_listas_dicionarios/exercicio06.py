2409f6692b3b5682a0d5afbeb55148144fe1892e
George Salu <george.salu10@gmail.com>
2026-06-01 09:26:48 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 06

---
+++ do-zero/tuplas_listas_dicionarios/exercicio06.py
@@ -0,0 +1,9 @@
+# Exercicio 5
+# escreva um algoritmo que leia o nome, alguta e peso de pessoas e armazene as informações em uma lista
+# o programa deve ir cadastrando um número indeterminado de dados e armazenar dentro da lista também o
+# IMC da pessoa. Ao final do programa, imprima a lista completa e também
+# o total de cadastros
+# a pessoa com maior IMC
+# a pessoa com menor IMC
+# # o cálculo do IMC deve ser realizado empregando uma função lambda e é dado como : IMC = peso / (altura^2)
+# onde a massa é dado em quilograma e a altura em metros
