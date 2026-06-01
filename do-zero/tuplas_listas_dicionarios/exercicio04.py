423b83d53ae2ef532372aba6e27ba00ce0fe2b31
George Salu <george.salu10@gmail.com>
2026-06-01 08:34:42 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 04

---
+++ do-zero/tuplas_listas_dicionarios/exercicio04.py
@@ -0,0 +1,4 @@
+# Exercicio 4
+# Escreva um algoritmo que crie uma lista vazia e ca adicionando valores referentes
+# a ota de um aluno nesta lista. quando o usuario desejar parar de digitar nota
+# (digitando um valor negativo, por exemplo), calcule a media das notas digitadas
