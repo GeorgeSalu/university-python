c7fd46fc4ca5aeffbbe594761e28f3e2af5a4b96
George Salu <george.salu10@gmail.com>
2026-06-02 09:57:57 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 08

---
+++ do-zero/tuplas_listas_dicionarios/exercicio08.py
@@ -0,0 +1,7 @@
+# Exercicio 8
+# Crie um programa para controle de estoque de produtos de um estabelecimento que vende produtos de hortifruti
+# Para o estoque, armazene todo dentro de um dicionario contendo listas.
+# A chave deverá ser o nome de cada produto e dentro de cada lista teremos o preco e quantidade disponivel no estoque
+# o estoque pode esta pre-cadastrado no sistema com a quantidade de itens a desejar.
+
+
