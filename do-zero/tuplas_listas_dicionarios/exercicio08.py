8bd3a4d25fe921b0b925122f8975b7e6c933553c
George Salu <george.salu10@gmail.com>
2026-06-02 10:13:32 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 08: estoque de produtos

--- do-zero/tuplas_listas_dicionarios/exercicio08.py
+++ do-zero/tuplas_listas_dicionarios/exercicio08.py
@@ -4,4 +4,8 @@
 # A chave deverá ser o nome de cada produto e dentro de cada lista teremos o preco e quantidade disponivel no estoque
 # o estoque pode esta pre-cadastrado no sistema com a quantidade de itens a desejar.
 
+# Simule uma compra. Peça ao usuario para digitar o nome do produto e a quantidade que deseja até que ele decida
+# encerrar a compra, ao final apresente tudo na tela num formato organizado, mostrando o total a ser pago por
+# produto e o total final do pedido. De baixa o sistema descontando o que foi comprado do total, imprima na tela o estoque restante
 
+loja = {'cenoura':[100,0.99], 'brocolis':[50,3.99], 'batata':[200,0.49], 'cebola':[75,1.10] }
