206998d5fddedec9408a80408f048a4c76038840
George Salu <george.salu10@gmail.com>
2026-06-02 09:54:34 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 07 (solucao)

--- do-zero/tuplas_listas_dicionarios/exercicio07.py
+++ do-zero/tuplas_listas_dicionarios/exercicio07.py
@@ -6,3 +6,20 @@
 #        media < 7 e >= 5 em exame
 #        media < 5, reprovado
 # apresente tudo na tela ao final do programa num formato organizado
+
+aluno = dict()
+aluno['nome'] = input("Qual o nome do aluno : ")
+n1 = float(input("Qual a primeira nota do aluno : "))
+n2 = float(input("Qual a segunda nota do aluno : "))
+n3 = float(input("Qual a terceira nota do aluno : "))
+aluno['media'] = (n1 + n2 + n3) / 3
+
+if aluno['media'] >= 7:
+    aluno['status'] = 'A'
+elif aluno['media'] >= 5 and aluno['media'] < 7:
+    aluno['status'] = 'E'
+else:
+    aluno['status'] = 'R'
+
+for chave, valor in aluno.items():
+    print(f"{chave} = {valor}")
