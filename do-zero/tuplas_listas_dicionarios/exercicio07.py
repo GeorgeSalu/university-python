9132a3effa5c768bca7dfeb12c99bab21aa0c9fc
George Salu <george.salu10@gmail.com>
2026-06-02 09:34:40 -0300

    o zero ao avancado - tuplas,listas e dicionarios - exercicio 07

--- do-zero/tuplas_listas_dicionarios/exercicio07.py
+++ do-zero/tuplas_listas_dicionarios/exercicio07.py
@@ -2,7 +2,7 @@
 # Escreva um programa que leia o nome de um aluno e tres notas
 # armazene num dicionário o nome e a média aritmetica da nota
 # ainda, armazene no dicionário a situação do aluno
-# media >= 7, aprovado
-# media < 7 e >= 5 em exame
-# media < 5, reprovado
+#        media >= 7, aprovado
+#        media < 7 e >= 5 em exame
+#        media < 5, reprovado
 # apresente tudo na tela ao final do programa num formato organizado
