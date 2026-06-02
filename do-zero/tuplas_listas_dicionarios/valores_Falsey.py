2ca349fa7c96d723ad18840e3051d8663fa7eb09
George Salu <george.salu10@gmail.com>
2026-06-02 12:44:30 -0300

    o zero ao avancado - tuplas,listas e dicionarios - valores falsey

---
+++ do-zero/tuplas_listas_dicionarios/valores_Falsey.py
@@ -0,0 +1,7 @@
+# são tratados como False?Falsey
+# - O número zero, seja ele inteiro ou ponto flutuante
+# - Uma string sem nenhum conteúdo
+# - tuplas vazias
+# - listas vazias
+# - dicionarios vazios
+# - intevalos vazios
