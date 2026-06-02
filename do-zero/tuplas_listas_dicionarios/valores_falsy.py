3f08fb0e17494c3bd24efe4fb8d9ddd1d45b69d2
George Salu <george.salu10@gmail.com>
2026-06-02 12:54:22 -0300

    o zero ao avancado - tuplas,listas e dicionarios - valores falsy

--- do-zero/tuplas_listas_dicionarios/valores_falsy.py
+++ do-zero/tuplas_listas_dicionarios/valores_falsy.py
@@ -5,3 +5,10 @@
 # - listas vazias
 # - dicionarios vazios
 # - intevalos vazios
+
+# Todos estes retornam False
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool([]))
