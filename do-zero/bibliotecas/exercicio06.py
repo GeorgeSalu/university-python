24e15de6ade16f18b208ffb5065821dd25ae25c5
George Salu <george.salu10@gmail.com>
2026-06-05 14:43:21 -0300

    o zero ao avancado - bibliotecas - exercicio 06 (solucao)

--- do-zero/bibliotecas/exercicio06.py
+++ do-zero/bibliotecas/exercicio06.py
@@ -1,3 +1,16 @@
 # Exercício 06
 # Converter Objeto Python para String JSON (dumps)
 # O método dumps (string dump) faz o processo inverso, serializando um objeto Python (como um dicionário) em uma string de texto JSON
+
+import json
+
+dados_python = {
+    "nome": "Carlos",
+    "linguagens": ["Python", "JavaScript"],
+    "online": True
+}
+
+# Convertendo para string JSON (com formatação legível)
+string_json = json.dumps(dados_python, indent=4, ensure_ascii=False)
+
+print(string_json)
