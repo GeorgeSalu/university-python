42db7fdfe1767b255e81e33e1715828c57999d8f
George Salu <george.salu10@gmail.com>
2026-06-05 13:50:43 -0300

    o zero ao avancado - bibliotecas - exercicio 02 (solucao 2)

--- do-zero/bibliotecas/exercicio02.py
+++ do-zero/bibliotecas/exercicio02.py
@@ -6,3 +6,7 @@ import random
 # Escolher um número aleatório entre 1 e 10
 numero_sorteado = random.randint(1, 10)
 print(numero_sorteado)
+
+# Escolher um item de uma lista
+cores = ['vermelho', 'azul', 'verde']
+print(random.choice(cores))
