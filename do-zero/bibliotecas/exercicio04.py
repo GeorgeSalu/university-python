7631a5c4f13fb756eef7fe1067fa6a780ca40eae
George Salu <george.salu10@gmail.com>
2026-06-05 14:14:45 -0300

    o zero ao avancado - bibliotecas - exercicio 04 (solucao)

--- do-zero/bibliotecas/exercicio04.py
+++ do-zero/bibliotecas/exercicio04.py
@@ -2,3 +2,9 @@
 # json (Manipulação de Dados)
 # Muito usada para ler, escrever e converter dados estruturados, crucial para trabalhar com APIs.
 
+import json
+
+# Converter dicionário Python para string JSON
+dados = {"nome": "Ana", "idade": 25}
+json_string = json.dumps(dados)
+print(json_string)
