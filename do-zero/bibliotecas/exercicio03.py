30caa30b332745aaa74b9f8379d187307e634b6a
George Salu <george.salu10@gmail.com>
2026-06-05 14:01:16 -0300

    o zero ao avancado - bibliotecas - exercicio 03 (solucao)

--- do-zero/bibliotecas/exercicio03.py
+++ do-zero/bibliotecas/exercicio03.py
@@ -1,3 +1,12 @@
-# Exercício 02
+# Exercício 03
 # os (Interação com o Sistema Operacional)
 # Permite navegar entre pastas, ler diretórios e executar comandos do sistema.
+
+import os
+
+# Descobrir qual é o diretório atual
+diretorio_atual = os.getcwd()
+print(diretorio_atual)
+
+# Listar todos os arquivos da pasta
+print(os.listdir('.'))
