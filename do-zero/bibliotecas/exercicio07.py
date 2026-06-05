1d7123c33ae489ec78c94c85b626b555bda146ad
George Salu <george.salu10@gmail.com>
2026-06-05 14:56:49 -0300

    o zero ao avancado - bibliotecas - exercicio 07 (solucao)

--- do-zero/bibliotecas/exercicio07.py
+++ do-zero/bibliotecas/exercicio07.py
@@ -2,3 +2,13 @@
 # Escrever arquivo JSON
 # O método dump permite salvar um objeto Python diretamente em um arquivo no formato JSON
 
+import json
+
+dados_para_salvar = {
+    "empresa": "Tech Solutions",
+    "funcionarios": 15
+}
+
+# Gravando os dados em um arquivo
+with open('saida.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(dados_para_salvar, arquivo, indent=4)
