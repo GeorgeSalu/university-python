f71bc3af0ba7eea4b104d5ddef5243837e6a5249
George Salu <george.salu10@gmail.com>
2026-06-05 14:29:28 -0300

    o zero ao avancado - bibliotecas - exercicio 06

---
+++ do-zero/bibliotecas/exercicio06.py
@@ -0,0 +1,3 @@
+# Exercício 06
+# Converter Objeto Python para String JSON (dumps)
+# O método dumps (string dump) faz o processo inverso, serializando um objeto Python (como um dicionário) em uma string de texto JSON
