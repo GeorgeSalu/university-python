c0b6ba2fafc020a09f800d4469bc799959bc99bd
George Salu <george.salu10@gmail.com>
2026-06-05 15:07:54 -0300

    o zero ao avancado - bibliotecas - exercicio 08 (solucao)

--- do-zero/bibliotecas/exercicio08.py
+++ do-zero/bibliotecas/exercicio08.py
@@ -1,3 +1,11 @@
 # Exercício 07
 # Ler arquivo JSON (load)
 # O método load é usado para ler um arquivo .json diretamente e convertê-lo para estruturas Python
+
+import json
+
+# Lendo dados de um arquivo
+with open('saida.json', 'r', encoding='utf-8') as arquivo:
+    dados_carregados = json.load(arquivo)
+
+print(dados_carregados)
