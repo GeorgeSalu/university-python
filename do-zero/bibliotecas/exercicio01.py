0a9586cfb2c71bd2c4739e113f68a2ad5fce8178
George Salu <george.salu10@gmail.com>
2026-06-05 10:14:10 -0300

    o zero ao avancado - bibliotecas - exercicio 01

--- do-zero/bibliotecas/exercicio01.py
+++ do-zero/bibliotecas/exercicio01.py
@@ -1,3 +1,3 @@
-# Exercicio 01
+# Exercício 01
 # Desenvolva um algoritmo que solicte ao usuario uma quantidade de dias, de horas e de minutos e de segundos
 # Calcule total de segundos resultantes e imprima na tela para o usuario
