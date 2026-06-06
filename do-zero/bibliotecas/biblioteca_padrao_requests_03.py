ca06caf390f73cb385a7902051c9e0ba53774a99
George Salu <george.salu10@gmail.com>
2026-06-06 12:42:10 -0300

    o zero ao avancado - bibliotecas - biblioteca requests 03

--- do-zero/bibliotecas/biblioteca_padrao_requests_03.py
+++ do-zero/bibliotecas/biblioteca_padrao_requests_03.py
@@ -1,2 +1,11 @@
 # Verificar o status da requisição antes de ler o JSON
 # Evita que erros de servidor ou páginas não encontradas gerem erros de decodificação.
+import requests
+
+response = requests.get("https://github.com")
+
+if response.status_code == 200:
+    dados = response.json()
+    print(f"Nome: {dados['name']}")
+else:
+    print("Falha na requisição.")
