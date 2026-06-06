8d3772370cddf41778b5c0954f1c9ee898edaf7b
George Salu <george.salu10@gmail.com>
2026-06-06 12:43:04 -0300

    o zero ao avancado - bibliotecas - biblioteca requests 04

--- do-zero/bibliotecas/biblioteca_padrao_requests_04.py
+++ do-zero/bibliotecas/biblioteca_padrao_requests_04.py
@@ -1,2 +1,10 @@
 # Usar raise_for_status() para tratamento de exceções
 # Lança uma exceção se a requisição retornar um código de erro HTTP (ex: 404 ou 500)
+import requests
+
+try:
+    response = requests.get("https://github.com")
+    response.raise_for_status()  # Levanta erro para status HTTP 4xx/5xx
+    dados = response.json()
+except requests.exceptions.HTTPError as err:
+    print(f"Erro HTTP: {err}")
