dc96ce09dcaacb539b48de89a5caac889196ead7
George Salu <george.salu10@gmail.com>
2026-06-06 20:32:19 -0300

    o zero ao avancado - bibliotecas - biblioteca collections 05

---
+++ do-zero/bibliotecas/biblioteca_padrao_collections_05.py
@@ -0,0 +1,8 @@
+# OrderedDict (Dicionário ordenado)
+# Dicionário que mantém a ordem exata em que os itens foram inseridos.
+from collections import OrderedDict
+
+tarefas = OrderedDict()
+tarefas['limpar'] = 1
+tarefas['cozinhar'] = 2
+print(tarefas)
