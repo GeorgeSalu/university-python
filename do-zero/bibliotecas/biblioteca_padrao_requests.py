f4dcd97543261b98ee23148baa58525b395e0fb6
George Salu <george.salu10@gmail.com>
2026-06-06 12:34:08 -0300

    o zero ao avancado - bibliotecas - biblioteca requests

--- do-zero/bibliotecas/biblioteca_padrao_requests.py
+++ do-zero/bibliotecas/biblioteca_padrao_requests.py
@@ -1,2 +1,9 @@
 # Consultar uma API e ler a resposta JSON
 # Converte os dados brutos recebidos para um formato manipulável em Python
+
+import requests
+
+response = requests.get("https://api.github.com")
+dados = response.json()
+
+print(dados["current_user_url"])
