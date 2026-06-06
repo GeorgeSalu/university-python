c941a92c2506a1d6c24226adf0f3aad93de93ef5
George Salu <george.salu10@gmail.com>
2026-06-06 12:37:16 -0300

    o zero ao avancado - bibliotecas - biblioteca requests 02

--- do-zero/bibliotecas/biblioteca_padrao_requests_02.py
+++ do-zero/bibliotecas/biblioteca_padrao_requests_02.py
@@ -1,2 +1,8 @@
 # Enviar dados JSON numa requisição POST
 # Utiliza o parâmetro json=... para serializar automaticamente o dicionário
+import requests
+
+payload = {"nome": "João", "cargo": "Desenvolvedor"}
+response = requests.post("https://httpbin.org", json=payload)
+
+print(response.json())
