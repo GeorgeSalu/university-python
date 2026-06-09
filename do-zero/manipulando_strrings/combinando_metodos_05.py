8b4ea37ac871968896b631881b573fc3f417a48e
George Salu <george.salu10@gmail.com>
2026-06-09 20:59:35 -0300

    o zero ao avancado - manipulando strings - descrevendo metodos rtrip() endswith()

--- do-zero/manipulando_strrings/combinando_metodos_05.py
+++ do-zero/manipulando_strrings/combinando_metodos_05.py
@@ -1,4 +1,5 @@
+# rstrip = remove espaços do fim da string
+# endswith = verifica se uma string termina com alguns determinados caracteres
 s1 = 'Vinicius Pozzobon Borin '
 r = s1.lower().rstrip().endswith('borin')
-# rstrip() remove espaços do fim da string
 print(r)
