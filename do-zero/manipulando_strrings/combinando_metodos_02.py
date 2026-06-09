ce6040a1fbac4f7025426d875718a61031a8b914
George Salu <george.salu10@gmail.com>
2026-06-09 20:41:05 -0300

    o zero ao avancado - manipulando strings - descrevendo metodos lower() endswith()

--- do-zero/manipulando_strrings/combinando_metodos_02.py
+++ do-zero/manipulando_strrings/combinando_metodos_02.py
@@ -1,3 +1,5 @@
+# lower = converte toda a string para caractere minúsculo
+# endswith = verifica se uma string termina com alguns determinados caracteres
 s1 = 'Vinicius Pozzobon Borin'
 r = s1.lower().endswith('borin')
 print(r)
