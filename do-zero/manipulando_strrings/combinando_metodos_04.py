3ad6b3b1ee1b69697c3303f548dc08066925c0aa
George Salu <george.salu10@gmail.com>
2026-06-09 20:42:34 -0300

    o zero ao avancado - manipulando strings - descrevendo metodos ltrip() startswith()

--- do-zero/manipulando_strrings/combinando_metodos_04.py
+++ do-zero/manipulando_strrings/combinando_metodos_04.py
@@ -1,4 +1,5 @@
+# lstrip = remove espaços do início da string
+# startswith = verifica se uma string inicia com alguns determinados caracteres
 s1 = ' Vinicius Pozzobon Borin'
 r = s1.lower().lstrip().startswith('vinicius')
-# lstrip() remove espaços do inicio da string
 print(r)
