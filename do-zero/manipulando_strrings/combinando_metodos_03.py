10ea4331661ea9d0361285f05ad1babec8995b6f
George Salu <george.salu10@gmail.com>
2026-06-09 20:41:55 -0300

    o zero ao avancado - manipulando strings - descrevendo metodos strip() startswith()

--- do-zero/manipulando_strrings/combinando_metodos_03.py
+++ do-zero/manipulando_strrings/combinando_metodos_03.py
@@ -1,4 +1,5 @@
+# strip = remove espaços do início e fim da string
+# startswith = verifica se uma string inicia com alguns determinados caracteres
 s1 = ' Vinicius Pozzobon Borin '
 r = s1.lower().strip().startswith('vinicius')
-# strip() remove espaços do inicio e fim da string
 print(r)
