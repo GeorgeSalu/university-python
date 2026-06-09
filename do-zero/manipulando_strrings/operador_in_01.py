d65d5ce5aca01ed78a40afd94fd4b0f9061013ec
George Salu <george.salu10@gmail.com>
2026-06-09 08:55:21 -0300

    o zero ao avancado - manipulando strings - descrevendo operador in

--- do-zero/manipulando_strrings/operador_in_01.py
+++ do-zero/manipulando_strrings/operador_in_01.py
@@ -1,3 +1,5 @@
+# O operador 'in' é usado para verificar a presença de um valor em uma estrutura iteravel, como listas, tuplas ou string
+# quando aplicado a strings, permite verificar se uma string (substring) esta contida em outra string
 s1 = "Ola, mundo"
 print("Ola" in s1)
 print("Tchau" in s1)
