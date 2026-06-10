5b956cfdcafbd6b119c37e70a0a3efc0b02b8fad
George Salu <george.salu10@gmail.com>
2026-06-10 08:37:56 -0300

    o zero ao avancado - manipulando strings - descrevendo metodos

--- do-zero/manipulando_strings/endswith.py
+++ do-zero/manipulando_strings/endswith.py
@@ -1,4 +1,4 @@
-# endswith = verifica se uma string termina com alguns determinados caracteres
+# endswith() = verifica se uma string termina com alguns determinados caracteres
 s1 = 'logica de programação om python'
 r = s1.endswith('python')
 print(r)
