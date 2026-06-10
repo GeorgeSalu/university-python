f61efc558d549a1d4e68740d50e9fc3b66092410
George Salu <george.salu10@gmail.com>
2026-06-10 08:36:20 -0300

    o zero ao avancado - manipulando strings - descrevendo metodos join()

--- do-zero/manipulando_strings/manipulando_strings_02.py
+++ do-zero/manipulando_strings/manipulando_strings_02.py
@@ -1,4 +1,4 @@
-# join = combina um iterável de strings em uma única string, inserindo a string separadora entre cada elemento
+# join() = combina um iterável de strings em uma única string, inserindo a string separadora entre cada elemento
 s1 = list('Python')
 print(s1) # print separado
 print(''.join(s1))
