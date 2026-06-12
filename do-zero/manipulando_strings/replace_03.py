f39f2e8f912ae3fb3cea3f96ddd9f20a2ce06926
George Salu <george.salu10@gmail.com>
2026-06-12 09:32:45 -0300

    o zero ao avancado - manipulando strings - replace 03

--- do-zero/manipulando_strings/replace_03.py
+++ do-zero/manipulando_strings/replace_03.py
@@ -1,4 +1,4 @@
 # replace() = substitui caracteres por outros
 frase = "Gato, gato, gato e gato."
-# Substitui apenas as 2 primeiras ocorrências
+# substitui apenas as 2 primeiras ocorrências
 print(frase.replace("gato", "cachorro", 2))
