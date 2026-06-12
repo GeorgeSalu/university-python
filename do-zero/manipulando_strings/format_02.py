aea3f3a205287b784bf1d812837475fa2d8b5969
George Salu <george.salu10@gmail.com>
2026-06-12 09:52:32 -0300

    o zero ao avancado - manipulando strings - format 02

---
+++ do-zero/manipulando_strings/format_02.py
@@ -0,0 +1,4 @@
+# O Método .format() tradicional
+# Útil se você precisar repetir ou mudar a ordem dos argumentos.
+frase = "{1} é uma linguagem focada em {0}.".format("legibilidade", "Python")
+print(frase)
