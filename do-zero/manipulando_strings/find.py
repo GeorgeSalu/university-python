778362501fb1b5a97eb07791b27be99b33e24fa5
George Salu <george.salu10@gmail.com>
2026-06-14 18:27:21 -0300

    o zero ao avancado - manipulando strings - find

---
+++ do-zero/manipulando_strings/find.py
@@ -0,0 +1,7 @@
+# find() = procurar se uma substring esta dentro de outra string, e retorna a posicção da primeira ocorrencia
+s1 = "ola, mundo"
+r = s1.find("ola")
+print(r)
+
+r = s1.find("mu")
+print(r)
