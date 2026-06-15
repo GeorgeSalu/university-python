278fb95fc960bf39a74eed87746bbedcb4e5a757
George Salu <george.salu10@gmail.com>
2026-06-15 16:29:15 -0300

    o zero ao avancado - manipulando strings - validando strings isdecimal

--- do-zero/manipulando_strings/validando_strings_isdecimal.py
+++ do-zero/manipulando_strings/validando_strings_isdecimal.py
@@ -1,4 +1,6 @@
-# isdecimal() = verifica se todos os caracteres de uma string são numéricos decimais (de 0 a 9).
+# isdecimal() = verifica se todos os caracteres de uma string são numéricos decimais (de 0 a 9). Ele retorna True apenas se a string
+# contiver exclusivamente números e não possuir letras, espaços ou símbolos, sendo útil para validar entradas de dados numéricos
+
 
 texto = "12345"
 resultado = texto.isdecimal()
