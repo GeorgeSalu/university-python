cee5f74ddc25fb35286d8cbc8d1d726831c116b6
George Salu <george.salu10@gmail.com>
2026-06-15 16:54:12 -0300

    o zero ao avancado - manipulando strings - validando strings isascii

--- do-zero/manipulando_strings/validando_strings_isascii.py
+++ do-zero/manipulando_strings/validando_strings_isascii.py
@@ -1,4 +1,5 @@
 # isascii() = é uma função interna do Python que verifica se todos os caracteres de uma string pertencem ao conjunto padrão ASCII.
+# Ele retorna True se a string estiver vazia ou contiver apenas caracteres com valores entre 0 e 127, e False caso contrário
 
 nome_usuario = "Carlos"
 print(nome_usuario.isascii())
