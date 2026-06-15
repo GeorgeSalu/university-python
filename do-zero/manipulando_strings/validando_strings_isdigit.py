f03ead1d954d6d69d27f84eea7127edf13ecf9d7
George Salu <george.salu10@gmail.com>
2026-06-15 09:19:34 -0300

    o zero ao avancado - manipulando strings - validando strings isdigit

---
+++ do-zero/manipulando_strings/validando_strings_isdigit.py
@@ -0,0 +1,9 @@
+# isdigit() = verifica se todos os caracteres de uma string são numéricos. Ele retorna True se a string não estiver vazia e contiver
+# apenas dígitos, ou False caso contrário. É muito usado para validar entradas antes de converter strings para números
+s1 = "curso de python"
+r = s1.isalpha()
+print(r)
+
+s1 = "42"
+r = s1.isalpha()
+print(r)
