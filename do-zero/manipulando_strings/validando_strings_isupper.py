33ff0551c227561ce888b0034730e9fb8d619d73
George Salu <george.salu10@gmail.com>
2026-06-15 09:40:12 -0300

    o zero ao avancado - manipulando strings - validando strings isupper

---
+++ do-zero/manipulando_strings/validando_strings_isupper.py
@@ -0,0 +1,11 @@
+# isupper() = verifica se todos os caracteres alfabéticos de uma string estão em letras maiúsculas. Ele retorna True se a
+# condição for atendida e False caso contrário. Caracteres especiais e números são ignorados na validação
+
+texto = "PYTHON É INCRÍVEL"
+print(texto.isupper())
+
+texto2 = "Python é legal"
+print(texto2.isupper())
+
+texto3 = "PYTHON 3!"
+print(texto3.isupper())
