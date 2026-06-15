c4e3a2226c0a02f0c2897098e15df2f3aecdf1e2
George Salu <george.salu10@gmail.com>
2026-06-15 09:30:06 -0300

    o zero ao avancado - manipulando strings - validando strings isnumeric

--- do-zero/manipulando_strings/validando_strings_isnumeric.py
+++ do-zero/manipulando_strings/validando_strings_isnumeric.py
@@ -1,3 +1,6 @@
+# isnumeric() = verifica se todos os caracteres de uma string são numéricos, retornando True (verdadeiro) ou False (falso)
+# Ele reconhece desde dígitos decimais comuns até caracteres especiais de contagem (como frações e subscritos Unicode)
+
 codigo = "1054"
 print(codigo.isnumeric())
 
