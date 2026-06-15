822a000343a5143126a93ee25dea3dbf22aadb2c
George Salu <george.salu10@gmail.com>
2026-06-15 09:10:49 -0300

    o zero ao avancado - manipulando strings - validando strings 02

---
+++ do-zero/manipulando_strings/validando_strings_02.py
@@ -0,0 +1,8 @@
+# isalpha() = retorna true somente se na string existir somente caracteres regulares e com acentuação, qualquer outra situação é false
+s1 = "curso de python"
+r = s1.isalpha()
+print(r)
+
+s1 = "42"
+r = s1.isalpha()
+print(r)
