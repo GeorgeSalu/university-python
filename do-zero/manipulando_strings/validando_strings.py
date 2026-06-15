83cd3ff7dcb0dd1bd1bb7315827553d84a4c0205
George Salu <george.salu10@gmail.com>
2026-06-15 09:08:47 -0300

    o zero ao avancado - manipulando strings - validando strings 01

--- do-zero/manipulando_strings/validando_strings.py
+++
@@ -1,7 +0,0 @@
-s1 = "curso de python"
-r = s1.isalnum()
-print(r)
-
-s1 = "42"
-r = s1.isalnum()
-print(r)
---
+++ do-zero/manipulando_strings/validando_strings_01.py
@@ -0,0 +1,9 @@
+# isalnum() = testa se a string contem caracteres e numeros dentro dela, somente. caso ela esteja vazia ou contenha caracteres
+# especiais, retornará False
+s1 = "curso de python"
+r = s1.isalnum()
+print(r)
+
+s1 = "42"
+r = s1.isalnum()
+print(r)
