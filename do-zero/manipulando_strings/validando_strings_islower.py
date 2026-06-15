1c6ceac41ca2101b6906a02f860a71cb43275385
George Salu <george.salu10@gmail.com>
2026-06-15 09:46:40 -0300

    o zero ao avancado - manipulando strings - validando strings ilower

---
+++ do-zero/manipulando_strings/validando_strings_islower.py
@@ -0,0 +1,10 @@
+# islower() = é uma função interna do Python que verifica se todos os caracteres alfabéticos de uma string estão em minúsculas.
+
+texto = "python"
+print(texto.islower())
+
+texto2 = "Python"
+print(texto2.islower())
+
+texto3 = "linguagem python 3"
+print(texto3.islower())
