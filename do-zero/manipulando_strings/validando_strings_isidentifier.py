6552b79819fd84369161869d8f1ebc4d5ce4656a
George Salu <george.salu10@gmail.com>
2026-06-16 08:51:27 -0300

    o zero ao avancado - manipulando strings - validando strings isidentifier

--- do-zero/manipulando_strings/validando_strings_isidentifier.py
+++ do-zero/manipulando_strings/validando_strings_isidentifier.py
@@ -1,4 +1,5 @@
-# isidentifier() = verifica se uma string é um identificador válido.
+# isidentifier() = verifica se uma string é um identificador válido.Retorna True se a string contiver apenas letras, números
+# e underscores, e não começar com um número. Ele é muito usado para validar nomes de variáveis, funções ou chaves em parsers
 
 nome_valido = "minha_variavel_1"
 print(nome_valido.isidentifier())
@@ -7,4 +8,4 @@ nome_invalido = "2a_variavel"
 print(nome_invalido.isidentifier())
 
 com_espaco = "variavel numero um"
-print(com_espaco.isidentifier()) 
+print(com_espaco.isidentifier())
