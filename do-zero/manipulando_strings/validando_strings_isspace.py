79f9884d2b44a93815c05f95d3a6542de578f397
George Salu <george.salu10@gmail.com>
2026-06-16 20:30:39 -0300

    o zero ao avancado - manipulando strings - corrige descricao isspace

--- do-zero/manipulando_strings/validando_strings_isspace.py
+++ do-zero/manipulando_strings/validando_strings_isspace.py
@@ -1,4 +1,4 @@
-# isspace() = verifica se uma string contém apenas espaços em branco (como espaços simples  , tabulações \t ou quebras de linha \n).
+# isspace() = verifica se uma string contém apenas espaços em branco (como espaços simples, tabulações \t ou quebras de linha \n).
 # Ele retorna True se todos os caracteres forem espaços e houver pelo menos um caractere, ou False caso contrário.
 
 texto = "   "
