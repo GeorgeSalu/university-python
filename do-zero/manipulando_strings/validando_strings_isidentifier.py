64fc9ab8db1a4ec8226af142081cc171f3c2dd21
George Salu <george.salu10@gmail.com>
2026-06-16 08:32:15 -0300

    o zero ao avancado - manipulando strings - validando strings isidentifier

---
+++ do-zero/manipulando_strings/validando_strings_isidentifier.py
@@ -0,0 +1,10 @@
+# isidentifier() = verifica se uma string é um identificador válido.
+
+nome_valido = "minha_variavel_1"
+print(nome_valido.isidentifier())
+
+nome_invalido = "2a_variavel"
+print(nome_invalido.isidentifier())
+
+com_espaco = "variavel numero um"
+print(com_espaco.isidentifier()) 
