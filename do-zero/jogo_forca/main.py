1402ab81fb8cc1a2f1dd5004a528a5729e970744
George Salu <george.salu10@gmail.com>
2026-06-17 14:44:39 -0300

    o zero ao avancado - jogo da forca - criando a logica da forca 02

--- do-zero/jogo_forca/main.py
+++ do-zero/jogo_forca/main.py
@@ -6,3 +6,16 @@ for x in range(50):
 digitadas = []
 acertos = []
 erros = 0
+
+while True:
+    adivinha = ""
+    for letra in palavra:
+        if letra in acertos:
+            adivinha += letra
+        else:
+            adivinha += "\u2588"
+    print(f"adivinhe: ")
+    for letra in adivinha:
+        print(f"{letra}", end="")
+
+    break
