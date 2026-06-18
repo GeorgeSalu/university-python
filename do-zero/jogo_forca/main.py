53a6e0c2689e828b0a4c82f2be0dbd531a40b001
George Salu <george.salu10@gmail.com>
2026-06-18 09:30:15 -0300

    o zero ao avancado - jogo da forca - criando a logica da forca 03

--- do-zero/jogo_forca/main.py
+++ do-zero/jogo_forca/main.py
@@ -14,8 +14,25 @@ while True:
             adivinha += letra
         else:
             adivinha += "\u2588"
-    print(f"adivinhe: ")
+    print(f"adivinhe ({len(palavra)} letras) : ")
     for letra in adivinha:
         print(f"{letra}", end="")
+    print()
 
-    break
+    # condição de vitoria
+    if adivinha == palavra:
+        print("voce acertou")
+        break
+
+    # tentativas
+    tentativa = input("\nDigite uma letra: ").lower().strip()
+    if tentativa in digitadas:
+        print("voce ja usou essa latra!")
+        continue
+    else:
+        digitadas += tentativa
+        if tentativa in palavra:
+            acertos += tentativa
+        else:
+            erros += 1
+            print(f"voce errou")
