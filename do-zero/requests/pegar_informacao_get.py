9ff1c68dc637edb35c466a31ef12c96eaec1b46d
George Salu <george.salu10@gmail.com>
2026-06-20 13:07:25 -0300

    o zero ao avancado - requests - pegar informação - get 01

--- do-zero/requests/pegar_informacao_get.py
+++ do-zero/requests/pegar_informacao_get.py
@@ -1,4 +1,7 @@
 import requests
 
 requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+# retorno da requesicao statusCode
 print(requisicao)
+# retorno da requisicao json
+print(requisicao.json())
