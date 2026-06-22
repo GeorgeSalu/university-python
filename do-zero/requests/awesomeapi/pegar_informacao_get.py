01b7cc7842d0c6d1f1eb95e22675c1341361b047
George Salu <george.salu10@gmail.com>
2026-06-22 09:39:30 -0300

    o zero ao avancado - requests - pegar informação - get 06

--- do-zero/requests/awesomeapi/pegar_informacao_get.py
+++ do-zero/requests/awesomeapi/pegar_informacao_get.py
@@ -3,6 +3,9 @@ import requests
 # Faz a requisição para a URL
 requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 
+# Mostra a URL final
+print(requisicao.url)
+
 # Imprime o código de status (ex: 200 para sucesso)
 print(requisicao.status_code)
 
