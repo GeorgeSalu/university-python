fe90c340753feb49f4cffec3ac2e9c0b31567f12
George Salu <george.salu10@gmail.com>
2026-06-26 20:49:50 -0300

    o zero ao avancado - requests - post informação - post autenticacao basica

--- do-zero/requests/awesomeapi/metodo_post_httpbin_04.py
+++ do-zero/requests/awesomeapi/metodo_post_httpbin_04.py
@@ -1,4 +1,6 @@
 import requests
 
+# Autenticação Básica
+# Envia credenciais de usuário (usuário e senha) no cabeçalho da requisição.
 response = requests.post('https://httpbin.org/post', auth=('user', 'pass'))
 print(response.status_code)
