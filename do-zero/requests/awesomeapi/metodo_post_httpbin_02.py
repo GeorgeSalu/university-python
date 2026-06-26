f41c46946bbf2f37c0b3c0ce1f8946e03aef73e8
George Salu <george.salu10@gmail.com>
2026-06-26 20:48:47 -0300

    o zero ao avancado - requests - post informação - post envio de dados simples

--- do-zero/requests/awesomeapi/metodo_post_httpbin_02.py
+++ do-zero/requests/awesomeapi/metodo_post_httpbin_02.py
@@ -1,5 +1,7 @@
 import requests
 
+# Envio de Dados Simples (Formulário)
+# Envia dados de um formulário padrão codificado como application/x-www-form-urlencoded
 payload = {'nome': 'Ana', 'idade': '28'}
 response = requests.post('https://httpbin.org/post', data=payload)
 print(response.json())
