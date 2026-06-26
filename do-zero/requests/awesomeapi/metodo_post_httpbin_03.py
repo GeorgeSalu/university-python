7561e6d6287739d50fe8f77651d55df9f315e635
George Salu <george.salu10@gmail.com>
2026-06-26 20:49:15 -0300

    o zero ao avancado - requests - post informação - post envio de dados json

--- do-zero/requests/awesomeapi/metodo_post_httpbin_03.py
+++ do-zero/requests/awesomeapi/metodo_post_httpbin_03.py
@@ -1,5 +1,7 @@
 import requests
 
+# Envio de Dados em JSON
+# Ideal para trabalhar com APIs modernas que exigem o envio de objetos estruturados.
 payload = {'titulo': 'Python', 'status': 'ativo'}
 response = requests.post('https://httpbin.org/post', json=payload)
 print(response.json())
