126bb0753ba88c3832096649d8927183e65c1dfc
George Salu <george.salu10@gmail.com>
2026-06-28 11:52:46 -0300

    o zero ao avancado - requests - put - put enviando dados json

--- do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_02.py
+++ do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_02.py
@@ -1,5 +1,7 @@
 import requests
 
+# Enviando dados no formato JSON
+# Ideal para APIs modernas REST. Converte o dicionário automaticamente com json=
 dados = {"nome": "Python", "tipo": "Linguagem"}
 resposta = requests.put("https://httpbin.org/put", json=dados)
 print(resposta.json()["json"])
