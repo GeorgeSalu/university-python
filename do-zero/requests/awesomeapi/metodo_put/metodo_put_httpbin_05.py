15f469a2c173816ea4d20898ea04fb9ea9132af4
George Salu <george.salu10@gmail.com>
2026-06-28 12:32:20 -0300

    o zero ao avancado - requests - put - definindo tempo limite

--- do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_05.py
+++ do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_05.py
@@ -1,5 +1,7 @@
 import requests
 
+# Definindo tempo limite (Timeout)
+# Evita que seu código trave caso o servidor demore a responder (exemplo define limite de 5 segundos).
 try:
     resposta = requests.put("https://httpbin.org/put", json={"ok": 1}, timeout=5)
     print(resposta.status_code)
