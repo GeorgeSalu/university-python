7f140bc6170f52473fa872a008e2b1d4120ee887
George Salu <george.salu10@gmail.com>
2026-06-28 12:21:35 -0300

    o zero ao avancado - requests - put - put configurando cabeçalhos headers

--- do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_04.py
+++ do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_04.py
@@ -1,5 +1,7 @@
 import requests
 
+# Configurando Cabeçalhos (Headers)
+# Envia metadados sobre a requisição (como especificar autenticação ou formato de dado).
 cabecalhos = {"Authorization": "Bearer meu_token_secreto", "Content-Type": "application/json"}
 resposta = requests.put(
     "https://httpbin.org/put", headers=cabecalhos, json={"ativo": True}
