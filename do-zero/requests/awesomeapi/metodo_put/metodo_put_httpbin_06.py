ccfb8d34b30b7fb30e49728142a1ae57cc5b35b0
George Salu <george.salu10@gmail.com>
2026-06-28 12:42:00 -0300

    o zero ao avancado - requests - put - autenticacao basica

---
+++ do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_06.py
@@ -0,0 +1,8 @@
+import requests
+
+# Autenticação Básica (HTTP Basic Auth)
+# Envia credenciais de usuário e senha de forma padrão.
+resposta = requests.put(
+    "https://httpbin.org/put", auth=("usuario_teste", "senha123")
+)
+print(resposta.status_code)
