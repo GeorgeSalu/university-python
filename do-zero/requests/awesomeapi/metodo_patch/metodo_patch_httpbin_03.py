ee6f4f04138a138b74493791a6dfe43aaeef3578
George Salu <george.salu10@gmail.com>
2026-06-28 13:46:28 -0300

    o zero ao avancado - requests - patch - enviando dados json

--- do-zero/requests/awesomeapi/metodo_patch/metodo_patch_httpbin_03.py
+++ do-zero/requests/awesomeapi/metodo_patch/metodo_patch_httpbin_03.py
@@ -1,5 +1,7 @@
 import requests
 
+# Enviando dados no formato JSON
+# Ideal para APIs modernas REST. Utilize o argumento json em vez de data para que a biblioteca defina o cabeçalho Content-Type: application/json automaticamente.
 url = "https://httpbin.org/patch"
 payload = {"titulo": "Novo Titulo", "ativo": True}
 
