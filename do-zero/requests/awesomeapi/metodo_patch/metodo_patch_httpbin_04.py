43d528a3d745903ae9f3c473d5876055a7c2ce4e
George Salu <george.salu10@gmail.com>
2026-06-28 13:57:45 -0300

    o zero ao avancado - requests - patch - enviando parametros e cabeçalhos

---
+++ do-zero/requests/awesomeapi/metodo_patch/metodo_patch_httpbin_04.py
@@ -0,0 +1,13 @@
+import requests
+
+# Enviando Parâmetros (Params) e Cabeçalhos (Headers)
+# Adiciona chaves de autenticação ou filtros na URL junto com os dados modificados.
+url = "https://httpbin.org/patch"
+meus_headers = {"Authorization": "Bearer SEU_TOKEN_AQUI"}
+meus_params = {"id_usuario": 105}
+dados_patch = {"nome": "Novo Nome"}
+
+resposta = requests.patch(url, params=meus_params, data=dados_patch, headers=meus_headers)
+
+print("Status Code:", resposta.status_code)
+print("Dados recebidos pelo servidor:", resposta.json())
