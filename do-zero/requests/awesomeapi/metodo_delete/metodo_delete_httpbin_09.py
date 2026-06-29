65d1d207ed3d0cf109e8e863272a1bc1f78421e8
George Salu <george.salu10@gmail.com>
2026-06-29 11:39:46 -0300

    o zero ao avancado - requests - delete - autenticacao basica

--- do-zero/requests/awesomeapi/metodo_delete/metodo_delete_httpbin_09.py
+++ do-zero/requests/awesomeapi/metodo_delete/metodo_delete_httpbin_09.py
@@ -1,5 +1,6 @@
 import requests
 
+# Definindo Autenticação Básica
 resposta = requests.delete("https://httpbin.org/delete", auth=("usuario", "senha"))
 print(resposta.json())
 print(resposta.status_code)
