11ceb823a816923ea13cbdec3b65a30c04d03169
George Salu <george.salu10@gmail.com>
2026-06-29 11:55:22 -0300

    o zero ao avancado - requests - patch - lancando excecao de erro

--- do-zero/requests/awesomeapi/metodo_patch/metodo_patch_httpbin_08.py
+++ do-zero/requests/awesomeapi/metodo_patch/metodo_patch_httpbin_08.py
@@ -1,5 +1,5 @@
 import requests
 
 # Lançando Exceção para Erros HTTP (4xx ou 5xx)
-resposta = requests.patch("https://httpbin.org")
+resposta = requests.patch("https://httpbin.org/patch")
 resposta.raise_for_status()
