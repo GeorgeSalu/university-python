d0318a0611fc664c1bb47b1cd32080d0697e7707
George Salu <george.salu10@gmail.com>
2026-06-30 20:02:38 -0300

    o zero ao avancado - requests - get - enviando query params

--- do-zero/requests/awesomeapi/metodo_get/metodo_get_github_02.py
+++ do-zero/requests/awesomeapi/metodo_get/metodo_get_github_02.py
@@ -1,6 +1,6 @@
 import requests
 
-# query params
+# Enviando Parâmetros de Query (URL)
 parametros = {'q': 'python', 'sort': 'stars'}
 resposta = requests.get('https://github.com', params=parametros)
 print(resposta.url)
