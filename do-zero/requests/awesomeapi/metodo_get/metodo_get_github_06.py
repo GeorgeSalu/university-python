d42f3c7d0ebc4203f661002783004d5dfcae6042
George Salu <george.salu10@gmail.com>
2026-07-01 09:03:47 -0300

    o zero ao avancado - requests - get - definindo autenticacao basica

--- do-zero/requests/awesomeapi/metodo_get/metodo_get_github_06.py
+++ do-zero/requests/awesomeapi/metodo_get/metodo_get_github_06.py
@@ -1,5 +1,6 @@
 import requests
 
+# Definindo Autenticação Básica
 from requests.auth import HTTPBasicAuth
 auth = HTTPBasicAuth('usuario', 'senha')
 resposta = requests.get('https://api.github.com/user', auth=auth)
