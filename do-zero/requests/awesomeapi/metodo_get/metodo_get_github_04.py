4eb42d1d9be6705f2c07b42dc427c8fd2a627180
George Salu <george.salu10@gmail.com>
2026-07-01 09:00:40 -0300

    o zero ao avancado - requests - get - enviando cabecalhos

--- do-zero/requests/awesomeapi/metodo_get/metodo_get_github_04.py
+++ do-zero/requests/awesomeapi/metodo_get/metodo_get_github_04.py
@@ -1,5 +1,6 @@
 import requests
 
+# Enviando Cabeçalhos (Headers)
 cabecalhos = {'User-Agent': 'MeuScriptPython/1.0'}
 resposta = requests.get('https://api.github.com', headers=cabecalhos)
 print(resposta.text)
