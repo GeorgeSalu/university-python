9b996965db134fc7bdfee78b48dd8f0767814401
George Salu <george.salu10@gmail.com>
2026-07-01 09:19:20 -0300

    o zero ao avancado - requests - get - resposta de redirecionamento

--- do-zero/requests/awesomeapi/metodo_get/metodo_get_github_09.py
+++ do-zero/requests/awesomeapi/metodo_get/metodo_get_github_09.py
@@ -1,4 +1,5 @@
 import requests
 
+# Isso permite inspecionar manualmente a resposta de redirecionamento
 resposta = requests.get('http://github.com', allow_redirects=False)
 print(resposta.status_code)
