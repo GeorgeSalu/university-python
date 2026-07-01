b30a115cb768f6ce89016bbee2a1c037c91fb44c
George Salu <george.salu10@gmail.com>
2026-07-01 09:58:21 -0300

    o zero ao avancado - requests - get - verificando status de resposta

--- do-zero/requests/awesomeapi/metodo_get/metodo_get_github_10.py
+++ do-zero/requests/awesomeapi/metodo_get/metodo_get_github_10.py
@@ -1,6 +1,7 @@
 import requests
 
 # verificando status de resposta
+# Se o código de status estiver entre 200 e 400, retornará True
 resposta = requests.get('https://api.github.com')
 
 if resposta.ok:  # Equivalente a resposta.status_code < 400
