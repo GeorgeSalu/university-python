31090e6d65d561981926d54447fe7f838b40a813
George Salu <george.salu10@gmail.com>
2026-07-02 09:52:57 -0300

    o zero ao avancado - requests - post - modificano sessao

--- do-zero/requests/awesomeapi/metodo_post/metodo_post_httpbin_08.py
+++ do-zero/requests/awesomeapi/metodo_post/metodo_post_httpbin_08.py
@@ -1,5 +1,6 @@
 import requests
 
+# modificando sessão
 sessao = requests.Session()
 sessao.headers.update({'x-app-id': '12345'})
 response = sessao.post('https://httpbin.org/post', data={'chave': 'valor'})
