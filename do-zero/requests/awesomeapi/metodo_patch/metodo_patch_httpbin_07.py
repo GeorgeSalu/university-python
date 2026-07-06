3df1e2fb8e955fe3e5a37552ae8cb5a6d6d68927
George Salu <george.salu10@gmail.com>
2026-07-06 16:51:45 -0300

    o zero ao avancado - requests - patch - autenticacao basica

--- do-zero/requests/awesomeapi/metodo_patch/metodo_patch_httpbin_07.py
+++ do-zero/requests/awesomeapi/metodo_patch/metodo_patch_httpbin_07.py
@@ -2,5 +2,6 @@ import requests
 
 # Definindo Autenticação Básica
 resposta = requests.patch("https://httpbin.org/patch", auth=("usuario", "senha"))
-print(resposta.json())
 print(resposta.status_code)
+print(resposta.json())
+print(resposta.json()["headers"]["Authorization"])
