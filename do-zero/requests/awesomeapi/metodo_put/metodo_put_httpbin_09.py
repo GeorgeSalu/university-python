9f301fee978d72a722d1270d37fb45c92ff2a564
George Salu <george.salu10@gmail.com>
2026-07-06 16:29:22 -0300

    o zero ao avancado - requests - put - envio de dados formulario

--- do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_09.py
+++ do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_09.py
@@ -1,5 +1,7 @@
 import requests
 
+# Envio de Dados Simples (Formulário)
+# Envia dados de um formulário padrão codificado como application/x-www-form-urlencoded
 payload = {'nome': 'Ana', 'idade': '28'}
 response = requests.put('https://httpbin.org/put', data=payload)
 print(response.json())
