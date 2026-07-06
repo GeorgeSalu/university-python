76025017d5044bf08bf2b349bdabbb4d05b94710
George Salu <george.salu10@gmail.com>
2026-07-06 16:18:34 -0300

    o zero ao avancado - requests - put - desabilitando a verificacao ssl

--- do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_07.py
+++ do-zero/requests/awesomeapi/metodo_put/metodo_put_httpbin_07.py
@@ -1,7 +1,7 @@
 import requests
 
 # Desabilitando a verificação SSL
-# Útil para desenvolvimento local com certificados auto-assinados (em produção, mantenha como True).
+# Útil para desenvolvimento local com certificados autoassinados (em produção, mantenha como True).
 resposta = requests.put(
     "https://httpbin.org/put", json={" ssl": "ignorado"}, verify=False
 )
